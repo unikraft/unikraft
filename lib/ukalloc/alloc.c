a8cb90f65ad35e8aee7156f73639b2ffb1bf71ac
Simon Kuenzer <simon.kuenzer@neclab.eu>
2017-11-29 15:39:59 +0100

    lib/ukalloc: Add abstraction library for memory allocators
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>
    Signed-off-by: Florian Schmidt <florian.schmidt@neclab.eu>

---
+++ lib/ukalloc/alloc.c
@@ -0,0 +1,269 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Florian Schmidt <florian.schmidt@neclab.eu>
+ *
+ * Copyright (c) 2017, NEC Europe Ltd., NEC Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
+ */
+
+/* This is a very simple, naive implementation of malloc.
+ * It's not space-efficient, because it always requests full pages from the
+ * underlying memory allocator. It's also not very fast, because for the
+ * same reason, it will never hand out memory from already-requested pages,
+ * and has to go through the underlying memory allocator on every malloc()
+ * and free() (and friends. And God have mercy on your soul if you call free()
+ * with a pointer that wasn't received from malloc(). But it's simple, and,
+ * above all, it is inherently reentrant, because all bookkeeping is
+ * decentralized. This is important, because we currently don't have proper
+ * locking support yet. Eventually, this should probably be replaced by
+ * something better.
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <uk/alloc.h>
+#include <uk/config.h>
+#include <uk/essentials.h>
+#include <uk/assert.h>
+#include <uk/arch/limits.h>
+
+static struct uk_alloc *uk_alloc_head;
+
+int uk_alloc_register(struct uk_alloc *a)
+{
+	struct uk_alloc *this = uk_alloc_head;
+
+	if (!uk_alloc_head) {
+		uk_alloc_head = a;
+		a->next = NULL;
+		return 0;
+	}
+
+	while (this && this->next)
+		this = this->next;
+	this->next = a;
+	a->next = NULL;
+	return 0;
+}
+
+struct uk_alloc *uk_alloc_get_default(void)
+{
+	return uk_alloc_head;
+}
+
+int uk_alloc_set_default(struct uk_alloc *a)
+{
+	struct uk_alloc *head, *this, *prev;
+
+	head = uk_alloc_get_default();
+
+	if (a == head)
+		return 0;
+
+	if (!head) {
+		uk_alloc_head = a;
+		return 0;
+	}
+
+	this = head;
+	while (this->next) {
+		prev = this;
+		this = this->next;
+		if (a == this) {
+			prev->next = this->next;
+			this->next = head->next;
+			head = this;
+			return 0;
+		}
+	}
+
+	/* a is not registered yet. Add in front of the queue. */
+	a->next = head;
+	uk_alloc_head = a;
+	return 0;
+}
+
+#if LIBUKALLOC_IFPAGES
+static void *uk_get_real_start(const void *ptr)
+{
+	void *intptr;
+
+	intptr = (void *) ALIGN_DOWN((uintptr_t) ptr,
+				     (uintptr_t) __PAGE_SIZE);
+	if (intptr == ptr) {
+		/* special case: the memory was page-aligned.
+		 * In this cas,e the size information lies at the start of the
+		 * previous page, with the rest of that page unused.
+		 */
+		intptr -= __PAGE_SIZE;
+	}
+	return intptr;
+}
+
+static size_t uk_getmallocsize(const void *ptr)
+{
+	size_t *intptr = uk_get_real_start(ptr);
+
+	return *intptr;
+}
+
+/* return the smallest order (1<<order pages) that can fit size bytes */
+static inline size_t uk_alloc_size_to_order(size_t size)
+{
+	size_t order = 0;
+
+	while ((__PAGE_SIZE << order) < size)
+		order++;
+	return order;
+}
+
+void *uk_malloc_ifpages(struct uk_alloc *a, size_t size)
+{
+	size_t *intptr;
+	size_t order;
+	size_t realsize = sizeof(order) + size;
+
+	UK_ASSERT(a);
+	if (!size)
+		return NULL;
+
+	order = uk_alloc_size_to_order(realsize);
+	intptr = uk_palloc(a, order);
+
+	if (!intptr)
+		return NULL;
+
+	*intptr = order;
+	return intptr + sizeof(order);
+}
+
+void uk_free_ifpages(struct uk_alloc *a, void *ptr)
+{
+	size_t *intptr;
+
+	UK_ASSERT(a);
+	if (!ptr)
+		return;
+
+	intptr = uk_get_real_start(ptr);
+	uk_pfree(a, intptr, *intptr);
+}
+
+void *uk_realloc_ifpages(struct uk_alloc *a, void *ptr, size_t size)
+{
+	void *retptr;
+	size_t mallocsize;
+
+	UK_ASSERT(a);
+	if (!ptr)
+		return uk_malloc_ifpages(a, size);
+
+	if (ptr && !size) {
+		uk_free_ifpages(a, ptr);
+		return NULL;
+	}
+
+	retptr = uk_malloc_ifpages(a, size);
+	if (!retptr)
+		return NULL;
+
+	mallocsize = uk_getmallocsize(ptr);
+
+	if (size < mallocsize)
+		memcpy(retptr, ptr, size);
+	else
+		memcpy(retptr, ptr, mallocsize);
+
+	uk_free_ifpages(a, ptr);
+	return retptr;
+}
+
+int uk_posix_memalign_ifpages(struct uk_alloc *a,
+				void **memptr, size_t align, size_t size)
+{
+	size_t *intptr;
+	size_t realsize;
+	size_t order;
+
+	UK_ASSERT(a);
+	if (((align - 1) & align) != 0
+	    || (align % sizeof(void *)) != 0
+	    || (align > __PAGE_SIZE))
+		return EINVAL;
+
+	if (!size) {
+		*memptr = NULL;
+		return 0;
+	}
+
+	/* For page-aligned memory blocks, the size information is not stored
+	 * immediately preceding the memory block, but instead at the
+	 * beginning of the page preceeding the memory handed out via malloc.
+	 */
+	if (align == __PAGE_SIZE)
+		realsize = ALIGN_UP(size + __PAGE_SIZE, align);
+	else
+		realsize = ALIGN_UP(size + sizeof(order), align);
+
+	order = uk_alloc_size_to_order(realsize);
+	intptr = uk_palloc(a, order);
+
+	if (!intptr)
+		return ENOMEM;
+
+	*intptr = order;
+	return ALIGN_UP((uintptr_t)intptr + sizeof(order), align);
+}
+
+#endif
+
+void *uk_calloc_compat(struct uk_alloc *a, size_t nmemb, size_t size)
+{
+	void *ptr;
+	size_t tlen = nmemb * size;
+
+	UK_ASSERT(a);
+	ptr = uk_malloc(a, tlen);
+	if (!ptr)
+		return NULL;
+
+	memset(ptr, 0, tlen);
+	return ptr;
+}
+
+void *uk_memalign_compat(struct uk_alloc *a, size_t align, size_t size)
+{
+	void *ptr;
+
+	UK_ASSERT(a);
+	if (!uk_posix_memalign(a, &ptr, align, size))
+		return NULL;
+
+	return ptr;
+}
---
+++ lib/ukalloc/include/uk/alloc.h
@@ -0,0 +1,270 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Simon Kuenzer <simon.kuenzer@neclab.eu>
+ *          Florian Schmidt <florian.schmidt@neclab.eu>
+ *
+ * Copyright (c) 2017, NEC Europe Ltd., NEC Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
+ */
+
+#ifndef __UK_ALLOC_H__
+#define __UK_ALLOC_H__
+
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <uk/config.h>
+#include <uk/assert.h>
+#include <uk/essentials.h>
+
+struct uk_alloc;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int uk_alloc_register(struct uk_alloc *a);
+struct uk_alloc *uk_alloc_get_default(void);
+int uk_alloc_set_default(struct uk_alloc *a);
+
+typedef void* (*uk_alloc_malloc_func_t)
+		(struct uk_alloc *a, size_t size);
+typedef void* (*uk_alloc_calloc_func_t)
+		(struct uk_alloc *a, size_t nmemb, size_t size);
+typedef int   (*uk_alloc_posix_memalign_func_t)
+		(struct uk_alloc *a, void **memptr, size_t align, size_t size);
+typedef void* (*uk_alloc_memalign_func_t)
+		(struct uk_alloc *a, size_t align, size_t size);
+typedef void* (*uk_alloc_realloc_func_t)
+		(struct uk_alloc *a, void *ptr, size_t size);
+typedef void  (*uk_alloc_free_func_t)
+		(struct uk_alloc *a, void *ptr);
+#if LIBUKALLOC_IFPAGES
+typedef void* (*uk_alloc_palloc_func_t)
+		(struct uk_alloc *a, size_t order);
+typedef void  (*uk_alloc_pfree_func_t)
+		(struct uk_alloc *a, void *ptr, size_t order);
+#endif
+typedef int   (*uk_alloc_addmem_func_t)
+		(struct uk_alloc *a, void *base, size_t size);
+#if LIBUKALLOC_IFSTATS
+typedef ssize_t (*uk_alloc_availmem_func_t)
+		(struct uk_alloc *a);
+#endif
+
+struct uk_alloc {
+	/* memory allocation */
+	uk_alloc_malloc_func_t malloc;
+	uk_alloc_calloc_func_t calloc;
+	uk_alloc_realloc_func_t realloc;
+	uk_alloc_posix_memalign_func_t posix_memalign;
+	uk_alloc_memalign_func_t memalign;
+	uk_alloc_free_func_t free;
+
+#if LIBUKALLOC_IFPAGES
+	/* page allocation interface */
+	uk_alloc_palloc_func_t palloc;
+	uk_alloc_pfree_func_t pfree;
+#endif
+#if LIBUKALLOC_IFSTATS
+	/* optional interface */
+	uk_alloc_availmem_func_t availmem;
+#endif
+	/* optional interface */
+	uk_alloc_addmem_func_t addmem;
+
+	/* internal */
+	struct uk_alloc *next;
+	int8_t private[];
+};
+
+/* wrapper functions */
+static inline void *uk_do_malloc(struct uk_alloc *a, size_t size)
+{
+	UK_ASSERT(a);
+	return a->malloc(a, size);
+}
+static inline void *uk_malloc(struct uk_alloc *a, size_t size)
+{
+	if (unlikely(!a)) {
+		errno = ENOMEM;
+		return NULL;
+	}
+	return uk_do_malloc(a, size);
+}
+
+static inline void *uk_do_calloc(struct uk_alloc *a,
+				 size_t nmemb, size_t size)
+{
+	UK_ASSERT(a);
+	return a->calloc(a, nmemb, size);
+}
+static inline void *uk_calloc(struct uk_alloc *a,
+			      size_t nmemb, size_t size)
+{
+	if (unlikely(!a)) {
+		errno = ENOMEM;
+		return NULL;
+	}
+	return uk_do_calloc(a, nmemb, size);
+}
+
+static inline void *uk_do_realloc(struct uk_alloc *a,
+				  void *ptr, size_t size)
+{
+	UK_ASSERT(a);
+	return a->realloc(a, ptr, size);
+}
+static inline void *uk_realloc(struct uk_alloc *a, void *ptr, size_t size)
+{
+	if (unlikely(!a)) {
+		errno = ENOMEM;
+		return NULL;
+	}
+	return uk_do_realloc(a, ptr, size);
+}
+
+static inline int uk_do_posix_memalign(struct uk_alloc *a, void **memptr,
+				       size_t align, size_t size)
+{
+	UK_ASSERT(a);
+	return a->posix_memalign(a, memptr, align, size);
+}
+static inline int uk_posix_memalign(struct uk_alloc *a, void **memptr,
+				    size_t align, size_t size)
+{
+	if (unlikely(!a)) {
+		*memptr = NULL;
+		return ENOMEM;
+	}
+	return uk_do_posix_memalign(a, memptr, align, size);
+}
+
+static inline void *uk_do_memalign(struct uk_alloc *a,
+				   size_t align, size_t size)
+{
+	UK_ASSERT(a);
+	return a->memalign(a, align, size);
+}
+static inline void *uk_memalign(struct uk_alloc *a,
+				size_t align, size_t size)
+{
+	if (unlikely(!a))
+		return NULL;
+	return uk_do_memalign(a, align, size);
+}
+
+static inline void uk_do_free(struct uk_alloc *a, void *ptr)
+{
+	UK_ASSERT(a);
+	a->free(a, ptr);
+}
+static inline void uk_free(struct uk_alloc *a, void *ptr)
+{
+	uk_do_free(a, ptr);
+}
+
+#if LIBUKALLOC_IFPAGES
+static inline void *uk_do_palloc(struct uk_alloc *a, size_t order)
+{
+	UK_ASSERT(a);
+	return a->palloc(a, order);
+}
+static inline void *uk_palloc(struct uk_alloc *a, size_t order)
+{
+	if (unlikely(!a))
+		return NULL;
+	return a->palloc(a, order);
+}
+static inline void uk_do_pfree(struct uk_alloc *a, void *ptr, size_t order)
+{
+	UK_ASSERT(a);
+	a->pfree(a, ptr, order);
+}
+static inline void uk_pfree(struct uk_alloc *a, void *ptr, size_t order)
+{
+	a->pfree(a, ptr, order);
+}
+#endif
+static inline int uk_alloc_addmem(struct uk_alloc *a, void *base,
+				  size_t size)
+{
+	UK_ASSERT(a);
+	if (a->addmem)
+		return a->addmem(a, base, size);
+	else
+		return -ENOTSUP;
+}
+#if LIBUKALLOC_IFSTATS
+static inline ssize_t uk_alloc_availmem(struct uk_alloc *a)
+{
+	UK_ASSERT(a);
+	if (!a->availmem)
+		return (ssize_t) -ENOTSUP;
+	return a->availmem(a);
+}
+#endif /* LIBUKALLOC_IFSTATS */
+
+#if LIBUKALLOC_IFPAGES
+/* uses palloc(), pfree() */
+void *uk_malloc_ifpages(struct uk_alloc *a, size_t size);
+void *uk_realloc_ifpages(struct uk_alloc *a, void *ptr, size_t size);
+int uk_posix_memalign_ifpages(struct uk_alloc *a, void **memptr,
+				size_t align, size_t size);
+void *uk_memalign_ifpages(struct uk_alloc *a, size_t align, size_t size);
+void uk_free_ifpages(struct uk_alloc *a, void *ptr);
+#endif
+
+/* generic, use malloc() */
+void *uk_calloc_compat(struct uk_alloc *a, size_t num, size_t len);
+void *uk_memalign_compat(struct uk_alloc *a, size_t align, size_t len);
+
+#if LIBUKALLOC_IFPAGES
+#define uk_alloc_init_palloc(a, palloc_func, pfree_func, addmem_func)	\
+	do {								\
+		(a)->malloc         = uk_malloc_ifpages;		\
+		(a)->calloc         = uk_calloc_compat;			\
+		(a)->realloc        = uk_realloc_ifpages;		\
+		(a)->posix_memalign = uk_posix_memalign_ifpages;	\
+		(a)->memalign       = uk_memalign_compat;		\
+		(a)->free           = uk_free_ifpages;			\
+		(a)->palloc         = (palloc_func);			\
+		(a)->pfree          = (pfree_func);			\
+		(a)->addmem         = (addmem_func);			\
+									\
+		uk_alloc_register((a));					\
+	} while (0)
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __UK_ALLOC_H__ */
