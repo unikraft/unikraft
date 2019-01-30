0f5275b4538ca5e8a995c53c86ff72c4f8d7cc3d
Yuri Volchkov <yuri.volchkov@neclab.eu>
2019-01-30 17:38:54 +0100

    lib/nolibc/include: import statfs.h from musl
    This is a combination of include/sys/statfs.h and
    arch/generic/bits/statfs.h. We do not need to provide different
    statfs.h for different architectures, because this files differs only
    for mips*, s390x, x32, which we do not have plans to support.
    
    tag: v1.1.20
    commit: 0fa1e638e87cf257e9f96b4019b2076afd674a19
    
    Signed-off-by: Yuri Volchkov <yuri.volchkov@neclab.eu>
    Reviewed-by: Sharan Santhanam <sharan.santhanam@neclab.eu>

---
+++ lib/nolibc/include/sys/statfs.h
@@ -0,0 +1,36 @@
+#ifndef	_SYS_STATFS_H
+#define	_SYS_STATFS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <sys/statvfs.h>
+
+typedef struct __fsid_t {
+	int __val[2];
+} fsid_t;
+
+struct statfs {
+	unsigned long f_type, f_bsize;
+	fsblkcnt_t f_blocks, f_bfree, f_bavail;
+	fsfilcnt_t f_files, f_ffree;
+	fsid_t f_fsid;
+	unsigned long f_namelen, f_frsize, f_flags, f_spare[4];
+};
+
+int statfs (const char *, struct statfs *);
+int fstatfs (int, struct statfs *);
+
+#if defined(_LARGEFILE64_SOURCE) || defined(_GNU_SOURCE)
+#define statfs64 statfs
+#define fstatfs64 fstatfs
+#define fsblkcnt64_t fsblkcnt_t
+#define fsfilcnt64_t fsfilcnt_t
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
