e6a88d61f12d9e839289070969b435425587ee90
Yuri Volchkov <yuri.volchkov@neclab.eu>
2019-05-24 12:55:58 +0200

    build: add debug symbols and comment sections to all linkers
    Gcc generates them anyways. Let's take a little control over what it
    does.
    
    Signed-off-by: Yuri Volchkov <yuri.volchkov@neclab.eu>
    Reviewed-by: Florian Schmidt <florian.schmidt@neclab.eu>

---
+++ plat/common/include/common.lds.h
@@ -0,0 +1,67 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2019, NEC Laboratories Europe GmbH, NEC Corporation.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef __UK_COMMON_LDS_H
+#define __UK_COMMON_LDS_H
+
+/* DWARF debug sections.  Symbols in the DWARF debugging sections are
+ * relative to the beginning of the section so we begin them at 0.
+ */
+#define DEBUG_SYMBOLS							\
+	/* DWARF 1 */							\
+	.debug          0 : { *(.debug) }				\
+	.line           0 : { *(.line) }				\
+	/* GNU DWARF 1 extensions */					\
+	.debug_srcinfo  0 : { *(.debug_srcinfo) }			\
+	.debug_sfnames  0 : { *(.debug_sfnames) }			\
+	/* DWARF 1.1 and DWARF 2 */					\
+	.debug_aranges  0 : { *(.debug_aranges) }			\
+	.debug_pubnames 0 : { *(.debug_pubnames) }			\
+	/* DWARF 2 */							\
+	.debug_info     0 : { *(.debug_info .gnu.linkonce.wi.*) }	\
+	.debug_abbrev   0 : { *(.debug_abbrev) }			\
+	.debug_line     0 : { *(.debug_line .debug_line.* .debug_line_end ) } \
+	.debug_frame    0 : { *(.debug_frame) }				\
+	.debug_str      0 : { *(.debug_str) }				\
+	.debug_loc      0 : { *(.debug_loc) }				\
+	.debug_macinfo  0 : { *(.debug_macinfo) }			\
+	/* SGI/MIPS DWARF 2 extensions */				\
+	.debug_weaknames 0 : { *(.debug_weaknames) }			\
+	.debug_funcnames 0 : { *(.debug_funcnames) }			\
+	.debug_typenames 0 : { *(.debug_typenames) }			\
+	.debug_varnames  0 : { *(.debug_varnames) }			\
+	/* DWARF 3 */							\
+	.debug_pubtypes 0 : { *(.debug_pubtypes) }			\
+	.debug_ranges   0 : { *(.debug_ranges) }			\
+	/* DWARF Extension.  */						\
+	.debug_macro    0 : { *(.debug_macro) }				\
+	.gnu.attributes 0 : { KEEP (*(.gnu.attributes)) }
+
+#endif /* __UK_COMMON_LDS_H */
