19202a37df6869a9bf5e678b2eb4a05a2cabb35c
Costin Lupu <costin.lupu@cs.pub.ro>
2019-08-14 17:56:31 +0300

    lib/posix-libdl: Adapt dlfcn.h
    Signed-off-by: Andrei Gogonea <andrei.gogonea09@gmail.com>
    Signed-off-by: Teodora Serbanescu <teo.serbanescu16@gmail.com>
    Signed-off-by: Costin Lupu <costin.lupu@cs.pub.ro>
    Reviewed-by: Felipe Huici <felipe.huici@neclab.eu>

--- lib/posix-libdl/include/dlfcn.h
+++ lib/posix-libdl/include/dlfcn.h
@@ -1,3 +1,29 @@
+/* SPDX-License-Identifier: MIT */
+/* ----------------------------------------------------------------------
+ * Copyright © 2005-2014 Rich Felker, et al.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ----------------------------------------------------------------------
+ */
+/* Taken from musl */
+
 #ifndef	_DLFCN_H
 #define	_DLFCN_H
 
@@ -5,7 +31,7 @@
 extern "C" {
 #endif
 
-#include <features.h>
+/* TODO #include <features.h> */
 
 #define RTLD_LAZY   1
 #define RTLD_NOW    2
@@ -24,7 +50,6 @@ char  *dlerror(void);
 void  *dlopen(const char *, int);
 void  *dlsym(void *__restrict, const char *__restrict);
 
-#if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
 typedef struct {
 	const char *dli_fname;
 	void *dli_fbase;
@@ -33,7 +58,6 @@ typedef struct {
 } Dl_info;
 int dladdr(const void *, Dl_info *);
 int dlinfo(void *, int, void *);
-#endif
 
 #ifdef __cplusplus
 }
