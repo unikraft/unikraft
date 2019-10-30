30f42955fc36ded7b643c818aa1180f2fee69f96
Roxana Nicolescu <nicolescu.roxana1996@gmail.com>
2019-10-30 17:54:48 +0200

    plat/xen/drivers: Introduce blkfront driver skeleton
    This patch introduces the blkfront driver skeleton.
    
    Signed-off-by: Roxana Nicolescu <nicolescu.roxana1996@gmail.com>
    Reviewed-by: Costin Lupu <costin.lupu@cs.pub.ro>

---
+++ plat/xen/drivers/blk/blkfront.c
@@ -0,0 +1,70 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Roxana Nicolescu <nicolescu.roxana1996@gmail.com>
+ *
+ * Copyright (c) 2019, University Politehnica of Bucharest.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
+ */
+#include <xenbus/xenbus.h>
+
+#define DRIVER_NAME		"xen-blkfront"
+
+static struct uk_alloc *drv_allocator;
+
+static int blkfront_add_dev(struct xenbus_device *dev)
+{
+	int rc = 0;
+
+	UK_ASSERT(dev != NULL);
+
+	return rc;
+}
+
+static int blkfront_drv_init(struct uk_alloc *allocator)
+{
+	/* driver initialization */
+	if (!allocator)
+		return -EINVAL;
+
+	drv_allocator = allocator;
+	return 0;
+}
+
+static const xenbus_dev_type_t blkfront_devtypes[] = {
+	xenbus_dev_vbd,
+};
+
+static struct xenbus_driver blkfront_driver = {
+	.device_types = blkfront_devtypes,
+	.init = blkfront_drv_init,
+	.add_dev = blkfront_add_dev
+};
+
+XENBUS_REGISTER_DRIVER(&blkfront_driver);
