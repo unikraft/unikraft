00bbf2cf50e87e28dd764f5c74615058fce69d34
Simon Kuenzer <simon.kuenzer@neclab.eu>
2020-02-09 17:03:34 +0100

    Release: 0.4 Rhea
    Release: 0.4 Rhea
    
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>
    Signed-off-by: Sharan Santhanam <sharan.santhanam@neclab.eu>
    Signed-off-by: Felipe Huici <felipe.huici@neclab.eu>

--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 UK_VERSION = 0
-UK_SUBVERSION = 3
-UK_EXTRAVERSION = 1
-UK_CODENAME = Iapetus
+UK_SUBVERSION = 4
+UK_EXTRAVERSION = 0
+UK_CODENAME = Rhea
 # https://en.wikipedia.org/wiki/Moons_of_Saturn (by discovery year)
