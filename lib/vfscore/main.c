6945fec0d817b30926cd11d8c16eb9241917daed
Vlad-Andrei BĂDOIU <vlad_andrei.badoiu@stud.acs.upb.ro>
2020-04-05 14:56:43 +0300

    lib/vfscore: Add scandir
    We adapt the scandir implementation for musl to work
    with our vfscore implementation.
    
    Signed-off-by: Vlad-Andrei Badoiu <vlad_andrei.badoiu@upb.ro>
    Reviewed-by: Felipe Huici <felipe.huici@neclab.eu>

--- lib/vfscore/main.c
+++ lib/vfscore/main.c
@@ -36,6 +36,7 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <string.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <vfscore/prex.h>
@@ -706,6 +707,53 @@ int closedir(DIR *dir)
 	close(dir->fd);
 	free(dir);
 	return 0;
+}
+
+int scandir(const char *path, struct dirent ***res,
+	int (*sel)(const struct dirent *),
+	int (*cmp)(const struct dirent **, const struct dirent **))
+{
+	DIR *d = opendir(path);
+	struct dirent *de, **names=0, **tmp;
+	size_t cnt=0, len=0;
+	int old_errno = errno;
+
+	if (!d)
+		return -1;
+
+	while ((errno=0), (de = readdir(d))) {
+		if (sel && !sel(de))
+			continue;
+		if (cnt >= len) {
+			len = 2*len+1;
+			if (len > SIZE_MAX/sizeof(*names))
+				break;
+			tmp = realloc(names, len * sizeof(*names));
+			if (!tmp)
+				break;
+			names = tmp;
+		}
+		names[cnt] = malloc(de->d_reclen);
+		if (!names[cnt])
+			break;
+		memcpy(names[cnt++], de, de->d_reclen);
+	}
+
+	closedir(d);
+
+	if (errno) {
+		if (names)
+			while (cnt-->0)
+				free(names[cnt]);
+		free(names);
+		return -1;
+	}
+	errno = old_errno;
+
+	if (cmp)
+		qsort(names, cnt, sizeof *names, (int (*)(const void *, const void *))cmp);
+	*res = names;
+	return cnt;
 }
 
 struct dirent *readdir(DIR *dir)
