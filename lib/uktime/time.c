2bb63ce73a8fff496cc410ee8ed75a616eac1458
Simon Kuenzer <simon.kuenzer@neclab.eu>
2021-02-17 15:00:20 +0100

    lib/uktime: Register 'setitimer' to syscall_shim
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

--- lib/uktime/time.c
+++ lib/uktime/time.c
@@ -195,8 +195,9 @@ UK_SYSCALL_R_DEFINE(int, times, struct tm *, buf)
 	return -ENOTSUP;
 }
 
-int setitimer(int which __unused, const struct itimerval *new_value __unused,
-		struct itimerval *old_value __unused)
+UK_SYSCALL_R_DEFINE(int, setitimer, int, which,
+		    const struct itimerval *, new_value,
+		    struct itimerval *, old_value)
 {
 	UK_WARN_STUBBED();
 	return 0;
