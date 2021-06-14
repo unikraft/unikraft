70b44687b2ce47a4ea023f4ff691e4521ac61d0d
Marc Rittinghaus <marc.rittinghaus@kit.edu>
2021-06-14 14:30:56 +0200

    plat/*/arm64: Use event-based trap interface
    The event-based trap interface makes architectural traps accessible as
    events (uk_event). This commit changes the arm64 platform code for kvm
    to raise the corresponding trap events defined in uk/arch/traps.h.
    
    Signed-off-by: Marc Rittinghaus <marc.rittinghaus@kit.edu>

--- plat/common/arm/traps.c
+++ plat/common/arm/traps.c
@@ -19,6 +19,10 @@
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <uk/arch/lcpu.h>
+#include <uk/arch/types.h>
+#include <arm/traps.h>
+#include <arm/cpu_defs.h>
 #include <stdint.h>
 #include <string.h>
 #include <uk/print.h>
@@ -31,6 +35,121 @@ static const char *exception_modes[]= {
 	"FIQ",
 	"Error"
 };
+
+enum aarch64_trap {
+	AARCH64_TRAP_INVALID_INSN,
+	AARCH64_TRAP_DEBUG,
+	AARCH64_TRAP_PAGE_FAULT,
+	AARCH64_TRAP_BUS_ERROR,
+	AARCH64_TRAP_MATH,
+	AARCH64_TRAP_SECURITY,
+
+	AARCH64_TRAP_MAX
+};
+
+/* Mapping of the fault status code (FSC) for instruction and data aborts to
+ * trap type (either page fault or bus error). Zero means invalid. The map
+ * takes 64 bytes but saves a ton of comparisons.
+ */
+#define NUM_ABORT_MAP_ENTRIES 64
+static const __u8 abort_map[NUM_ABORT_MAP_ENTRIES] = {
+	[ESR_ISS_ABRT_FSC_ADDR_L0]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_ADDR_L1]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_ADDR_L2]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_ADDR_L3]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_TRANS_L0]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_TRANS_L1]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_TRANS_L2]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_TRANS_L3]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_ACCF_L0]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_ACCF_L1]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_ACCF_L2]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_ACCF_L3]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_PERM_L0]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_PERM_L1]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_PERM_L2]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_PERM_L3]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_SYNC]			= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_TAG]			= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_SYNC_PT_LM1]		= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_PT_L0]		= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_PT_L1]		= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_PT_L2]		= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_PT_L3]		= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_ECC]		= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_ECC_PT_LM1]	= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_ECC_PT_L0]	= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_ECC_PT_L1]	= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_ECC_PT_L2]	= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_SYNC_ECC_PT_L3]	= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_ALIGN]		= AARCH64_TRAP_BUS_ERROR,
+	[ESR_ISS_ABRT_FSC_ADDR_LM1]		= AARCH64_TRAP_PAGE_FAULT,
+	[ESR_ISS_ABRT_FSC_TRANS_LM1]		= AARCH64_TRAP_PAGE_FAULT
+};
+
+DECLARE_TRAP_EVENT(TRAP_INVALID_INSN);
+DECLARE_TRAP_EVENT(TRAP_DEBUG);
+DECLARE_TRAP_EVENT(TRAP_PAGE_FAULT);
+DECLARE_TRAP_EVENT(TRAP_BUS_ERROR);
+DECLARE_TRAP_EVENT(TRAP_MATH);
+DECLARE_TRAP_EVENT(TRAP_SECURITY);
+
+static const struct {
+	const struct uk_event *event;
+	const char *str;
+	int signr;
+} _trap_table[AARCH64_TRAP_MAX] = {
+	{ UK_EVENT_PTR(TRAP_INVALID_INSN), "invalid insn",    4 /* SIGILL  */ },
+	{ UK_EVENT_PTR(TRAP_DEBUG),        "debug",           5 /* SIGTRAP */ },
+	{ UK_EVENT_PTR(TRAP_PAGE_FAULT),   "page fault",     11 /* SIGSEGV */ },
+	{ UK_EVENT_PTR(TRAP_BUS_ERROR),    "bus error",       7 /* SIGBUS  */ },
+	{ UK_EVENT_PTR(TRAP_MATH),         "floating point",  8 /* SIGFPE  */ },
+	{ UK_EVENT_PTR(TRAP_SECURITY),     "security",        4 /* SIGILL  */ }
+};
+
+static enum aarch64_trap esr_to_trap(unsigned long esr)
+{
+	__u8 fsc, trap;
+
+	/* We expect Unikraft to run in EL1. So do not catch traps from EL0. */
+	switch (ESR_EC_FROM(esr)) {
+	case ESR_EL1_EC_MMU_IABRT_EL1:
+	case ESR_EL1_EC_MMU_DABRT_EL1:
+		fsc = ESR_ISS_ABRT_FSC_FROM(ESR_ISS_FROM(esr));
+		UK_ASSERT(fsc < NUM_ABORT_MAP_ENTRIES);
+
+		trap = abort_map[fsc];
+		if (trap == 0)
+			break; /* Neither page fault, nor bus error */
+
+		return trap;
+
+	case ESR_EL1_EC_PC_ALIGN:
+	case ESR_EL1_EC_SP_ALIGN:
+		return AARCH64_TRAP_BUS_ERROR;
+
+	case ESR_EL1_EC_SVE_ASIMD_FP_ACC:
+	case ESR_EL1_EC_SVE_ACC:
+	case ESR_EL1_EC_FP64:
+		return AARCH64_TRAP_MATH;
+
+	case ESR_EL1_EC_UNKNOWN:
+	case ESR_EL1_EC_ILL:
+		return AARCH64_TRAP_INVALID_INSN;
+
+	case ESR_EL1_EC_BTI:
+	case ESR_EL1_EC_FPAC:
+		return AARCH64_TRAP_SECURITY;
+
+	case ESR_EL1_EC_BRK_EL1:
+	case ESR_EL1_EC_STEP_EL1:
+	case ESR_EL1_EC_WATCHP_EL1:
+	case ESR_EL1_EC_BRK64:
+		return AARCH64_TRAP_DEBUG;
+	}
+
+	return AARCH64_TRAP_MAX;
+}
 
 static void dump_registers(struct __regs *regs, uint64_t far)
 {
@@ -64,6 +183,14 @@ void invalid_trap_handler(struct __regs *regs, uint32_t el,
 
 void trap_el1_sync(struct __regs *regs, uint64_t far)
 {
+	struct ukarch_trap_ctx ctx = {regs, regs->esr_el1, 1, 0, far};
+	enum aarch64_trap trap = esr_to_trap(regs->esr_el1);
+
+	if (trap < AARCH64_TRAP_MAX) {
+		if (uk_raise_event_ptr(_trap_table[trap].event, &ctx))
+			return;
+	}
+
 	uk_pr_crit("Unikraft: EL1 sync trap caught\n");
 
 	dump_registers(regs, far);
