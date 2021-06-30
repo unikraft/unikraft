0c9409ab243b1ca7d80e6cbf43862af2f290afa3
Paul-Sebastian Ungureanu <ungureanupaulsebastian@gmail.com>
2021-06-30 23:20:22 +0000

    lib/ukvsockdev: Implement a primitive vsock UNIX-like interface
    Virtio vsock implements only stream sockets, therefore datagram
    related functions are not implemented.
    This interface is still in progress and requires additional work.
    It should be based on the UNIX socket implementation.

---
+++ lib/ukvsockdev/include/uk/vsock.h
@@ -0,0 +1,83 @@
+#ifndef __UK_VSOCK__
+#define __UK_VSOCK__
+
+#include <uk/list.h>
+#include <sys/types.h>
+#include <uk/mutex.h>
+#include <uk/semaphore.h>
+
+#define VSOCK_RING_BUFS		64
+
+#define VIRTIO_VSOCK_SHUTDOWN_RCV	1 << 0
+#define VIRTIO_VSOCK_SHUTDOWN_SEND	1 << 1
+
+#define VMADDR_CID_ANY			-1
+#define VMADDR_CID_HYPERVISOR	 0
+#define VMADDR_CID_LOCAL		 1
+#define VMADDR_CID_HOST			 2
+
+#define VMADDR_PORT_ANY			-1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum vsock_state {
+	VSOCK_STATE_INIT,
+	VSOCK_STATE_LISTEN,
+	VSOCK_STATE_ACCEPT,
+	VSOCK_STATE_WAIT_CONNECT,
+	VSOCK_STATE_CONNECTED
+};
+
+struct vsock_sockaddr {
+	unsigned long cid;
+	unsigned int port;
+};
+
+struct vsock_buf {
+	/* total capacity of the buffer */
+	int cap;
+	/* read pointer */
+	int head;
+	/* write pointer */
+	int tail;
+	/* actual data */
+	char *buf;
+	/* if buffer is full */
+	int full;
+};
+
+struct vsock_sock;
+struct vsock_sock {
+    int fd;
+	enum vsock_state state;
+    struct vsock_sockaddr local_addr;
+    struct vsock_sockaddr remote_addr;
+	
+	struct uk_mutex rx_mutex;
+	struct vsock_buf rxb;
+	struct uk_mutex tx_mutex;
+	struct vsock_buf txb;
+
+	struct uk_semaphore conn_sem;
+	int tx_cnt;
+	int buf_alloc;
+	int fwd_cnt;
+	UK_TAILQ_ENTRY(struct vsock_sock) _list;
+};
+
+UK_TAILQ_HEAD(vsock_sock_list, struct vsock_sock);
+
+struct vsock_handler {
+	struct uk_alloc *a;
+	int cnt;
+	int sock_list_initialized;
+	struct vsock_sock_list sock_list;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __UK_VSOCK__ */
--- lib/ukvsockdev/vsockdev.c
+++ lib/ukvsockdev/vsockdev.c
@@ -10,6 +10,7 @@
 #include <uk/ctors.h>
 #include <uk/arch/atomic.h>
 #include <uk/vsockdev.h>
+#include <uk/vsock.h>
 
 /*
  * Vsock device is different from the network device where we could have
@@ -141,13 +142,249 @@ static void _destroy_event_handler(struct uk_vsockdev_event_handler *h
 #endif
 }
 
-static int uk_vsockdev_rx_one(struct uk_vsockdev *dev, 
+static int uk_vsockdev_tx(struct vsock_sock *sock, struct uk_alloc *a,
+	enum virtio_vsock_op op, const void *buf, int len, int flags)
+{
+	int sz;
+	int rc;
+	int peer_free;
+	int ret = 0;
+	struct virtio_vsock_packet *packet;
+	struct uk_vsockdev *dev;
+	
+	dev = uk_vskdev;
+	
+	do {
+		int used;
+		sz = len < VIRTIO_VSOCK_BUF_DATA_SIZE ? len : VIRTIO_VSOCK_BUF_DATA_SIZE;
+
+		peer_free = sock->buf_alloc - (sock->tx_cnt - sock->fwd_cnt);
+		if (sz > peer_free) {
+			uk_vsockdev_tx(sock, a, VIRTIO_VSOCK_OP_CREDIT_REQUEST, NULL, 0, 0);
+			break;
+		}
+
+		packet = uk_calloc(a, 1, sizeof(struct virtio_vsock_packet));
+		packet->hdr.src_cid = sock->local_addr.cid;
+		packet->hdr.dst_cid = sock->remote_addr.cid;
+		packet->hdr.src_port = sock->local_addr.port;
+		packet->hdr.dst_port = sock->remote_addr.port;
+		packet->hdr.len = sz;
+		packet->hdr.type = VIRTIO_VSOCK_TYPE_STREAM;
+		packet->hdr.op = op;
+		packet->hdr.flags = 0;
+		packet->hdr.buf_alloc = sock->rxb.cap;
+		used = sock->rxb.tail - sock->rxb.head;
+		if (used < 0) {
+			used += sock->rxb.cap;
+		}
+		packet->hdr.fwd_cnt = sock->rxb.cap - used;
+		packet->data = uk_calloc(a, sz, 1);
+		memcpy(packet->data, buf, sz);
+
+		len -= sz;
+		sock->tx_cnt += sz;
+		rc = dev->tx_one(dev, NULL, packet);
+		if (unlikely(rc != 0))
+			return -1;
+
+		ret += sz;
+	} while (len > 0);
+
+	return ret;
+}
+
+void handle_vsock_connection_request(struct vsock_handler *vsh,
+	struct virtio_vsock_packet *pkt)
+{
+	struct vsock_sock *sock;
+	struct vsock_sock *new_sock;
+	int new_fd = 0;
+
+	UK_TAILQ_FOREACH(sock, &vsh->sock_list, _list) {
+		if (sock->state == VSOCK_STATE_ACCEPT && 
+			(sock->local_addr.port == pkt->hdr.dst_port || sock->local_addr.port == VMADDR_PORT_ANY)) {
+			int sz;
+			
+			sz = VIRTIO_VSOCK_RX_DATA_SIZE * VSOCK_RING_BUFS;
+			new_sock = uk_calloc(vsh->a, 1, sizeof(struct vsock_sock));
+
+			new_sock->rxb.buf = uk_calloc(vsh->a, sz, 1);
+			new_sock->rxb.cap = sz;
+			new_sock->txb.buf = uk_calloc(vsh->a, sz, 1);
+			new_sock->txb.cap = sz;
+
+		    new_sock->buf_alloc = VIRTIO_VSOCK_RX_DATA_SIZE * VSOCK_RING_BUFS;
+			new_sock->fd = ++vsh->cnt;
+			new_sock->state = VSOCK_STATE_CONNECTED;
+
+			new_sock->local_addr.cid = pkt->hdr.dst_cid;
+			new_sock->local_addr.port = pkt->hdr.dst_port;
+			new_sock->remote_addr.cid = pkt->hdr.src_cid;
+			new_sock->remote_addr.port = pkt->hdr.src_port;
+
+			uk_mutex_init(&new_sock->rx_mutex);
+			uk_mutex_init(&new_sock->tx_mutex);
+			uk_semaphore_init(&new_sock->conn_sem, 0);
+			
+			new_fd = new_sock->fd;
+			UK_TAILQ_INSERT_TAIL(&vsh->sock_list, new_sock, _list);
+			break;
+		}
+	}
+
+	if (!new_fd) {
+		uk_pr_err("Couldn't handle connection request\n");
+		return;
+	}
+
+	uk_semaphore_up(&sock->conn_sem);
+	uk_vsockdev_tx(new_sock, vsh->a, VIRTIO_VSOCK_OP_RESPONSE, NULL, 0, 0);
+}
+
+void handle_vsock_connection_resposne(struct vsock_handler *vsh,
+	struct virtio_vsock_packet *pkt)
+{
+	struct vsock_sock *sock;
+
+	UK_TAILQ_FOREACH(sock, &vsh->sock_list, _list) {
+		if (sock->state == VSOCK_STATE_WAIT_CONNECT &&
+			(sock->local_addr.port == pkt->hdr.dst_port || sock->local_addr.port == VMADDR_PORT_ANY)) {
+			break;
+		}
+	}
+
+	sock->remote_addr.port = pkt->hdr.src_port;
+	sock->state = VSOCK_STATE_CONNECTED;
+	uk_semaphore_up(&sock->conn_sem);
+}
+
+static void handle_vsock_connection_reset(struct vsock_handler *vsh,
+	struct virtio_vsock_packet *pkt)
+{
+	struct vsock_sock *sock;
+
+	UK_TAILQ_FOREACH(sock, &vsh->sock_list, _list) {
+		if (sock->local_addr.port == pkt->hdr.dst_port) {
+			break;
+		}
+	}
+
+	if (!sock) {
+		return;
+	}
+
+	UK_TAILQ_REMOVE(&vsh->sock_list, sock, _list);
+
+}
+
+static void handle_vsock_connection_shutdown(struct vsock_handler *vsh,
+	struct virtio_vsock_packet *pkt)
+{
+	struct vsock_sock *sock;
+
+	UK_TAILQ_FOREACH(sock, &vsh->sock_list, _list) {
+		if (sock->local_addr.port == pkt->hdr.dst_port) {
+			break;
+		}
+	}
+
+	if (!sock) {
+		return;
+	}
+
+	UK_TAILQ_REMOVE(&vsh->sock_list, sock, _list);
+}
+
+static void handle_vsock_data_recv(struct vsock_handler *vsh,
+	struct virtio_vsock_packet *pkt)
+{
+	int len;
+	struct vsock_sock *sock = NULL;
+
+	UK_TAILQ_FOREACH(sock, &vsh->sock_list, _list) {
+		if (sock->state == VSOCK_STATE_CONNECTED &&
+			sock->local_addr.port == pkt->hdr.dst_port) {
+			break;
+		}
+	}
+
+	if (!sock) {
+		uk_pr_err("Received vsock message, but socket not found\n");
+		return;
+	}
+
+	uk_mutex_lock(&sock->rx_mutex);
+	if (pkt->hdr.len + sock->rxb.tail >= sock->rxb.cap) {
+		/* overflow, must copy at the beginning of the buffer */
+		len = sock->rxb.cap - sock->rxb.tail;
+		memcpy(sock->rxb.buf + sock->rxb.tail, pkt->data, len);
+		memcpy(sock->rxb.buf, pkt->data + len, pkt->hdr.len - len);
+	} else {
+		/* fits perfectly */
+		memcpy(sock->rxb.buf + sock->rxb.tail, pkt->data, pkt->hdr.len);
+	}
+	sock->rxb.tail += pkt->hdr.len;
+
+	if (sock->rxb.tail >= sock->rxb.cap) {
+		sock->rxb.tail -= sock->rxb.cap;
+	}
+
+	if (sock->rxb.head == sock->rxb.tail) {
+		sock->rxb.full = 1;
+	}
+	uk_mutex_unlock(&sock->rx_mutex);
+}
+
+static void handle_vsock_credit_update(struct vsock_handler *vsh,
+	struct virtio_vsock_packet *pkt)
+{
+	struct vsock_sock *sock = NULL;
+
+	UK_TAILQ_FOREACH(sock, &vsh->sock_list, _list) {
+		if (sock->state == VSOCK_STATE_CONNECTED &&
+			sock->local_addr.port == pkt->hdr.dst_port) {
+			break;
+		}
+	}
+
+	if (!sock) {
+		return;
+	}
+
+	sock->buf_alloc = pkt->hdr.buf_alloc;
+	sock->fwd_cnt = pkt->hdr.fwd_cnt;
+}
+
+static void handle_vsock_credit_request(struct vsock_handler *vsh,
+	struct virtio_vsock_packet *pkt)
+{
+	struct vsock_sock *sock = NULL;
+
+	UK_TAILQ_FOREACH(sock, &vsh->sock_list, _list) {
+		if (sock->state == VSOCK_STATE_CONNECTED &&
+			sock->local_addr.port == pkt->hdr.dst_port) {
+			break;
+		}
+	}
+
+	if (!sock) {
+		return;
+	}
+
+	uk_vsockdev_tx(sock, vsh->a, VIRTIO_VSOCK_OP_CREDIT_REQUEST, NULL, 0, 0);
+}
+
+static void uk_vsockdev_rx_one(struct uk_vsockdev *dev, 
 			   uint16_t queue_id __unused, void *argp __unused)
 {
 	struct uk_alloc *a;
 	struct virtio_vsock_packet *pkt;
+	struct vsock_handler *vsh;
 	int rc = 0;
 
+	vsh = (struct vsock_handler *) argp;
+	
 	a = uk_alloc_get_default();
 	pkt = uk_calloc(a, 1, sizeof(struct virtio_vsock_packet));
 	pkt->data = uk_calloc(a, VIRTIO_VSOCK_RX_DATA_SIZE, sizeof(__u8));
@@ -160,7 +397,31 @@ static int uk_vsockdev_rx_one(struct uk_vsockdev *dev,
 	if (rc)
 		return;
 
-	return rc;
+	switch (pkt->hdr.op) {
+	case VIRTIO_VSOCK_OP_REQUEST:
+		handle_vsock_connection_request(vsh, pkt);
+		break;
+	case VIRTIO_VSOCK_OP_RESPONSE:
+		handle_vsock_connection_resposne(vsh, pkt);
+		break;
+	case VIRTIO_VSOCK_OP_RST:
+		handle_vsock_connection_reset(vsh, pkt);
+		break;
+	case VIRTIO_VSOCK_OP_SHUTDOWN:
+		handle_vsock_connection_shutdown(vsh, pkt);
+		break;
+	case VIRTIO_VSOCK_OP_RW:
+		handle_vsock_data_recv(vsh, pkt);
+		break;
+	case VIRTIO_VSOCK_OP_CREDIT_UPDATE:
+		handle_vsock_credit_update(vsh, pkt);
+		break;
+	case VIRTIO_VSOCK_OP_CREDIT_REQUEST:
+		handle_vsock_credit_request(vsh, pkt);
+		break;
+	default:
+		return;
+	}
 }
 
 int uk_vsockev_rxq_configure(struct uk_vsockdev *dev, uint16_t queue_id,
@@ -195,6 +456,242 @@ err_out:
 	return err;
 }
 
+struct vsock_handler vsh;
+
+int vsock_socket() {
+    struct vsock_sock *sock;
+	int sz;
+
+	sz = VIRTIO_VSOCK_RX_DATA_SIZE * VSOCK_RING_BUFS;
+
+    sock = uk_calloc(vsh.a, 1, sizeof(struct vsock_sock));
+    sock->rxb.buf = uk_calloc(vsh.a, sz, 1);
+	sock->rxb.cap = sz;
+    sock->txb.buf = uk_calloc(vsh.a, sz, 1);
+	sock->txb.cap = sz;
+
+    sock->buf_alloc = sz;
+
+	uk_mutex_init(&sock->rx_mutex);
+	uk_mutex_init(&sock->tx_mutex);
+	uk_semaphore_init(&sock->conn_sem, 0);
+
+    sock->fd = ++vsh.cnt;
+	UK_TAILQ_INSERT_TAIL(&vsh.sock_list, sock, _list);
+
+    return sock->fd;
+}
+
+struct vsock_sock *vsock_get(int fd)
+{
+	struct vsock_sock *sock;
+
+    UK_TAILQ_FOREACH(sock, &vsh.sock_list, _list) {
+        if (sock->fd == fd) {
+            return sock;
+        }
+	}
+
+    return NULL;
+}
+
+int vsock_close(int sockfd)
+{
+    struct vsock_sock *sock;
+
+    sock = vsock_get(sockfd);
+    if (sock == NULL) {
+        return -1;
+    }
+
+	uk_vsockdev_tx(sock, vsh.a, VIRTIO_VSOCK_OP_SHUTDOWN, NULL, 0,
+		VIRTIO_VSOCK_SHUTDOWN_RCV | VIRTIO_VSOCK_SHUTDOWN_SEND);
+	uk_vsockdev_tx(sock, vsh.a, VIRTIO_VSOCK_OP_RST, NULL, 0, 0);
+
+	UK_TAILQ_REMOVE(&vsh.sock_list, sock, _list);
+
+	return 0;
+}
+
+int vsock_bind(int sockfd, struct vsock_sockaddr *addr)
+{
+    struct vsock_sock *sock;
+
+    if (addr->cid != VMADDR_CID_ANY) {
+        return -1;   
+    }
+
+    sock = vsock_get(sockfd);
+    if (sock == NULL) {
+        errno = EBADF;
+        return -1;
+    }
+
+	addr->cid = uk_vskdev->dev_ops->get_cid(uk_vskdev);
+
+    sock->local_addr.cid = addr->cid;
+    sock->local_addr.port = addr->port;
+
+    return 0;
+}
+
+int vsock_listen(int sockfd, int backlog)
+{
+    struct vsock_sock *sk;
+    
+    sk = vsock_get(sockfd);
+    if (sk == NULL) {
+        return -1;
+    }
+    sk->state = VSOCK_STATE_LISTEN;
+
+    return 0;
+}
+
+int vsock_accept(int sockfd, struct vsock_sockaddr *addr)
+{
+    struct vsock_sock *sk;
+	struct vsock_sock *sock;
+
+    sk = vsock_get(sockfd);
+    if (sk == NULL) {
+        return -1;
+    }
+
+	sk->state = VSOCK_STATE_ACCEPT;
+	
+	uk_semaphore_down(&sk->conn_sem);
+	sock = UK_TAILQ_LAST(&vsh.sock_list, vsock_sock_list);
+	addr->cid = sock->remote_addr.cid;
+	addr->port = sock->remote_addr.port;
+
+    return sock->fd;
+}
+
+int vsock_connect(int sockfd, const struct vsock_sockaddr *addr)
+{
+	struct vsock_sock *sock;
+	
+	sock = vsock_get(sockfd);
+	if (!sock) {
+		return -1;
+	}
+
+	sock->remote_addr.cid = addr->cid;
+	sock->remote_addr.port = addr->port;
+
+	sock->state = VSOCK_STATE_WAIT_CONNECT;
+	uk_vsockdev_tx(sock, vsh.a, VIRTIO_VSOCK_OP_REQUEST, NULL, 0, 0);
+	uk_semaphore_down(&sock->conn_sem);
+
+    return 0;
+}
+
+ssize_t vsock_send(int sockfd, const void *buf, size_t len, int flags)
+{
+	int l;
+	int sz = 0;
+	unsigned long avail;
+	struct vsock_sock *sock;
+	
+	sock = vsock_get(sockfd);
+	if (!sock) {
+		return -1;
+	}
+
+	if (sock->txb.full)
+		return 0;
+
+	uk_mutex_lock(&sock->tx_mutex);
+	if (sock->txb.tail < sock->txb.head) {
+		avail = sock->txb.head - sock->txb.tail;
+	} else {
+		avail = sock->txb.cap - (sock->txb.tail - sock->txb.head);
+	}
+	if (len > avail) {
+		/* user tries to write more than available space */
+		sz = avail;
+	} else {
+		sz = len;
+	}
+
+	if (sock->txb.tail + sz >= sock->txb.cap) {
+		/* overflow, must copy at the beginning of the buffer */
+		l = sock->txb.cap - sock->txb.tail;
+		memcpy(sock->txb.buf + sock->txb.tail, buf, l);
+		memcpy(sock->txb.buf, buf, sz - l);
+	} else {
+		/* fits perfectly */
+		memcpy(sock->txb.buf + sock->txb.tail, buf, sz);
+	}
+
+	sock->txb.tail += sz;
+	if  (sock->txb.tail >= sock->txb.cap) {
+		sock->txb.tail -= sock->txb.cap; 
+	}
+
+	if (sock->txb.tail == sock->txb.head) {
+		sock->txb.full = 1;
+	}
+
+	uk_vsockdev_tx(sock, vsh.a, VIRTIO_VSOCK_OP_RW, buf, sz, 0);
+	uk_mutex_unlock(&sock->tx_mutex);
+    return sz;
+}
+
+ssize_t vsock_recv(int sockfd, void *buf, size_t len, int flags)
+{
+	int l;
+	int sz;
+	long avail;
+	struct vsock_sock *sock;
+	
+	sock = vsock_get(sockfd);
+	if (!sock) {
+		return -1;
+	}
+
+	uk_mutex_lock(&sock->rx_mutex);
+	avail = sock->rxb.tail - sock->rxb.head;
+	if (avail < 0) {
+		avail += sock->rxb.cap;
+	}
+
+	if (len > avail) {
+		/* the user required more data than possible */
+		sz = avail;
+	} else {
+		/* enough data for user */
+		sz = len;
+	}
+	if (sock->rxb.head + sz >= sock->rxb.cap) {
+		l = sock->rxb.cap - sock->rxb.head;
+		memcpy(buf, sock->rxb.buf + sock->rxb.head, l);
+		memcpy(buf + l, sock->rxb.buf, l - sz);
+	} else {
+		memcpy(buf, sock->rxb.buf + sock->rxb.head, sz);
+	}
+
+	sock->rxb.head += sz;
+	if (sock->rxb.head >= sock->rxb.cap) {
+		sock->rxb.head -= sock->rxb.cap;
+	}
+	sock->rxb.full = 0;
+	uk_mutex_unlock(&sock->rx_mutex);
+	
+    return sz;
+}
+
+void vsock_init() {
+    vsh.a = uk_alloc_get_default();
+    if (!vsh.sock_list_initialized) {
+        UK_TAILQ_INIT(&vsh.sock_list);
+        vsh.sock_list_initialized = 1;
+    }
+    vsh.cnt = 1;
+}
+
+
 int uk_vsockdev_init() {
 	int rc = 0;
 	struct uk_vsockdev_rxqueue_conf rx_conf;
@@ -207,10 +704,12 @@ int uk_vsockdev_init() {
 	UK_ASSERT(dev->dev_ops);
 	UK_ASSERT(dev->dev_ops->dev_start);
 
+	vsock_init();
 	a = uk_alloc_get_default();
 
 	rx_conf.a = a;
 	rx_conf.callback = uk_vsockdev_rx_one;
+	rx_conf.callback_cookie = &vsh;
 #ifdef CONFIG_LIBUKVSOCKDEV_DISPATCHERTHREADS
 	rxq_conf.s = uk_sched_get_default();
 	if (!rxq_conf.s)
