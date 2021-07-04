fd1110f2ef9caf97558d92ea7dd590bf9faca53f
Stefan Teodorescu <stefanl.teodorescu@gmail.com>
2021-07-04 22:29:40 +0200

    lib/posix-mmap: Add mmap/munmap functions
    Add mmap and munmap functions that allow creating and destroying memory
    mappings. For now, only anonymous (not backed by files) are supported.
    
    I have also introduced a very basic allocator, which searches linearly
    through the whole memory searching for an area large enough to map the
    requestd amount of virtual memory. Eventually, this implementation would
    have to use a proper allocator, for efficiency.
    
    Signed-off-by: Stefan Teodorescu <stefanl.teodorescu@gmail.com>

--- lib/posix-mmap/include/sys/mman.h
+++ lib/posix-mmap/include/sys/mman.h
@@ -33,4 +33,23 @@
 #ifndef __POSIX_MMAP__
 #define __POSIX_MMAP__
 
+#define MAP_FAILED	((void *) -1)
+
+#define PROT_NONE	0x0
+#define PROT_READ	0x1
+#define PROT_WRITE	0x2
+#define PROT_EXEC	0x4
+
+#define MAP_SHARED	0x1
+#define MAP_PRIVATE	0x2
+
+#define MAP_FIXED	0x10
+#define MAP_ANONYMOUS	0x20
+#define MAP_ANON	MAP_ANONYMOUS
+
+void *mmap(void *addr, size_t length, int prot, int flags,
+		   int fd, off_t offset);
+
+int munmap(void *addr, size_t length);
+
 #endif /* __POSIX_MMAP__ */
--- lib/posix-mmap/mm.c
+++ lib/posix-mmap/mm.c
@@ -30,5 +30,164 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <uk/bitmap.h>
+#include <uk/print.h>
+#include <uk/plat/mm.h>
+#include <uk/syscall.h>
+#include <uk/plat/common/mem_layout.h>
+
 #include <sys/mman.h>
 
+/*
+ * XXX
+ * This does for now a linear search, starting from |start|, looking for a
+ * memory area with |length| bytes (aligned to page size)
+ */
+static unsigned long get_free_virtual_area(unsigned long start, size_t length)
+{
+	unsigned long page;
+
+	if (!PAGE_ALIGNED(length))
+		return -1;
+
+	while (start <= MMAP_AREA_END - length) {
+		for (page = start; page < start + length; page += PAGE_SIZE) {
+			if (PAGE_PRESENT(uk_virt_to_pte(page)))
+				break;
+		}
+
+		if (page == start + length)
+			return start;
+
+		start = page + PAGE_SIZE;
+	}
+
+	return -1;
+}
+
+static int libc_to_internal_prot(int prot)
+{
+	int page_prot = PAGE_PROT_NONE;
+
+	if (prot & PROT_READ)
+		page_prot |= PAGE_PROT_READ;
+	if (prot & PROT_WRITE)
+		page_prot |= PAGE_PROT_WRITE;
+	if (prot & PROT_EXEC)
+		page_prot |= PAGE_PROT_EXEC;
+
+	return page_prot;
+}
+
+UK_SYSCALL_DEFINE(void*, mmap, void*, addr, size_t, len, int, prot,
+		int, flags, int, fildes, off_t, off)
+{
+	unsigned long page_addr = (unsigned long) addr;
+	unsigned long area_to_map, page_prot, page;
+	size_t i;
+
+	if (flags & MAP_ANONYMOUS) {
+		if (fd != -1 || offset) {
+			errno = EINVAL;
+			return MAP_FAILED;
+		}
+	} else {
+		/* TODO: We don't currently support mapping files */
+		errno = ENOTSUP;
+		return MAP_FAILED;
+	}
+
+
+	/* At least one of MAP_SHARED or MAP_PRIVATE has to be specified */
+	if (!(flags & MAP_SHARED) && !(flags & MAP_PRIVATE)) {
+		errno = EINVAL;
+		return MAP_FAILED;
+	}
+
+	if (!length) {
+		errno = EINVAL;
+		return MAP_FAILED;
+	}
+
+	length = PAGE_ALIGN_UP(length);
+	if (!length) {
+		errno = ENOMEM;
+		return MAP_FAILED;
+	}
+
+	if (flags & MAP_FIXED) {
+		/* Discard any overlapping mappings */
+		if (munmap(addr, length)) {
+			errno = EINVAL;
+			return MAP_FAILED;
+		}
+		page_addr = PAGE_ALIGN_UP(page_addr);
+	} else {
+		if ((void *) page_addr == NULL || page_addr < MMAP_AREA_START)
+			page_addr = MMAP_AREA_START;
+		else
+			page_addr = PAGE_ALIGN_UP(page_addr);
+	}
+
+	area_to_map = get_free_virtual_area(page_addr, length);
+	if (area_to_map == (unsigned long) -1) {
+		errno = ENOMEM;
+		return MAP_FAILED;
+	}
+
+	for (i = 0; i < length; i += PAGE_SIZE) {
+		page = area_to_map + i;
+		if (ukplat_page_map(ukplat_get_active_pt(), page,
+					PAGE_PADDR_ANY,
+					PAGE_PROT_READ | PAGE_PROT_WRITE, 0)) {
+			munmap((void *) area_to_map, length);
+			errno = ENOMEM;
+			return MAP_FAILED;
+		}
+	}
+
+	if (flags & MAP_ANONYMOUS) {
+		/* MAP_ANONYMOUS pages are zeroed out */
+		/*
+		 * XXX: there is a bug when building with performance
+		 * optimizations flag that make this memset loop infintely.
+		 * Using for loop for now.
+		 */
+		/* memset((void *) area_to_map, 0, length); */
+		for (i = 0; i < length / sizeof(unsigned long); i++)
+			*((unsigned long *) area_to_map + i) = 0;
+	} else {
+		/* TODO: file mapping */
+	}
+
+	page_prot = libc_to_internal_prot(prot);
+	for (page = area_to_map; page < area_to_map + length; page += PAGE_SIZE)
+		ukplat_page_set_prot(ukplat_get_active_pt(), page, page_prot);
+
+	return (void *) area_to_map;
+}
+
+UK_SYSCALL_DEFINE(int, munmap, void*, addr, size_t, len)
+{
+	unsigned long start = (unsigned long) addr;
+	unsigned long page;
+
+	if (!PAGE_ALIGNED(start)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (!length)
+		return 0;
+
+	length = PAGE_ALIGN_UP(length);
+	for (page = start; page < start + length; page += PAGE_SIZE)
+		ukplat_page_unmap(ukplat_get_active_pt(), page);
+
+	return 0;
+}
