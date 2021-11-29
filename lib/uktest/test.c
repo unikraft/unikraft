ea6354de17d86ef209edbb2ce813fdc637b7bdea
Alexander Jung <a.jung@lancs.ac.uk>
2021-11-29 19:11:58 +0100

    lib/uktest: Provide multi boot-level registration of test suites
    This commit introduces a minimal implementation testing infastructure into
    Unikraft.  The primary contents includes the definition of a test (or suite
    of tests) and the mechanism by which one can use to register the suite to
    be run.
    
    Checkpatch-Ignore: COMPLEX_MACRO
    Checkpatch-Ignore: MULTISTATEMENT_MACRO_USE_DO_WHILE
    Checkpatch-Ignore: MACRO_WITH_FLOW_CONTROL
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

--- lib/uktest/test.c
+++ lib/uktest/test.c
@@ -29,3 +29,119 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+
+#include <uk/test.h>
+#include <uk/list.h>
+#include <uk/config.h>
+#include <uk/essentials.h>
+
+/**
+ * Test suite, case and assertion statistics
+ */
+struct uk_test_stats {
+	unsigned int total;
+	unsigned int fail;
+	unsigned int success;
+};
+
+static struct uk_test_stats testsuite_stats = {0};
+static struct uk_test_stats testcase_stats = {0};
+static struct uk_test_stats testassert_stats = {0};
+
+
+static void
+_uk_test_compute_assert_stats(struct uk_testcase *esac,
+			      struct uk_test_stats *out)
+{
+	struct uk_assert *assert;
+
+	UK_ASSERT(esac);
+	UK_ASSERT(out);
+
+	out->total = 0;
+	out->fail = 0;
+	out->success = 0;
+
+	uk_test_assert_foreach(esac, assert) {
+		out->total++;
+
+		switch (assert->status) {
+		case UK_TEST_ASSERT_SUCCESS:
+			out->success++;
+			break;
+		case UK_TEST_ASSERT_FAIL:
+			out->fail++;
+			break;
+		}
+	}
+}
+
+int
+uk_testsuite_run(struct uk_testsuite *suite)
+{
+	int ret = 0;
+	struct uk_testcase *esac;
+	struct uk_test_stats stats;
+
+	UK_ASSERT(suite);
+
+	/* Increase the number of recognized test suites. */
+	testsuite_stats.total++;
+
+	if (suite->init) {
+		ret = suite->init(suite);
+
+		if (ret != 0) {
+			uk_pr_err("Could not initialize test suite: %s\n",
+				  suite->name);
+			goto ERR_EXIT;
+		}
+	}
+
+	uk_testsuite_case_foreach(suite, esac) {
+		/* Increase the number of recognized test cases. */
+		testcase_stats.total++;
+
+#ifdef CONFIG_LIBUKTEST_LOG_TESTS
+		printf(LVLC_TESTNAME "test:" UK_ANSI_MOD_RESET
+		       " %s->%s",
+		       suite->name,
+		       esac->name);
+		if (esac->desc != NULL)
+			printf(": %s\n", esac->desc);
+		else
+			printf("\n");
+#endif /* CONFIG_LIBUKTEST_LOG_TESTS */
+
+		esac->func(esac);
+
+		_uk_test_compute_assert_stats(esac, &stats);
+
+		testassert_stats.total += stats.total;
+		if (stats.fail > 0) {
+			suite->failed_cases++;
+			testcase_stats.fail++;
+			testassert_stats.fail += stats.fail;
+		}
+		if (stats.success > 0) {
+			testcase_stats.success++;
+			testassert_stats.success += stats.success;
+		}
+	}
+
+	if (suite->failed_cases > 0)
+		testsuite_stats.fail++;
+	else
+		testsuite_stats.success++;
+
+EXIT:
+	return ret;
+
+ERR_EXIT:
+	testsuite_stats.fail++;
+	goto EXIT;
+}
