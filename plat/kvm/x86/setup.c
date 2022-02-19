c0d9e5fd8933b2bfacb9f1fd3cf9a243a7e71ba9
cristian-vijelie <cristianvijelie@gmail.com>
2022-02-19 11:41:40 +0200

    plat/kvm/x86: Add support for Multiboot v2
    Signed-off-by: cristian-vijelie <cristianvijelie@gmail.com>

--- plat/kvm/x86/setup.c
+++ plat/kvm/x86/setup.c
@@ -34,8 +34,13 @@
 #include <kvm/console.h>
 #include <kvm/intctrl.h>
 #include <kvm-x86/bootinfo.h>
+#ifndef CONFIG_UKPLAT_HAVE_MULTIBOOT2
 #include <kvm-x86/multiboot.h>
 #include <kvm-x86/multiboot_defs.h>
+#else
+#include <kvm-x86/multiboot2.h>
+#include <kvm-x86/multiboot2_defs.h>
+#endif /* CONFIG_UKPLAT_HAVE_MULTIBOOT2 */
 #include <uk/arch/limits.h>
 #include <uk/arch/types.h>
 #include <uk/plat/console.h>
@@ -55,6 +60,7 @@ struct uk_bootinfo bootinfo = { 0 };
 extern void _libkvmplat_newstack(uintptr_t stack_start, void (*tramp)(void *),
 				 void *arg);
 
+#ifndef CONFIG_UKPLAT_HAVE_MULTIBOOT2
 static void _convert_mbinfo(struct multiboot_info *mi)
 {
 	multiboot_memory_map_t *m;
@@ -86,7 +92,7 @@ static void _convert_mbinfo(struct multiboot_info *mi)
 	bootinfo.max_addr = m->addr + m->len;
 	if (bootinfo.max_addr > PLATFORM_MAX_MEM_ADDR)
 		bootinfo.max_addr = PLATFORM_MAX_MEM_ADDR;
-	UK_ASSERT((size_t) __END <= bootinfo.max_addr);
+	UK_ASSERT((size_t)__END <= bootinfo.max_addr);
 
 	/*
 	 * Reserve space for boot stack at the end of found memory
@@ -114,17 +120,79 @@ static void _convert_mbinfo(struct multiboot_info *mi)
 
 	if (mod1->mod_end == mod1->mod_start) {
 		uk_pr_debug("Ignoring empty initrd\n");
-		bootinfo.initrd_start = 0;
-		bootinfo.initrd_end = 0;
+		bootinfo.initrd_start  = 0;
+		bootinfo.initrd_end    = 0;
 		bootinfo.initrd_length = 0;
-		bootinfo.has_initrd = 0;
+		bootinfo.has_initrd    = 0;
 	} else {
-		bootinfo.initrd_start = mod1->mod_start;
-		bootinfo.initrd_end = mod1->mod_end;
+		bootinfo.initrd_start  = mod1->mod_start;
+		bootinfo.initrd_end    = mod1->mod_end;
 		bootinfo.initrd_length = mod1->mod_end - mod1->mod_start;
-		bootinfo.has_initrd = 1;
+		bootinfo.has_initrd    = 1;
 	}
 }
+#else
+static void _convert_mbitags(unsigned long addr)
+{
+	struct multiboot_tag *tag;
+	multiboot_memory_map_t *mmap;
+
+	for (tag = (struct multiboot_tag *)(addr + 8);
+	     tag->type != MULTIBOOT_TAG_TYPE_END;
+	     tag = (struct multiboot_tag *)((multiboot_uint8_t *)tag
+					    + ((tag->size + 7) & ~7))) {
+		uk_pr_debug("Tag 0x%x, Size 0x%x\n", tag->type, tag->size);
+		switch (tag->type) {
+		case MULTIBOOT_TAG_TYPE_CMDLINE:
+			uk_pr_debug("Cmdline detected: %p\n", tag);
+			bootinfo.u64_cmdline =
+			    (__u64)((struct multiboot_tag_string *)tag)->string;
+			break;
+		case MULTIBOOT_TAG_TYPE_MODULE:
+			if (!bootinfo.has_initrd) {
+				uk_pr_debug("Initrd found: %p\n", tag);
+				bootinfo.has_initrd = 1;
+				bootinfo.initrd_start =
+				    ((struct multiboot_tag_module *)tag)
+					->mod_start;
+				bootinfo.initrd_end =
+				    ((struct multiboot_tag_module *)tag)
+					->mod_end;
+				bootinfo.initrd_length =
+				    bootinfo.initrd_end - bootinfo.initrd_start;
+			} else
+				uk_pr_debug("Additional module found at %p, "
+					    "cmdline: %s\n",
+					    tag,
+					    ((struct multiboot_tag_module *)tag)
+						->cmdline);
+			break;
+		case MULTIBOOT_TAG_TYPE_MMAP:
+			for (mmap = ((struct multiboot_tag_mmap *)tag)->entries;
+			     (multiboot_uint8_t *)mmap
+			     < (multiboot_uint8_t *)tag + tag->size;
+			     mmap = (multiboot_memory_map_t
+					 *)((unsigned long)mmap
+					    + ((struct multiboot_tag_mmap *)tag)
+						  ->entry_size))
+				if (mmap->addr == PLATFORM_MEM_START
+				    && mmap->type == MULTIBOOT_MEMORY_AVAILABLE)
+					break;
+
+			bootinfo.max_addr = mmap->addr + mmap->len;
+
+			if (bootinfo.max_addr > PLATFORM_MAX_MEM_ADDR)
+				bootinfo.max_addr = PLATFORM_MAX_MEM_ADDR;
+			UK_ASSERT(bootinfo.max_addr >= (size_t)__END);
+
+			if ((bootinfo.max_addr - mmap->addr) < __STACK_SIZE)
+				UK_CRASH("Not enough memory to allocate boot "
+					 "stack\n");
+			break;
+		}
+	}
+}
+#endif /* CONFIG_UKPLAT_HAVE_MULTIBOOT2 */
 
 static inline void _get_cmdline(struct uk_bootinfo *bi)
 {
@@ -149,12 +217,12 @@ static inline void _get_cmdline(struct uk_bootinfo *bi)
 static inline void _init_mem(struct uk_bootinfo *bi)
 {
 	_libkvmplat_cfg.heap.start = ALIGN_UP((uintptr_t)__END, __PAGE_SIZE);
-	_libkvmplat_cfg.heap.end = (uintptr_t)bi->max_addr - __STACK_SIZE;
-	_libkvmplat_cfg.heap.len =
+	_libkvmplat_cfg.heap.end   = (uintptr_t)bi->max_addr - __STACK_SIZE;
+	_libkvmplat_cfg.heap.len   =
 	    _libkvmplat_cfg.heap.end - _libkvmplat_cfg.heap.start;
 	_libkvmplat_cfg.bstack.start = _libkvmplat_cfg.heap.end;
-	_libkvmplat_cfg.bstack.end = bi->max_addr;
-	_libkvmplat_cfg.bstack.len = __STACK_SIZE;
+	_libkvmplat_cfg.bstack.end   = bi->max_addr;
+	_libkvmplat_cfg.bstack.len   = __STACK_SIZE;
 }
 
 static inline void _init_initrd(struct uk_bootinfo *bi)
@@ -202,7 +270,7 @@ static inline void _init_initrd(struct uk_bootinfo *bi)
 			/* End of initrd within heap range;
 			 * Use the remaining left piece as heap */
 			heap1_start = ALIGN_UP(_libkvmplat_cfg.initrd.end,
-				      	       __PAGE_SIZE);
+					       __PAGE_SIZE);
 			heap1_end   = _libkvmplat_cfg.heap.end;
 		}
 	} else {
@@ -255,7 +323,8 @@ static inline void _init_initrd(struct uk_bootinfo *bi)
 	 * places the initrd close to the beginning of the heap region. One need
 	 * to assign just more memory in order to avoid this crash.
 	 */
-	if (RANGE_OVERLAP(_libkvmplat_cfg.heap.start, _libkvmplat_cfg.heap.len,
+	if (RANGE_OVERLAP(_libkvmplat_cfg.heap.start,
+			  _libkvmplat_cfg.heap.len,
 			  _libkvmplat_cfg.initrd.start,
 			  _libkvmplat_cfg.initrd.len))
 		UK_CRASH("Not enough space at end of memory for boot stack\n");
@@ -279,17 +348,35 @@ static void _libkvmplat_entry2(void *arg __attribute__((unused)))
 
 void _libkvmplat_entry(void *arg)
 {
+#ifndef CONFIG_UKPLAT_HAVE_MULTIBOOT2
 	struct multiboot_info *mi = (struct multiboot_info *)arg;
+#else
+	unsigned long ulong_arg = (unsigned long)arg;
+	unsigned mbi_size;
+#endif /* CONFIG_UKPLAT_HAVE_MULTIBOOT2 */
 
 	_init_cpufeatures();
 	_libkvmplat_init_console();
 	traps_init();
 	intctrl_init();
 
+#ifndef CONFIG_UKPLAT_HAVE_MULTIBOOT2
 	uk_pr_info("Entering from KVM (x86)...\n");
 	uk_pr_info("     multiboot: %p\n", mi);
 
 	_convert_mbinfo(mi);
+#else
+	if (ulong_arg & 7)
+		UK_CRASH("Unaligned mbi: 0x%lx\n", ulong_arg);
+
+	mbi_size = *(unsigned *)ulong_arg;
+
+	uk_pr_info("Entering from KVM (x86)...\n");
+	uk_pr_info("     multiboot v2: 0x%lx\n", ulong_arg);
+	uk_pr_info("	 Announced mbi size: 0x%x\n", mbi_size);
+
+	_convert_mbitags(ulong_arg);
+#endif /* CONFIG_UKPLAT_HAVE_MULTIBOOT2 */
 
 	/*
 	 * The multiboot structures may be anywhere in memory, so take a copy of
