426fa1c2be05e42a61d26292ee567d3ab681caba
samoilescusebastian <samoilescusebastian@gmail.com>
2022-04-07 15:40:56 +0300

    lib/ukswrand: remove ukcpuid llibrary from this PR

--- lib/ukswrand/rdrand.c
+++ lib/ukswrand/rdrand.c
@@ -3,16 +3,13 @@
 #include <string.h>
 
 #include <uk/swrand.h>
-#include <uk/cpuid.h>
-
+#include "rdrand.h"
 
 int has_amd_cpu(void)
 {
-	struct CPUIDinfo info;
+	struct CPUID_info info;
 
-	if (get_cpuid_info(&info, 0x00, 0x00)) {
-		return 0;
-	}
+	cpuid_macro(info.EAX, info.EBX, info.ECX, info.EDX, 0x00, 0x00);
 
 	return (memcmp((char *) (&info.EBX), "htuA", 4) == 0
 		&& memcmp((char *) (&info.EDX), "itne", 4) == 0
@@ -22,11 +19,9 @@ int has_amd_cpu(void)
 
 int has_intel_cpu(void)
 {
-	struct CPUIDinfo info;
+	struct CPUID_info info;
 
-	if (get_cpuid_info(&info, 0x00, 0x00)) {
-		return 0;
-	}
+	cpuid_macro(info.EAX, info.EBX, info.ECX, info.EDX, 0x00, 0x00);
 
 	return (memcmp((char *) (&info.EBX), "Genu", 4) == 0
 		&& memcmp((char *) (&info.EDX), "ineI", 4) == 0
@@ -35,40 +30,36 @@ int has_intel_cpu(void)
 
 int has_RDRAND(void)
 {
-	struct CPUIDinfo info;
+	struct CPUID_info info;
 	static const unsigned int RDRAND_FLAG = (1 << 30);
 
 	if (!(has_amd_cpu() || has_intel_cpu())) {
 		return 0;
 	}
 
-	if (get_cpuid_info(&info, 0x01, 0x00)) {
-		return 0;
-	}
+	cpuid_macro(info.EAX, info.EBX, info.ECX, info.EDX, 0x00, 0x00);
 
 	return (info.ECX & RDRAND_FLAG) == RDRAND_FLAG;
 }
 
-ssize_t RDRAND_bytes(unsigned char *buf, size_t buflen)
+int RDRAND_bytes(unsigned char *buf, size_t buflen)
 {
+	size_t idx = 0, rem = buflen;
+	size_t safety = buflen / sizeof(unsigned int) + 4;
+	u_int8_t error_code;
+	unsigned int val;
+
 	if (!has_RDRAND()) {
 		return -1;
 	}
 
-	size_t idx = 0, rem = buflen;
-	size_t safety = buflen / sizeof(unsigned int) + 4;
-
-	unsigned int val;
-
 	while (rem > 0 && safety > 0) {
-		char rc;
-
 		__asm__ volatile(
 			"rdrand %0 ; setc %1"
-			: "=r" (val), "=qm" (rc)
+			: "=r" (val), "=qm" (error_code)
 		);
 
-		if (rc) {
+		if (error_code) {
 			size_t cnt = (rem < sizeof(val) ? rem : sizeof(val));
 
 			memcpy(buf + idx, &val, cnt);
@@ -82,5 +73,5 @@ ssize_t RDRAND_bytes(unsigned char *buf, size_t buflen)
 
 	*((volatile unsigned int*)&val) = 0;
 
-	return (ssize_t)rem;
+	return (ssize_t)(buflen - rem);
 }
