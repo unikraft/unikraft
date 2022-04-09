cac8b54ecbdfc71485bd8ab051ef2ce15ff9fff0
Simon Kuenzer <simon.kuenzer@neclab.eu>
2022-04-09 03:32:11 +0200

    lib/posix-process: First clone syscall version (ongoing)
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

---
+++ lib/posix-process/clone.c
@@ -0,0 +1,145 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Simon Kuenzer <simon.kuenzer@neclab.eu>
+ *
+ * Copyright (c) 2022, NEC Laboratories Europe GmbH, NEC Corporation.
+ *                     All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/prctl.h>
+#include <sys/resource.h>
+#include <uk/process.h>
+#include <uk/print.h>
+#include <uk/syscall.h>
+#include <uk/arch/limits.h>
+#include <uk/sched.h>
+
+#include "process.h"
+
+/* NOTE: From man pages about clone(2)
+ *       [https://man7.org/linux/man-pages/man2/clone.2.html]:
+ *       "The raw clone() system call corresponds more closely to fork(2)
+ *        in that execution in the child continues from the point of the
+ *        call.  As such, the fn and arg arguments of the clone() wrapper
+ *        function are omitted.
+ *
+ *        In contrast to the glibc wrapper, the raw clone() system call
+ *        accepts NULL as a stack argument (and clone3() likewise allows
+ *        cl_args.stack to be NULL).  In this case, the child uses a
+ *        duplicate of the parent's stack.  (Copy-on-write semantics ensure
+ *        that the child gets separate copies of stack pages when either
+ *        process modifies the stack.)  In this case, for correct
+ *        operation, the CLONE_VM option should not be specified.  (If the
+ *        child shares the parent's memory because of the use of the
+ *        CLONE_VM flag, then no copy-on-write duplication occurs and chaos
+ *        is likely to result.)
+ *
+ *        The order of the arguments also differs in the raw system call,
+ *        and there are variations in the arguments across architectures,
+ *        as detailed in the following paragraphs.
+ *
+ *        The raw system call interface on x86-64 and some other
+ *        architectures (including sh, tile, and alpha) is:
+ *
+ *            long clone(unsigned long flags, void *stack,
+ *                       int *parent_tid, int *child_tid,
+ *                       unsigned long tls);
+ *
+ *        On x86-32, and several other common architectures (including
+ *        score, ARM, ARM 64, PA-RISC, arc, Power PC, xtensa, and MIPS),
+ *        the order of the last two arguments is reversed:
+ *
+ *            long clone(unsigned long flags, void *stack,
+ *                       int *parent_tid, unsigned long tls,
+ *                       int *child_tid);
+ *       "
+ */
+static void _clone_child_gc(struct uk_thread *t)
+{
+	if (t->name) {
+		free(DECONST(char *, t->name));
+		t->name = NULL;
+	}
+}
+
+#if CONFIG_ARCH_X86_64
+UK_LLSYSCALL_R_DEFINE(int, clone,
+		      unsigned long, flags,
+		      void *, sp,
+		      int *, parent_tid,
+		      int *, child_tid,
+		      unsigned long, tlsp)
+#else /* !CONFIG_ARCH_X86_64 */
+UK_LLSYSCALL_R_DEFINE(int, clone,
+		      unsigned long, flags,
+		      void *, sp,
+		      int *, parent_tid,
+		      unsigned long, tlsp,
+		      int *, child_tid)
+#endif /* !CONFIG_ARCH_X86_64 */
+{
+	struct uk_thread *t;
+	struct uk_sched *s;
+	struct uk_thread *child = NULL;
+
+	t = uk_thread_current();
+	s = uk_sched_current();
+
+	UK_ASSERT(s);
+	UK_ASSERT(uk_syscall_return_addr());
+
+	child = uk_thread_create_container(s->allocator,
+					   NULL, 0, /* no stack */
+					   s->allocator, /* UKTLS, TODO: TLS allocator */
+					   false,
+					   (t->name) ? strdup(t->name) : NULL,
+					   NULL,
+					   _clone_child_gc);
+	if (!child)
+		return -EAGAIN;
+
+	/*
+	 * Child starts at return address, sets given stack and given TLS:
+	 * It starts at userland context...
+	 */
+	ukarch_ctx_init_entry0(&child->ctx,
+			       (__uptr) sp,
+			       false,
+			       (ukarch_ctx_entry0) uk_syscall_return_addr());
+	child->tlsp = (__uptr) tlsp;
+	child->flags |= UK_THREADF_RUNNABLE;
+
+	uk_sched_thread_add(s, child);
+	uk_sched_yield(); /* Enable the child to execute directly */
+
+	return ukthread2tid(child);
+}
