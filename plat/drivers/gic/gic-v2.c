a10a19b6c335c88937d7eba9b8d14d1ac5c9f1b2
Marc Rittinghaus <marc.rittinghaus@kit.edu>
2022-06-17 11:37:35 +0200

    plat/drivers/gic: Fix format specifiers in dbg msg
    The __lcpuid type changed from __u32 to __u64. This commit fixes the
    warning in the GIC-v2 and GIC-v3 implementation.
    
    Signed-off-by: Marc Rittinghaus <marc.rittinghaus@kit.edu>
    Reviewed-by: Renê de Souza Pinto <rene@renesp.com.br>
    Approved-by: Renê de Souza Pinto <rene@renesp.com.br>
    Tested-by: Unikraft CI <monkey@unikraft.io>
    GitHub-Closes: #478

--- plat/drivers/gic/gic-v2.c
+++ plat/drivers/gic/gic-v2.c
@@ -365,9 +365,9 @@ static void gicv2_handle_irq(void)
 		irq = stat & GICC_IAR_INTID_MASK;
 
 #ifndef CONFIG_HAVE_SMP
-		uk_pr_debug("EL1 IRQ#%d trap caught\n", irq);
+		uk_pr_debug("EL1 IRQ#%"__PRIu32" caught\n", irq);
 #else /* !CONFIG_HAVE_SMP */
-		uk_pr_debug("Core %d: EL1 IRQ#%d trap caught\n",
+		uk_pr_debug("Core %"__PRIu64": EL1 IRQ#%"__PRIu32" caught\n",
 			    ukplat_lcpu_id(), irq);
 #endif /* CONFIG_HAVE_SMP */
 
