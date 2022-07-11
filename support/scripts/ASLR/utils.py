49b63d4b9ca7dd41047f96293cb0c1193b19e8b9
Terry Loslever <terrylos@terrylos.voo.be>
2022-07-11 08:36:42 +0200

    update header

--- support/scripts/ASLR/utils.py
+++ support/scripts/ASLR/utils.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python
+
+# Copyright (c) 2019, NEC Laboratories Europe GmbH, NEC Corporation.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+
 def check_file(elfFile):
 
 	if len(elfFile) > 4 and elfFile[1:4] == b'ELF':
