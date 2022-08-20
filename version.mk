3a997c16983039ea5d32704b6c20f26c3666764b
Alexander <alex@unikraft.org>
2022-08-20 19:54:34 +0000

    Release: v0.10.0 Phoebe
    Signed-off-by: Marc Rittinghaus <marc@unikraft.org>
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.org>
    Signed-off-by: Simon Kuenzer <simon@unikraft.org>
    Signed-off-by: Alexander Jung <alex@unikraft.org>
    Signed-off-by: Felipe Huici <felipe@unikraft.org>

--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 UK_VERSION = 0
-UK_SUBVERSION = 9
+UK_SUBVERSION = 10
 UK_EXTRAVERSION = 0
-UK_CODENAME = Hyperion
+UK_CODENAME = Phoebe
 # https://en.wikipedia.org/wiki/Moons_of_Saturn (by discovery year)
