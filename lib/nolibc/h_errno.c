87d01058d13444344c22793ede3c7d36bac6c402
Razvan Deaconescu <razvand@unikraft.io>
2022-11-23 23:50:58 +0200

    lib/nolibc: Add definition of h_errno
    LWIP has been updated to work with Musl. Headers and source code files
    from LWIP have been removed since they are part of Musl.
    
    In order to keep nolibc working with LWIP, previous items part of LWIP
    now have to be part of nolibc.
    
    This commit adds the definition of the `h_errno` variable.  It had been
    previously defined in LWIP, but has now been removed to make it
    compatible with Musl.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

---
+++ lib/nolibc/h_errno.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Razvan Deaconescu <razvand@unikraft.io>
+ *
+ * Copyright (c) 2022, Unikraft GmbH. All rights reserved.
+ */
+
+#include <netdb.h>
+
+int h_errno;
---
+++ lib/nolibc/include/netdb.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Razvan Deaconescu <razvand@unikraft.io>
+ *
+ * Copyright (c) 2022, Unikraft GmbH. All rights reserved.
+ */
+
+#ifndef __NETDB_H__
+#define __NETDB_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <uk/config.h>
+
+extern int h_errno;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __NETDB_H__ */
