cb863223411526457fb0a05774554da6526b9416
Alexander Jung <alex@unikraft.io>
2022-12-02 01:39:55 +0000

    Release: v0.11.0 Janus
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>
    Signed-off-by: Marc Rittinghaus <marc@unikraft.io>
    Signed-off-by: Simon Kuenzer <simon@unikraft.io>
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 UK_VERSION = 0
-UK_SUBVERSION = 10
+UK_SUBVERSION = 11
 UK_EXTRAVERSION = 0
-UK_CODENAME = Phoebe
+UK_CODENAME = Janus
 # https://en.wikipedia.org/wiki/Moons_of_Saturn (by discovery year)
