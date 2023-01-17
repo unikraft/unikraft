8cd52c8acb60977fd6921e04968e2bf7d942e704
Marc Rittinghaus <marc.rittinghaus@unikraft.io>
2023-01-17 17:10:57 +0100

    include/uk/plat/common: Add memregion list
    This commit adds a platform internal memory region list type based
    on the public memory region descriptor. The list is effectively a
    pre-allocated array that can be filled up to its capacity.
    Non-overlapping regions in the list are sorted in ascending order
    according to the physical address of the memory regions.
    
    Signed-off-by: Marc Rittinghaus <marc.rittinghaus@unikraft.io>
    Reviewed-by: Marco Schlumpp <marco@unikraft.io>
    Reviewed-by: Sergiu Moga <sergiu.moga@protonmail.com>
    Approved-by: Marco Schlumpp <marco@unikraft.io>
    Tested-by: Unikraft CI <monkey@unikraft.io>
    GitHub-Closes: #722

--- plat/common/include/uk/plat/common/memory.h
+++ plat/common/include/uk/plat/common/memory.h
@@ -33,6 +33,108 @@
 #ifndef __PLAT_CMN_MEMORY_H__
 #define __PLAT_CMN_MEMORY_H__
 
+#include <uk/config.h>
+#include <uk/assert.h>
+#include <uk/essentials.h>
+#include <uk/arch/types.h>
+#include <uk/plat/memory.h>
+
+#include <errno.h>
+#include <string.h>
+
+struct ukplat_memregion_list {
+	/** Maximum regions in the list */
+	__u32 capacity;
+	/** Current number of regions in the list */
+	__u32 count;
+	/** Array of memory regions */
+	struct ukplat_memregion_desc mrds[];
+} __packed __align(__SIZEOF_LONG__);
+
+/**
+ * Initializes the fields of an empty memory region list.
+ *
+ * @param list
+ *   The list to initialize
+ * @param capacity
+ *   The number of memory region descriptors that the list can hold. This
+ *   number must be set according to the amount of allocated memory.
+ */
+static inline void
+ukplat_memregion_list_init(struct ukplat_memregion_list *list, __u32 capacity)
+{
+	UK_ASSERT(capacity >= 1);
+
+	list->capacity = capacity;
+	list->count    = 0;
+}
+
+/**
+ * Inserts the region into the memory region list. The list is kept in
+ * ascending order of physical base addresses. Order is only preserved among
+ * ranges that do not overlap.
+ *
+ * @param list
+ *   The memory region list to insert the range into
+ * @param mrd
+ *   The memory range to insert
+ *
+ * @return
+ *   The index of the element added on success, an negative errno otherwise
+ */
+static inline int
+ukplat_memregion_list_insert(struct ukplat_memregion_list *list,
+			     const struct ukplat_memregion_desc *mrd)
+{
+	struct ukplat_memregion_desc *p;
+	__u32 i;
+
+	if (unlikely(list->count == list->capacity))
+		return -ENOMEM;
+
+	/* We start from the tail as it is more likely that we add memory
+	 * regions in semi-sorted order.
+	 */
+	p = &list->mrds[list->count];
+	for (i = list->count; i > 0; i--) {
+		--p;
+
+		if (p->pbase + p->len <= mrd->pbase) {
+			++p;
+			break;
+		}
+	}
+
+	memmove(p + 1, p, sizeof(*p) * (list->count - i));
+
+	*p = *mrd;
+	list->count++;
+	return (int)i;
+}
+
+/**
+ * Delete the specified region from the memory list.
+ *
+ * @param list
+ *   The memory region list from which to delete the region
+ * @param idx
+ *   The index of the region to delete
+ */
+static inline void
+ukplat_memregion_list_delete(struct ukplat_memregion_list *list, __u32 idx)
+{
+	struct ukplat_memregion_desc *p;
+
+	UK_ASSERT(idx < list->count);
+
+	if (idx != list->count - 1) {
+		p = &list->mrds[idx];
+		memmove(p, p + 1, sizeof(*p) * (list->count - idx));
+	}
+
+	list->count--;
+}
+
 /**
  * Initializes the platform memory mappings which require an allocator. This
  * function must always be called after initializing a memory allocator and
