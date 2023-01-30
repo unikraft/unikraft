818f01132c79cb03fc93dfedba81d684e3777a77
Simon Kuenzer <simon@unikraft.io>
2023-01-30 03:51:50 +0100

    lib/ukstreambuf: Unit tests: Append operations for C-strings
    Registers `libuktest` unit tests for append operations of C-strings to a
    streambuf object.
    
    Signed-off-by: Simon Kuenzer <simon@unikraft.io>

--- lib/ukstreambuf/tests/test_ukstreambuf.c
+++ lib/ukstreambuf/tests/test_ukstreambuf.c
@@ -48,4 +48,240 @@ UK_TESTCASE(ukstreambuf, streambuf_init_termshift)
 	UK_TEST_EXPECT_SNUM_EQ(buf[0], '\0');
 }
 
+UK_TESTCASE(ukstreambuf, streambuf_strcpy)
+{
+	struct uk_streambuf sb;
+	char buf[10];
+	const char result[10] = { 't', 'e', 's', 't', '1', '\0',
+				  '-', '\0', 't', '\0' };
+	__sz ret;
+
+	uk_streambuf_init(&sb, buf, ARRAY_SIZE(buf), 0x0);
+
+	/* First string */
+	ret = uk_streambuf_strcpy(&sb, "test1");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 6); /* written size incl. '\0' */
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 6);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 6);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 4);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[6]);
+	UK_TEST_EXPECT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Append another string */
+	ret = uk_streambuf_strcpy(&sb, "-");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 2);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 8);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 8);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 2);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[8]);
+	/* '\0'-termination should be kept between appended strings */
+	UK_TEST_EXPECT_SNUM_EQ(buf[5], '\0');
+	UK_TEST_EXPECT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Running out of space */
+	ret = uk_streambuf_strcpy(&sb, "test2");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 2);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 10);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 10);
+	UK_TEST_EXPECT_ZERO(uk_streambuf_left(&sb));
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[10]);
+	UK_TEST_EXPECT_SNUM_EQ(buf[7], '\0');
+	UK_TEST_EXPECT_SNUM_EQ(buf[9], '\0');
+	UK_TEST_EXPECT_NOT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* No space left */
+	ret = uk_streambuf_strcpy(&sb, "-");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 0);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 10);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 10);
+	UK_TEST_EXPECT_ZERO(uk_streambuf_left(&sb));
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[10]);
+	UK_TEST_EXPECT_SNUM_EQ(buf[9], '\0');
+	UK_TEST_EXPECT_NOT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Result on buf as expected? */
+	UK_TEST_EXPECT_ZERO(memcmp(buf, result, ARRAY_SIZE(buf)));
+}
+
+UK_TESTCASE(ukstreambuf, streambuf_strcpy_termshift)
+{
+	struct uk_streambuf sb;
+	char buf[10];
+	const char result[10] = { 't', 'e', 's', 't', '1', '-',
+				  't', 'e', 's', '\0' };
+	__sz ret;
+
+	/*
+	 * NOTE: With TERMSHIFT, the wptr should move one position backwards
+	 *       for successive writes (resulting in C-string concatenation).
+	 */
+	uk_streambuf_init(&sb, buf, ARRAY_SIZE(buf), UK_STREAMBUF_C_TERMSHIFT);
+
+	/* First string */
+	ret = uk_streambuf_strcpy(&sb, "test1");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 6); /* written size incl. '\0' */
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 5);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 6);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 5);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[5]);
+	UK_TEST_EXPECT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Append another string */
+	ret = uk_streambuf_strcpy(&sb, "-");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 2);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 6);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 7);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 4);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[6]);
+	/* There should be no '\0'-termination between concatenated strings */
+	UK_TEST_EXPECT_SNUM_NQ(buf[5], '\0');
+	UK_TEST_EXPECT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Running out of space */
+	ret = uk_streambuf_strcpy(&sb, "test2");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 4);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 9);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 10);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 1);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[9]);
+	UK_TEST_EXPECT_SNUM_NQ(buf[6], '\0');
+	UK_TEST_EXPECT_SNUM_EQ(buf[9], '\0');
+	UK_TEST_EXPECT_NOT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* No space left */
+	ret = uk_streambuf_strcpy(&sb, "-");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 1); /* '\0' written */
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 9);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 10);
+	/* Last '\0' stays over-writable (but only with '\0')*/
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 1);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[9]);
+	UK_TEST_EXPECT_SNUM_EQ(buf[9], '\0');
+	UK_TEST_EXPECT_NOT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Result on buf as expected? */
+	UK_TEST_EXPECT_ZERO(memcmp(buf, result, ARRAY_SIZE(buf)));
+}
+
+UK_TESTCASE(ukstreambuf, streambuf_printf)
+{
+	struct uk_streambuf sb;
+	char buf[10];
+	const char result[10] = { 't', 'e', 's', 't', '1', '\0',
+				  '-', '\0', 't', '\0' };
+	__sz ret;
+
+	uk_streambuf_init(&sb, buf, ARRAY_SIZE(buf), 0x0);
+
+	/* First string */
+	ret = uk_streambuf_printf(&sb, "%s", "test1");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 6); /* written size incl. '\0' */
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 6);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 6);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 4);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[6]);
+	UK_TEST_EXPECT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Append another string */
+	ret = uk_streambuf_printf(&sb, "%s", "-");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 2);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 8);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 8);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 2);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[8]);
+	/* '\0'-termination should be kept between appended strings */
+	UK_TEST_EXPECT_SNUM_EQ(buf[5], '\0');
+	UK_TEST_EXPECT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Running out of space */
+	ret = uk_streambuf_printf(&sb, "%s", "test2");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 2);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 10);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 10);
+	UK_TEST_EXPECT_ZERO(uk_streambuf_left(&sb));
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[10]);
+	UK_TEST_EXPECT_SNUM_EQ(buf[7], '\0');
+	UK_TEST_EXPECT_SNUM_EQ(buf[9], '\0');
+	UK_TEST_EXPECT_NOT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* No space left */
+	ret = uk_streambuf_printf(&sb, "%s", "-");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 0);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 10);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 10);
+	UK_TEST_EXPECT_ZERO(uk_streambuf_left(&sb));
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[10]);
+	UK_TEST_EXPECT_SNUM_EQ(buf[9], '\0');
+	UK_TEST_EXPECT_NOT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Result on buf as expected? */
+	UK_TEST_EXPECT_ZERO(memcmp(buf, result, ARRAY_SIZE(buf)));
+}
+
+UK_TESTCASE(ukstreambuf, streambuf_printf_termshift)
+{
+	struct uk_streambuf sb;
+	char buf[10];
+	const char result[10] = "test1-tes";
+	__sz ret;
+
+	/*
+	 * NOTE: With TERMSHIFT, the wptr should move one position backwards
+	 *       for successive writes (resulting in C-string concatenation).
+	 */
+	uk_streambuf_init(&sb, buf, ARRAY_SIZE(buf), UK_STREAMBUF_C_TERMSHIFT);
+
+	/* First string */
+	ret = uk_streambuf_printf(&sb, "%s", "test1");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 6); /* written size incl. '\0' */
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 5);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 6);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 5);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[5]);
+	UK_TEST_EXPECT_SNUM_EQ(buf[5], '\0');
+	UK_TEST_EXPECT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Append another string */
+	ret = uk_streambuf_printf(&sb, "%s", "-");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 2);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 6);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 7);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 4);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[6]);
+	/* There should be no '\0'-termination between concatenated strings */
+	UK_TEST_EXPECT_SNUM_NQ(buf[5], '\0');
+	UK_TEST_EXPECT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Running out of space */
+	ret = uk_streambuf_printf(&sb, "%s", "test2");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 4);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 9);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 10);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 1);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[9]);
+	UK_TEST_EXPECT_SNUM_NQ(buf[6], '\0');
+	UK_TEST_EXPECT_SNUM_EQ(buf[9], '\0');
+	UK_TEST_EXPECT_NOT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* No space left */
+	ret = uk_streambuf_printf(&sb, "%s", "-");
+	UK_TEST_EXPECT_SNUM_EQ(ret, 1); /* '\0' written */
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_seek(&sb), 9);
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_len(&sb), 10);
+	/* Last '\0' stays over-writable (but only with '\0')*/
+	UK_TEST_EXPECT_SNUM_EQ(uk_streambuf_left(&sb), 1);
+	UK_TEST_EXPECT_PTR_EQ(uk_streambuf_wptr(&sb), &buf[9]);
+	UK_TEST_EXPECT_SNUM_EQ(buf[9], '\0');
+	UK_TEST_EXPECT_NOT_ZERO(uk_streambuf_istruncated(&sb));
+
+	/* Result on buf as expected? */
+	UK_TEST_EXPECT_ZERO(memcmp(buf, result, ARRAY_SIZE(buf)));
+}
+
+/*
+ * NOTE: `uk_streambuf_printf()` relies on `snprintf()` which may require a
+ *       TLS (depending on selected libc):
+ *       We should run the unit tests after a TLS is set up. This means that
+ *       the tests should not be invoked from the ctortab.
+ */
 uk_testsuite_register(ukstreambuf, NULL);
