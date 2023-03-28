a012ed567119a2c068548ed5d0b65efb29a3d575
Sergiu Moga <sergiu.moga@protonmail.com>
2023-03-28 17:20:01 +0300

    plat/kvm/efi.c: Add support for initial RAM disk file
    This enables the EFI stub to load an initial RAM disk file from the
    same filesystem that the Unikraft image was loaded from (the EFI
    System Partition) and register it as a `Memory Region Descriptor`.
    
    The name of the `initrd` file is given through the
    `CONFIG_UK_EFI_STUB_INITRD_FNAME` configuration entry and it tells
    the loader the name of the file to load from the `\EFI\BOOT` directory
    of the EFI System Partition.
    
    Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

--- plat/kvm/efi.c
+++ plat/kvm/efi.c
@@ -482,6 +482,33 @@ static void uk_efi_setup_bootinfo_cmdl(struct ukplat_bootinfo *bi)
 	bi->cmdline_len = len;
 }
 
+static void uk_efi_setup_bootinfo_initrd(struct ukplat_bootinfo *bi)
+{
+	struct ukplat_memregion_desc mrd = {0};
+	struct uk_efi_ld_img_hndl *uk_img_hndl;
+	char *initrd;
+	__sz len;
+	int rc;
+
+	if (sizeof(CONFIG_KVM_BOOT_EFI_STUB_INITRD_FNAME) <= 1)
+		return;
+
+	uk_img_hndl = uk_efi_get_uk_img_hndl();
+
+	uk_efi_read_file(uk_img_hndl->device_handle,
+			 UK_EFI_ABS_FNAME(CONFIG_KVM_BOOT_EFI_STUB_INITRD_FNAME),
+			 (char **)&initrd, &len);
+
+	mrd.pbase = (__paddr_t)initrd;
+	mrd.vbase = (__vaddr_t)initrd;
+	mrd.len = PAGE_ALIGN_UP(len);
+	mrd.type = UKPLAT_MEMRT_INITRD;
+	mrd.flags = UKPLAT_MEMRF_READ | UKPLAT_MEMRF_MAP;
+	rc = ukplat_memregion_list_insert(&bi->mrds, &mrd);
+	if (unlikely(rc < 0))
+		uk_efi_crash("Failed to insert initrd mrd\n");
+}
+
 static void uk_efi_setup_bootinfo(void)
 {
 	const char bl[] = "EFI_STUB";
@@ -495,6 +522,7 @@ static void uk_efi_setup_bootinfo(void)
 	memcpy(bi->bootloader, bl, sizeof(bl));
 	memcpy(bi->bootprotocol, bp, sizeof(bp));
 	uk_efi_setup_bootinfo_cmdl(bi);
+	uk_efi_setup_bootinfo_initrd(bi);
 	uk_efi_setup_bootinfo_mrds(bi);
 
 	bi->efi_st = (__u64)uk_efi_st;
