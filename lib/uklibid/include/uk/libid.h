8999cd4be1c239bdd1b643bc83b2117914ce33d3
Simon Kuenzer <simon@unikraft.io>
2023-05-31 18:12:19 +0200

    lib/uklibid: Add library skeleton
    libuklibid will provide generated identifiers for each selected library.
    
    Signed-off-by: Simon Kuenzer <simon@unikraft.io>
    Reviewed-by: Michalis Pappas <michalis@unikraft.io>
    Reviewed-by: Robert Kuban <robert.kuban@opensynergy.com>
    Approved-by: Razvan Deaconescu <razvand@unikraft.io>
    Tested-by: Unikraft CI <monkey@unikraft.io>
    GitHub-Closes: #938

---
+++ lib/uklibid/include/uk/libid.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+#ifndef __LIBUKLIBID_H__
+#define __LIBUKLIBID_H__
+
+#endif /* __LIBUKLIBID_H__ */
