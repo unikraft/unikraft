f7511c8b0612cfe4f19e9e632b6bb2ca6297d1b3
Marco Schlumpp <marco@unikraft.io>
2023-07-03 17:20:59 +0200

    lib/vfscore: Initialize stdio file lock
    The code was previously relying on being able to zero initialize the
    lock structure.
    
    Signed-off-by: Marco Schlumpp <marco@unikraft.io>
    Reviewed-by: Tu Dinh Ngoc <dinhngoc.tu@irit.fr>
    Approved-by: Razvan Deaconescu <razvand@unikraft.io>
    Tested-by: Unikraft CI <monkey@unikraft.io>
    GitHub-Closes: #999

--- lib/vfscore/stdio.c
+++ lib/vfscore/stdio.c
@@ -221,6 +221,7 @@ static struct vfscore_file  stdio_file = {
 	.f_flags = UK_FWRITE | UK_FREAD,
 	.f_dentry = &stdio_dentry,
 	.f_vfs_flags = UK_VFSCORE_NOPOS,
+	.f_lock = UK_MUTEX_INITIALIZER(stdio_file.f_lock),
 	/* reference count is 2 because close(0) is a valid
 	 * operation. However it is not properly handled in the
 	 * current implementation. */
