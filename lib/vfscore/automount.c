6cea5be5eec643eb6b0001b8eef143419303aa6e
Sergiu Moga <sergiu@unikraft.io>
2023-07-12 17:29:42 +0300

    lib/vfscore: Implement individual volume automounting
    Provide a command-line argument `vfs.fstab` that is meant to
    contain a list of whitespace separated strings with the following
    format:
    ```
    	vfs.fstab=[
    		"<src_dev>:<mntpoint>:<fsdriver>[:<flags>:<opts>]"
    		"<src_dev>:<mntpoint>:<fsdriver>[:<flags>:<opts>]"
    		...
    	]
    ```
    
    The core will parse the provided strings as volume information and
    attempt to mount them accordingly, if `CONFIG_LIBVFSCORE_FSTAB` is
    provided.
    
    Furthermore, add a configurable `CONFIG_LIBVFSCORE_FSTAB_SIZE`
    option that will be used as the maximum amount of automatically mounted
    volumes, excluding the `rootfs` and modularize `initrd` extraction and
    mounting functionality into a standalone function to be used by both
    `fstab` and implicit `rootfs`.
    
    As a last change, since the file's functionality drastically changed,
    rename `rootfs.c` to `automount.c`.
    
    Signed-off-by: Sergiu Moga <sergiu@unikraft.io>

---
+++ lib/vfscore/automount.c
@@ -0,0 +1,310 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * VFS Automatic Mounts
+ *
+ * Authors: Simon Kuenzer <simon.kuenzer@neclab.eu>
+ *          Robert Hrusecky <roberth@cs.utexas.edu>
+ *          Omar Jamil <omarj2898@gmail.com>
+ *          Sachin Beldona <sachinbeldona@utexas.edu>
+ *          Sergiu Moga <sergiu@unikraft.io>
+ *
+ * Copyright (c) 2019, NEC Laboratories Europe GmbH, NEC Corporation.
+ *                     All rights reserved.
+ * Copyright (c) 2023, Unikraft GmbH. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mount.h>
+#include <uk/assert.h>
+#ifdef CONFIG_LIBUKCPIO
+#include <uk/cpio.h>
+#endif /* CONFIG_LIBUKCPIO */
+#include <uk/init.h>
+#include <uk/libparam.h>
+#include <uk/plat/memory.h>
+
+#ifdef CONFIG_LIBVFSCORE_AUTOMOUNT_ROOTFS
+#include <errno.h>
+#include <uk/config.h>
+#include <uk/arch/types.h>
+#include <sys/stat.h>
+#endif /* CONFIG_LIBVFSCORE_AUTOMOUNT_ROOTFS */
+
+struct vfscore_volume {
+	/* Volume source device */
+	const char *sdev;
+	/* Mount point absolute path */
+	const char *path;
+	/* Corresponding filesystem driver name */
+	const char *drv;
+	/* Mount flags */
+	unsigned long flags;
+	/* Mount options */
+	const char *opts;
+};
+
+static inline int vfscore_mount_volume(struct vfscore_volume *vv)
+{
+	return mount(vv->sdev, vv->path, vv->drv, vv->flags, vv->opts);
+}
+
+#ifdef CONFIG_LIBVFSCORE_AUTOMOUNT_ROOTFS
+
+static const char *rootfs   = CONFIG_LIBVFSCORE_ROOTFS;
+
+#ifndef CONFIG_LIBVFSCORE_ROOTDEV
+static const char *rootdev  = "";
+#else
+static const char *rootdev  = CONFIG_LIBVFSCORE_ROOTDEV;
+#endif
+
+#ifndef CONFIG_LIBVFSCORE_ROOTOPTS
+static const char *rootopts = "";
+#else
+static const char *rootopts = CONFIG_LIBVFSCORE_ROOTOPTS;
+#endif
+
+#ifndef CONFIG_LIBVFSCORE_ROOTFLAGS
+static __u64 rootflags;
+#else
+static __u64 rootflags      = (__u64) CONFIG_LIBVFSCORE_ROOTFLAGS;
+#endif
+
+UK_LIB_PARAM_STR(rootfs);
+UK_LIB_PARAM_STR(rootdev);
+UK_LIB_PARAM_STR(rootopts);
+UK_LIB_PARAM(rootflags, __u64);
+
+#endif /* CONFIG_LIBVFSCORE_AUTOMOUNT_ROOTFS */
+
+#ifdef CONFIG_LIBVFSCORE_FSTAB
+
+#define LIBVFSCORE_FSTAB_VOLUME_ARGS_SEP			':'
+
+static char *vfscore_fstab[CONFIG_LIBVFSCORE_FSTAB_SIZE];
+
+UK_LIBPARAM_PARAM_ARR_ALIAS(fstab, &vfscore_fstab, charp,
+			    CONFIG_LIBVFSCORE_FSTAB_SIZE,
+			    "VFSCore Filesystem Table");
+
+/**
+ * Expected command-line argument format:
+ *	vfs.fstab=[
+ *		"<src_dev>:<mntpoint>:<fsdriver>[:<flags>:<opts>]"
+ *		"<src_dev>:<mntpoint>:<fsdriver>[:<flags>:<opts>]"
+ *		...
+ *	]
+ * These list elements are expected to be separated by whitespaces.
+ * Mount options and flags are optional.
+ */
+static char *vfscore_fstab_next_volume_arg(char **pos)
+{
+	char *ipos;
+
+	UK_ASSERT(pos);
+
+	if (!*pos)
+		return NULL;
+
+	ipos = *pos;
+	*pos = strchr(ipos, LIBVFSCORE_FSTAB_VOLUME_ARGS_SEP);
+
+	if (*pos && **pos != '\0') {
+		**pos = '\0';
+		(*pos)++;
+	} else {
+		*pos = NULL;
+	}
+
+	return ipos;
+}
+
+static void vfscore_fstab_fetch_volume_args(char *v, struct vfscore_volume *vv)
+{
+	const char *flags;
+	char *pos;
+
+	UK_ASSERT(v);
+	UK_ASSERT(vv);
+
+	pos = v;
+
+	/* sdev, path and drv are mandatory */
+	vv->sdev = vfscore_fstab_next_volume_arg(&pos);
+	UK_ASSERT(vv->sdev);
+
+	vv->path = vfscore_fstab_next_volume_arg(&pos);
+	UK_ASSERT(vv->path);
+
+	vv->drv = vfscore_fstab_next_volume_arg(&pos);
+	UK_ASSERT(vv->drv);
+
+	if (!pos)
+		return;
+
+	/* Allow empty flags: "<src_dev>:<mntpoint>:<fsdriver>::<opts>" */
+	flags = vfscore_fstab_next_volume_arg(&pos);
+	if (flags && flags[0] != '\0')
+		vv->flags = strtol(flags, NULL, 0);
+	else
+		vv->flags = 0;
+
+	vv->opts = vfscore_fstab_next_volume_arg(&pos);
+	if (vv->opts && vv->opts[0] == '\0')
+		vv->opts = NULL;
+}
+#endif /* CONFIG_LIBVFSCORE_FSTAB */
+
+#ifdef CONFIG_LIBUKCPIO
+static int vfscore_mount_initrd_volume(struct vfscore_volume *vv)
+{
+	struct ukplat_memregion_desc *initrd;
+	int rc;
+
+	UK_ASSERT(vv);
+
+	/* TODO: Support multiple Initial RAM Disks */
+	rc = ukplat_memregion_find_initrd0(&initrd);
+	if (unlikely(rc < 0)) {
+		uk_pr_crit("Could not find an initrd!\n");
+
+		return -1;
+	}
+
+	rc = mount(vv->sdev, vv->path, "ramfs", vv->flags, vv->opts);
+	if (unlikely(rc)) {
+		uk_pr_crit("Failed to mount ramfs to /: %d\n", errno);
+
+		return -1;
+	}
+
+	uk_pr_info("Extracting initrd @ %p (%"__PRIsz" bytes) to %s...\n",
+		   (void *)initrd->vbase, initrd->len, vv->path);
+
+	rc = ukcpio_extract(vv->path, (void *)initrd->vbase, initrd->len);
+	if (unlikely(rc)) {
+		uk_pr_crit("Failed to extract cpio archive to %s: %d\n",
+			   vv->path, rc);
+
+		return -1;
+	}
+
+	return 0;
+}
+
+#else /* CONFIG_LIBUKCPIO */
+static int vfscore_mount_initrd_volume(struct vfscore_volume *vv __unused)
+{
+	return -1;
+}
+#endif /* !CONFIG_LIBUKCPIO */
+
+#ifdef CONFIG_LIBVFSCORE_AUTOMOUNT_ROOTFS
+static int vfscore_automount_rootfs(void)
+{
+	/* Convert to `struct vfscore_volume` */
+	struct vfscore_volume vv = {
+		.sdev = rootdev,
+		.path = "/",
+		.drv = rootfs,
+		.flags = rootflags,
+		.opts = rootopts,
+	};
+	int rc;
+
+	/*
+	 * Initialization of the root filesystem '/'
+	 * NOTE: Any additional sub mount points (like '/dev' with devfs)
+	 * have to be mounted later.
+	 *
+	 * Silently return 0, as user might not have configured implicit rootfs.
+	 */
+	if (!vv.drv || vv.drv[0] == '\0')
+		return 0;
+
+#if CONFIG_LIBVFSCORE_ROOTFS_INITRD
+	if (!strncmp(vv.drv, "initrd", sizeof("initrd") - 1))
+		return vfscore_mount_initrd_volume(&vv);
+#endif /* CONFIG_LIBVFSCORE_ROOTFS_INITRD */
+
+	rc = vfscore_mount_volume(&vv);
+	if (unlikely(rc))
+		uk_pr_crit("Failed to mount %s (%s) at /: %d\n", vv.sdev,
+			   vv.drv, rc);
+
+	return rc;
+}
+#else /* CONFIG_LIBVFSCORE_AUTOMOUNT_ROOTFS */
+static int vfscore_automount_rootfs(void)
+{
+	return 0;
+}
+#endif /* !CONFIG_LIBVFSCORE_AUTOMOUNT_ROOTFS */
+
+#ifdef CONFIG_LIBVFSCORE_FSTAB
+static int vfscore_automount_fstab_volumes(void)
+{
+	struct vfscore_volume vv;
+	int rc, i;
+
+	for (i = 0; i < CONFIG_LIBVFSCORE_FSTAB_SIZE && vfscore_fstab[i]; i++) {
+		vfscore_fstab_fetch_volume_args(vfscore_fstab[i], &vv);
+
+		if (!strncmp(vv.drv, "initrd", sizeof("initrd") - 1))
+			rc = vfscore_mount_initrd_volume(&vv);
+		else
+			rc = vfscore_mount_volume(&vv);
+		if (unlikely(rc)) {
+			uk_pr_err("Failed to mount %s: error %d\n", vv.sdev,
+				  rc);
+
+			return rc;
+		}
+	}
+
+	return 0;
+}
+#else /* CONFIG_LIBVFSCORE_FSTAB */
+static int vfscore_automount_fstab_volumes(void)
+{
+	return 0;
+}
+#endif /* !CONFIG_LIBVFSCORE_FSTAB */
+
+static int vfscore_automount(void)
+{
+	int rc;
+
+	rc = vfscore_automount_rootfs();
+	if (unlikely(rc < 0))
+		return rc;
+
+	return vfscore_automount_fstab_volumes();
+}
+
+uk_rootfs_initcall_prio(vfscore_automount, 4);
