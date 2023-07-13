faf77d778c4f3a454d71465c2f7b9301f4f4e925
Robert Kuban <robert.kuban@opensynergy.com>
2023-07-13 12:53:53 +0200

    plat/kvm/arm: Do not enable interrupts before `wfi`
    There is the opportunity to miss an IRQ on the path to `wfi` from the
    idle thread after deciding to sleep:
    The interrupts are enable just before exuting `wfi`.
    If the interrupt already arrived before that, it will be executed
    before `wfi`, and `wfi` will go to sleep afterward, not being woken
    up by the interrupt.
    
    This patch removes calls to enable/disable interrupts around `wfi`.
    `wfi` will be woken up by pending interrupts regardless.
    
    Signed-off-by: Robert Kuban <robert.kuban@opensynergy.com>

--- plat/kvm/arm/lcpu.c
+++ plat/kvm/arm/lcpu.c
@@ -48,9 +48,12 @@ void ukplat_lcpu_halt_irq(void)
 {
 	UK_ASSERT(ukplat_lcpu_irqs_disabled());
 
-	ukplat_lcpu_enable_irq();
+	/* Note: If priority masking is enabled
+	 * interrupts need to be unmasked in the GIC.
+	 *
+	 * See Linux `cpu_do_idle(void)` implementation
+	 */
 	halt();
-	ukplat_lcpu_disable_irq();
 }
 
 unsigned long ukplat_lcpu_save_irqf(void)
