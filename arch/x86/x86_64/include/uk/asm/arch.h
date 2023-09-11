965dd4885ae1eb4f385a711ee98acd853e13d8bb
Michalis Pappas <michalis@unikraft.io>
2023-09-11 20:41:02 +0200

    lib/ukintctlr: Migrate PIC implementation to the xPIC driver
    Migrate PIC to drivers/ukintctlr/xpic. Move architectural definitions
    to arch/.
    
    Notice: Picking individual commits in this PR will break the build.
    
    Signed-off-by: Michalis Pappas <michalis@unikraft.io>
    Reviewed-by: Marco Schlumpp <marco@unikraft.io>
    Reviewed-by: Sergiu Moga <sergiu@unikraft.io>
    Approved-by: Razvan Deaconescu <razvand@unikraft.io>
    GitHub-Closes: #1103

---
+++ arch/x86/x86_64/include/uk/asm/arch.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+#ifndef __UK_ARCH_X86_64_H__
+#define __UK_ARCHX86_64__H__
+
+#include <uk/asm/pic.h>
+
+#endif /* __UK_ARCH_X86_64_H__ */
