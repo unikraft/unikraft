0ba907f908e6d24718875c34e0dbc88206a5ce37
Simon Kuenzer <simon@unikraft.io>
2023-09-15 12:57:10 +0200

    lib/uklibid: Embedded library information
    This commit introduces the `.uk_libinfo` section which is used to store
    additional library information in library object files. We define the data
    structure of the section and declare it as layout version 1. For each
    library the build system will generate a single header and each header can
    contain an individual number of records to store values.
    Layout version 1 also supports storing of information related to the image
    as a whole and not to a specific library. A single header is created for
    global information, which differs from the library headers in that it does
    not contain a record for a library name (LIBNAME).
    
    When a final Unikraft image is linked, the individual sections of each
    library are merged into one section.
    
    By default, a minimal set of library and global information is enabled. The
    motivation behind this is that library objects are instrumented with
    uk_libinfo from now on. In a future commit, the build system could provide
    an option to remove this section from (non-debug) images if it is not used
    by Unikraft code itself.
    
    Signed-off-by: Simon Kuenzer <simon@unikraft.io>

---
+++ lib/uklibid/include/uk/libid/info.h
@@ -0,0 +1,93 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+#ifndef __UK_LIBID_INFO_H__
+#define __UK_LIBID_INFO_H__
+
+#include <uk/arch/types.h>
+#include <uk/essentials.h>
+
+/*
+ * Library information is stored in the special section " .uk_libinfo". For
+ * each library there is one `struct uk_libid_info_hdr` generated. Each header
+ * can contain multiple `struct uk_libid_info_rec` records. A library name
+ * record (`UKLI_REC_LIBNAME`) is the only mandatory record type for each
+ * library.
+ * Additionally, a single header is created for global information, which
+ * differs from the library headers in that it does not contain a record for a
+ * library name.
+ *
+ * Layout for each information header:
+ *
+ *                              _____________
+ *  +--------------------------+             \
+ *  | struct uk_libid_info_hdr |              |
+ *  |                          |               > hdr->len
+ *  +--------------------------+              |
+ *  | struct uk_libid_info_rec |              |
+ *  +--------------------------+ -.           |
+ *  | struct uk_libid_info_rec |   > rec->len |
+ *  |                          |  |           |
+ *  +--------------------------+ -`           |
+ *  :          . . .           :              :
+ *  +--------------------------+              |
+ *  | struct uk_libid_info_rec |              |
+ *  |                          |              |
+ *  +--------------------------+ ____________/
+ */
+
+/*
+ * NOTE: The layout version needs only be updated if the structs
+ *       `uk_libid_info_rec` and `uk_libid_info_hdr` are updated.
+ */
+ #define UKLI_LAYOUT              0x0001
+
+/*
+ * NOTE: In order to keep backwards compatibility with pre-compiled
+ *       libraries, this list should only be extended with new entries.
+ */
+#define UKLI_REC_LIBNAME          0x0001 /* C-string, absent for global hdr */
+#define UKLI_REC_COMMENT          0x0002 /* C-string */
+#define UKLI_REC_VERSION          0x0003 /* C-string */
+#define UKLI_REC_LICENSE          0x0004 /* C-string */
+#define UKLI_REC_GITDESC          0x0005 /* C-string */
+#define UKLI_REC_UKVERSION        0x0006 /* C-string, Unikraft version only */
+#define UKLI_REC_UKFULLVERSION    0x0007 /* C-string, UK version with gitsha */
+#define UKLI_REC_UKCODENAME       0x0008 /* C-string */
+#define UKLI_REC_UKCONFIG         0x0009 /* binary data */
+#define UKLI_REC_UKCONFIGGZ       0x000A /* binary data */
+#define UKLI_REC_COMPILER         0x000B /* C-string */
+#define UKLI_REC_COMPILEDATE      0x000C /* C-string */
+#define UKLI_REC_COMPILEDBY       0x000D /* C-string */
+#define UKLI_REC_COMPILEDBYASSOC  0x000E /* C-string */
+#define UKLI_REC_COMPILEOPTS      0x000F /* __u32 flags */
+
+#if !__ASSEMBLY__
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct uk_libid_info_rec {
+	__u16 type;    /* record type */
+	__u32 len;     /* including record header */
+	char  data[];  /* raw data */
+} __packed __align(1);
+
+/*
+ * NOTE: Due to binary cross-compatibility, the sizes, offsets, data types and
+ *       meanings must remain the same regardless of the layout version:
+ *       `len` and `version`.
+ */
+struct uk_libid_info_hdr {
+	__u32 len;     /* including header */
+	__u16 version; /* layout version */
+	struct uk_libid_info_rec recs[];
+} __packed __align(1);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !__ASSEMBLY__ */
+#endif /* __UK_LIBID_INFO_H__ */
