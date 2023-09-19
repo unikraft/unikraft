e16dc63cedefe73c94ed6d42ce88919ac6b7626f
Michalis Pappas <michalis@unikraft.io>
2023-09-19 16:37:51 +0200

    drivers/ukintctlr/xpic: Add MAX_IRQ limits for xPIC
    Notice: Picking individual commits in this PR will break the build.
    
    Signed-off-by: Michalis Pappas <michalis@unikraft.io>

---
+++ drivers/ukintctlr/xpic/include/uk/intctlr/limits.h
@@ -0,0 +1,11 @@
+/* Copyright (c) 2023, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+
+#ifndef __UK_INTCTLR_XPIC_LIMITS_H__
+#define __UK_INTCTLR_XPIC_LIMITS_H__
+
+#define UK_INTCTLR_MAX_IRQ                             16
+
+#endif /* __UK_INTCTLR_XPIC_LIMITS_H__ */
