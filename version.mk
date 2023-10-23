97523b55ac9e1327f7294da1feec8d4869da534d
Alexander Jung <alex@unikraft.io>
2023-10-23 09:11:34 +0000

    Release: v0.15.0 Pandora
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>
    Signed-off-by: Marc Rittinghaus <marc@unikraft.io>
    Signed-off-by: Simon Kuenzer <simon@unikraft.io>
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 UK_VERSION = 0
-UK_SUBVERSION = 14
+UK_SUBVERSION = 15
 UK_EXTRAVERSION = 0
-UK_CODENAME = Prometheus
+UK_CODENAME = Pandora
 # https://en.wikipedia.org/wiki/Moons_of_Saturn (by discovery year)
