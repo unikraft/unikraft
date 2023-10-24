afcba56944675d51047262f12f3696c4532329c3
Simon Kuenzer <simon@unikraft.io>
2023-10-24 17:05:26 +0200

    include/uk: Notes about shutdown request event queue
    This commit documents the shutdown request event queue within
    `include/uk/plat/bootstrap.h`. The idea is that a platform declare the
    queue and raises events with drivers. Any library can then register
    shutdown handlers to this queue and can take according actions.
    
    Signed-off-by: Simon Kuenzer <simon@unikraft.io>
    Reviewed-by: Sergiu Moga <sergiu@unikraft.io>
    Approved-by: Razvan Deaconescu <razvand@unikraft.io>
    GitHub-Closes: #1148

--- include/uk/plat/bootstrap.h
+++ include/uk/plat/bootstrap.h
@@ -102,6 +102,20 @@ void ukplat_terminate(enum ukplat_gstate request) __noreturn;
  */
 int ukplat_suspend(void);
 
+/*
+ * NOTE: Shutdown request event queue
+ * ==================================
+ *
+ * The platform may define a UKPLAT_SHUTDOWN_EVENT event queue according to
+ * `<uk/event.h>`. Drivers will raise events there as soon as a shutdown is
+ * requested. The driver will pass an `enum ukplat_gstate` argument describing
+ * the shutdown action that the system is requested to perform. Any library is
+ * able to register a handler to this event queue and is able to take according
+ * actions.
+ * Please note that the event handlers must be non-blocking and ISR-safe because
+ * the queue can be raised from interrupt-context.
+ */
+
 #ifdef __cplusplus
 }
 #endif
