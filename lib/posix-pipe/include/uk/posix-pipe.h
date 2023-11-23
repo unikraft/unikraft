c17fb4e5992feb7962929d0db846d139349dc421
Andrei Tatar <andrei@unikraft.io>
2023-11-23 19:43:50 +0100

    lib/posix-pipe: Introduce posix-pipe library
    This change introduces the posix-pipe library, providing
    POSIX-compatible newvfs pipe files.
    Initial implementation does not support packet pipes, and only exposes
    Unikraft-internal interfaces.
    
    Checkpatch-Ignore: LINE_SPACING
    Checkpatch-Ignore: VOLATILE
    Signed-off-by: Andrei Tatar <andrei@unikraft.io>

---
+++ lib/posix-pipe/include/uk/posix-pipe.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+
+/* Support for POSIX pipes */
+
+#ifndef __UKPOSIX_PIPE_H__
+#define __UKPOSIX_PIPE_H__
+
+#include <uk/file.h>
+#include <uk/posix-fdtab.h>
+
+/* File creation */
+
+int uk_pipefile_create(struct uk_file *pipes[2], int flags);
+
+/* Internal syscalls */
+
+int uk_sys_pipe(int pipefd[2], int flags);
+
+#endif /* __UKPOSIX_PIPE_H__ */
