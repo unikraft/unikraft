ecda7fd046577a0e0795be2a5c042d3876185b1b
Sergiu Moga <sergiu@unikraft.io>
2023-11-24 14:31:18 +0200

    arch/x86/x86_64: Allow asm files access to register offsets macros
    It may come in handy to be able to know the offsets of each register
    inside the `struct __regs` structure during more fragile states of
    execution, usually coded in assembly.
    
    Therefore, move the guarding `#ifdef` below the register definitions
    so that assembly files may be able to include this header and benefit
    from the aforementioned macros.
    
    Signed-off-by: Sergiu Moga <sergiu@unikraft.io>
    Reviewed-by: Michalis Pappas <michalis@unikraft.io>
    Approved-by: Razvan Deaconescu <razvand@unikraft.io>
    GitHub-Closes: #1175

--- arch/x86/x86_64/include/uk/asm/lcpu.h
+++ arch/x86/x86_64/include/uk/asm/lcpu.h
@@ -34,8 +34,35 @@
 
 #define CACHE_LINE_SIZE	64
 
+#define __REGS_OFFSETOF_PAD       0
+#define __REGS_OFFSETOF_R15       8
+#define __REGS_OFFSETOF_R14       16
+#define __REGS_OFFSETOF_R13       24
+#define __REGS_OFFSETOF_R12       32
+#define __REGS_OFFSETOF_RBP       40
+#define __REGS_OFFSETOF_RBX       48
+#define __REGS_OFFSETOF_R11       56
+#define __REGS_OFFSETOF_R10       64
+#define __REGS_OFFSETOF_R9        72
+#define __REGS_OFFSETOF_R8        80
+#define __REGS_OFFSETOF_RAX       88
+#define __REGS_OFFSETOF_RCX       96
+#define __REGS_OFFSETOF_RDX       104
+#define __REGS_OFFSETOF_RSI       112
+#define __REGS_OFFSETOF_RDI       120
+#define __REGS_OFFSETOF_ORIG_RAX  128
+#define __REGS_OFFSETOF_RIP       136
+#define __REGS_OFFSETOF_CS        144
+#define __REGS_OFFSETOF_EFLAGS    152
+#define __REGS_OFFSETOF_RSP       160
+#define __REGS_OFFSETOF_SS        168
+
+#define __REGS_PAD_SIZE           __REGS_OFFSETOF_R15
+#define __REGS_SIZEOF             176
+
 #ifndef __ASSEMBLY__
 #include <uk/arch/types.h>
+#include <uk/essentials.h>
 
 struct __regs {
 	unsigned long pad; /* 8 bytes to make struct size multiple of 16 */
@@ -66,32 +93,6 @@ struct __regs {
 /* top of stack page */
 };
 #endif /* !__ASSEMBLY__ */
-
-#define __REGS_OFFSETOF_PAD       0
-#define __REGS_OFFSETOF_R15       8
-#define __REGS_OFFSETOF_R14       16
-#define __REGS_OFFSETOF_R13       24
-#define __REGS_OFFSETOF_R12       32
-#define __REGS_OFFSETOF_RBP       40
-#define __REGS_OFFSETOF_RBX       48
-#define __REGS_OFFSETOF_R11       56
-#define __REGS_OFFSETOF_R10       64
-#define __REGS_OFFSETOF_R9        72
-#define __REGS_OFFSETOF_R8        80
-#define __REGS_OFFSETOF_RAX       88
-#define __REGS_OFFSETOF_RCX       96
-#define __REGS_OFFSETOF_RDX       104
-#define __REGS_OFFSETOF_RSI       112
-#define __REGS_OFFSETOF_RDI       120
-#define __REGS_OFFSETOF_ORIG_RAX  128
-#define __REGS_OFFSETOF_RIP       136
-#define __REGS_OFFSETOF_CS        144
-#define __REGS_OFFSETOF_EFLAGS    152
-#define __REGS_OFFSETOF_RSP       160
-#define __REGS_OFFSETOF_SS        168
-
-#define __REGS_PAD_SIZE           __REGS_OFFSETOF_R15
-#define __REGS_SIZEOF             176
 
 /* sanity check */
 #if __REGS_SIZEOF & 0xf
