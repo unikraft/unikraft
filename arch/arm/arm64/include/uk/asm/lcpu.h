ec836119bce051d37a96bbd211a5aee9dbd9c5ae
Sergiu Moga <sergiu@unikraft.io>
2023-11-24 14:31:18 +0200

    arch/arm/arm64: Allow asm files access to register offsets macros
    Therefore, move the guarding `#ifdef` below the register definitions
    so that assembly files may be able to include this header and benefit
    from the aforementioned macros.
    
    Signed-off-by: Sergiu Moga <sergiu@unikraft.io>

--- arch/arm/arm64/include/uk/asm/lcpu.h
+++ arch/arm/arm64/include/uk/asm/lcpu.h
@@ -34,6 +34,7 @@
 #include <uk/asm.h>
 #include <uk/asm/arch.h>
 #include <uk/config.h>
+#include <uk/essentials.h>
 
 #define CACHE_LINE_SIZE		64
 
@@ -166,7 +167,6 @@ static __attribute__((unused))
 unsigned char tcr_ips_bits[] = {32, 36, 40, 42, 44, 48, 52};
 #endif
 
-#ifdef __ASSEMBLY__
 /*
  * Stack size to save general purpose registers and essential system
  * registers. 8 * (30 + lr + elr_el1 + spsr_el1 + esr_el1) = 272.
@@ -189,7 +189,7 @@ unsigned char tcr_ips_bits[] = {32, 36, 40, 42, 44, 48, 52};
  */
 #define __CALLEE_SAVED_SIZE    96
 
-#else
+#if !__ASSEMBLY__
 
 #include <stdint.h>
 
@@ -376,4 +376,4 @@ static inline void ukarch_spinwait(void)
 	/* Intelligent busy wait not supported on arm64. */
 }
 
-#endif /* __ASSEMBLY__ */
+#endif /* !__ASSEMBLY__ */
