241b13a558fb56c0ba9ff22d7b16f8c5c8785550
Andrei Tatar <andrei@unikraft.io>
2024-02-14 16:43:42 +0100

    lib/nolibc: Provide fd functions without vfscore
    This change makes nolibc provide file descriptor-related functions in
    `unistd.h` when the corresponding syscalls are provided by posix-fd*
    libraries, even without vfscore selected.
    
    Signed-off-by: Andrei Tatar <andrei@unikraft.io>

--- lib/nolibc/include/unistd.h
+++ lib/nolibc/include/unistd.h
@@ -99,7 +99,25 @@ int dup3(int oldfd, int newfd, int flags);
 int unlink(const char *pathname);
 off_t lseek(int fd, off_t offset, int whence);
 int rmdir(const char *pathname);
-#endif
+#else /* !CONFIG_LIBVFSCORE */
+
+#if CONFIG_LIBPOSIX_FDTAB
+int close(int fd);
+int dup(int oldfd);
+int dup2(int oldfd, int newfd);
+int dup3(int oldfd, int newfd, int flags);
+
+#if CONFIG_LIBPOSIX_FDIO
+ssize_t write(int fd, const void *buf, size_t count);
+ssize_t pwrite(int fd, const void *buf, size_t count, off_t offset);
+ssize_t read(int fd, void *buf, size_t count);
+ssize_t pread(int fd, void *buf, size_t count, off_t offset);
+int fsync(int fd);
+off_t lseek(int fd, off_t offset, int whence);
+#endif /* CONFIG_LIBPOSIX_FDIO */
+#endif /* CONFIG_LIBPOSIX_FDTAB */
+
+#endif /* !CONFIG_LIBVFSCORE */
 
 #if CONFIG_LIBUKSIGNAL
 unsigned int alarm(unsigned int seconds);
