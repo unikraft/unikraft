207f6b961b7f47bb0cb30bd1bf94268dc21fcecd
Sergiu Moga <sergiu@unikraft.io>
2024-02-18 11:22:10 +0200

    plat/kvm/x86: Add early COM1 init/print for CPU init errors
    Usually early boot failures tend to be very confusing since there
    is no message printed. To ease figuring out what went wrong, implement
    a very basic early initialization macro for the COM1 port as well as
    a corresponding printing macro that can be used before having a stack.
    
    As a first use case of these newly added macros, print an error message
    when failing early CPU features initialization, right before halting the
    system.
    
    Signed-off-by: Sergiu Moga <sergiu@unikraft.io>

--- plat/kvm/x86/serial_console.c
+++ plat/kvm/x86/serial_console.c
@@ -29,33 +29,6 @@
 #include <kvm-x86/serial_console.h>
 #include <x86/cpu.h>
 
-#define COM1 0x3f8
-
-#define COM1_DATA (COM1 + 0)
-#define COM1_INTR (COM1 + 1)
-#define COM1_CTRL (COM1 + 3)
-#define COM1_STATUS (COM1 + 5)
-
-/* only when DLAB is set */
-#define COM1_DIV_LO (COM1 + 0)
-#define COM1_DIV_HI (COM1 + 1)
-
-/* baudrate divisor */
-#define COM1_BAUDDIV_HI 0x00
-
-#if CONFIG_KVM_SERIAL_BAUD_19200
-#define COM1_BAUDDIV_LO 0x04
-#elif CONFIG_KVM_SERIAL_BAUD_38400
-#define COM1_BAUDDIV_LO 0x03
-#elif CONFIG_KVM_SERIAL_BAUD_57600
-#define COM1_BAUDDIV_LO 0x02
-#else /* default, CONFIG_KVM_SERIAL_BAUD_115200 */
-#define COM1_BAUDDIV_LO 0x01
-#endif
-
-#define DLAB 0x80
-#define PROT 0x03 /* 8N1 (8 bits, no parity, one stop bit) */
-
 void _libkvmplat_init_serial_console(void)
 {
 	outb(COM1_INTR, 0x00);  /* Disable all interrupts */
