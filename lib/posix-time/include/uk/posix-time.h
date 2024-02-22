a02ae396b5001619c36b1141e7bc1bc767c4ec01
Andrei Tatar <andrei@unikraft.io>
2024-02-22 18:07:21 +0100

    lib/posix-time: Add internal syscall interface
    This change adds Unikraft-internal syscalls (uk_sys_*) to posix-time,
    allowing the use of time functions without either a libc or
    syscall-shim selected.
    
    Signed-off-by: Andrei Tatar <andrei@unikraft.io>

---
+++ lib/posix-time/include/uk/posix-time.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+
+#ifndef __UK_POSIX_TIME_H__
+#define __UK_POSIX_TIME_H__
+
+#include <time.h>
+#include <sys/time.h>
+
+int uk_sys_nanosleep(const struct timespec *req, struct timespec *rem);
+
+time_t uk_sys_time(time_t *tloc);
+
+int uk_sys_gettimeofday(struct timeval *restrict tv, void *tz);
+
+int uk_sys_clock_getres(clockid_t clockid, struct timespec *res);
+
+int uk_sys_clock_gettime(clockid_t clockid, struct timespec *tp);
+int uk_sys_clock_settime(clockid_t clockid, const struct timespec *tp);
+
+int uk_sys_clock_nanosleep(clockid_t clockid, int flags,
+			   const struct timespec *request,
+			   struct timespec *remain);
+
+#endif /* __UK_POSIX_TIME_H__ */
