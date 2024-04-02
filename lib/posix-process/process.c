54cb42b789437300a5a518203c825a2ebc4ee346
Michalis Pappas <michalis@unikraft.io>
2024-04-02 16:57:48 +0200

    lib/posix-process: Return negative error values
    Update pprocess_create_pthread() to return negative error values.
    
    Signed-off-by: Michalis Pappas <michalis@unikraft.io>

--- lib/posix-process/process.c
+++ lib/posix-process/process.c
@@ -126,14 +126,14 @@ static struct posix_thread *pprocess_create_pthread(
 	a = pprocess->_a;
 
 	tid = find_and_reserve_tid();
-	if (tid < 0) {
-		err = EAGAIN;
+	if (unlikely(tid < 0)) {
+		err = -EAGAIN;
 		goto err_out;
 	}
 
 	pthread = uk_zalloc(a, sizeof(*pthread));
-	if (!pthread) {
-		err = ENOMEM;
+	if (unlikely(!pthread)) {
+		err = -ENOMEM;
 		goto err_free_tid;
 	}
 
