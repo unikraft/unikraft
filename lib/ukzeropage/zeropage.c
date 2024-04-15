065c10fd14cb6a3a124b27185ff91d6dcdf0c491
Andrei Tatar <andrei@unikraft.io>
2024-04-15 20:14:43 +0200

    lib/ukzeropage: Introduce zeropage library
    This change introduces ukzeropage, a library providing a singleton
    instance of read-only page filled with zero bytes.
    This page exists independent of virtual memory support.
    
    Signed-off-by: Andrei Tatar <andrei@unikraft.io>

---
+++ lib/ukzeropage/zeropage.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2024, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+
+#include <uk/essentials.h>
+#include <uk/arch/paging.h>
+
+const char UK_ZEROPAGE[PAGE_SIZE] __align(PAGE_SIZE) = { 0 };
