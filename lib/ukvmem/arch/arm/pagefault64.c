unikraft/unikraft

commit 4d973417284a9518ee049ba1f3bc51ba3887e862

lib/ukvmem/arch: Do not print error message if demand paging disabled
In cases such as those of `lib/uknofault`, pafe faults may actually be
intentional. So, to avoid unnecessarily throwing out error messages,
check if on-demand paging was disabled prior to faulting.

Signed-off-by: Sergiu Moga <sergiu@unikraft.io>
Reviewed-by: Andrei Tatar <andrei@unikraft.io>
Approved-by: Razvan Deaconescu <razvand@unikraft.io>
GitHub-Closes: #1334

lib/ukvmem/arch/arm/pagefault64.c
@@ -11,6 +11,7 @@
 #include <uk/arch/types.h>
 #include <uk/print.h>
 #include <uk/config.h>
+#include <string.h>
 
 static int vmem_arch_pagefault(void *data)
 {
@@ -21,6 +22,7 @@ static int vmem_arch_pagefault(void *data)
 	};
 	unsigned int faulttype;
 	unsigned long dfsc;
+	struct uk_vas *vas;
 	int rc;
 
 	if (ctx->esr & ARM64_PF_ESR_WnR)
@@ -39,10 +41,13 @@ static int vmem_arch_pagefault(void *data)
 
 	rc = vmem_pagefault(vaddr, faulttype, ctx->regs);
 	if (unlikely(rc < 0)) {
-		uk_pr_crit("Cannot handle %s page fault at 0x%"__PRIvaddr
-			   " (ec: 0x%lx): %d\n",
-			   faultstr[faulttype & UK_VMA_FAULT_ACCESSTYPE],
-			   vaddr, ctx->esr, rc);
+		vas = uk_vas_get_active();
+		if (unlikely(vas && !(vas->flags & UK_VAS_FLAG_NO_PAGING)))
+			uk_pr_crit("Cannot handle %s page fault at 0x%"
+				   __PRIvaddr" (ec: 0x%x): %s (%d).\n",
+				   faultstr[faulttype &
+					    UK_VMA_FAULT_ACCESSTYPE],
+				   vaddr, ctx->error_code, strerror(-rc), -rc);
 
 		return UK_EVENT_NOT_HANDLED;
 	}

lib/ukvmem/arch/x86_64/pagefault.c
@@ -11,6 +11,7 @@
 #include <uk/arch/types.h>
 #include <uk/print.h>
 #include <uk/config.h>
+#include <string.h>
 
 static int vmem_arch_pagefault(void *data)
 {
@@ -20,6 +21,7 @@ static int vmem_arch_pagefault(void *data)
 		"read", "write", "exec"
 	};
 	unsigned int faulttype;
+	struct uk_vas *vas;
 	int rc;
 
 	if (ctx->error_code & X86_PF_EC_WR)
@@ -35,11 +37,14 @@ static int vmem_arch_pagefault(void *data)
 		faulttype |= UK_VMA_FAULT_MISCONFIG;
 
 	rc = vmem_pagefault(vaddr, faulttype, ctx->regs);
-	if (unlikely(rc < 0)) {
-		uk_pr_crit("Cannot handle %s page fault at 0x%"__PRIvaddr
-			   " (ec: 0x%x): %d\n",
-			   faultstr[faulttype & UK_VMA_FAULT_ACCESSTYPE],
-			   vaddr, ctx->error_code, rc);
+	if (rc < 0) {
+		vas = uk_vas_get_active();
+		if (unlikely(vas && !(vas->flags & UK_VAS_FLAG_NO_PAGING)))
+			uk_pr_crit("Cannot handle %s page fault at 0x%"
+				   __PRIvaddr" (ec: 0x%x): %s (%d).\n",
+				   faultstr[faulttype &
+					    UK_VMA_FAULT_ACCESSTYPE],
+				   vaddr, ctx->error_code, strerror(-rc), -rc);
 
 		return UK_EVENT_NOT_HANDLED;
 	}

commit 31eb2d13531851560bde0582be9d8608ea0bc821

lib/ukvmem/arch/arm: Fix `error_code` build error with correct `esr`
Commit 4d973417284a ("lib/ukvmem/arch: Do not print error message if demand paging disabled")
introduced a build error and a warning by copy-pasting the change from
x86 to Arm. This is incorrect as the trap context from Arm does not have
the `error_code` but the `esr` field instead. Fix this by using using the
corect field and correct string format.

Signed-off-by: Sergiu Moga <sergiu@unikraft.io>

lib/ukvmem/arch/arm/pagefault64.c
@@ -44,10 +44,10 @@ static int vmem_arch_pagefault(void *data)
 		vas = uk_vas_get_active();
 		if (unlikely(vas && !(vas->flags & UK_VAS_FLAG_NO_PAGING)))
 			uk_pr_crit("Cannot handle %s page fault at 0x%"
-				   __PRIvaddr" (ec: 0x%x): %s (%d).\n",
+				   __PRIvaddr" (ec: 0x%lx): %s (%d).\n",
 				   faultstr[faulttype &
 					    UK_VMA_FAULT_ACCESSTYPE],
-				   vaddr, ctx->error_code, strerror(-rc), -rc);
+				   vaddr, ctx->esr, strerror(-rc), -rc);
 
 		return UK_EVENT_NOT_HANDLED;
 	}