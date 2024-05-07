c43225fc2b3aaf8d936c4c66f064db9d3fecb146
Alex Apostolescu <alexx.apostolescu@gmail.com>
2024-05-07 15:18:39 +0300

    sys/endian.h: Add host to little/big endian functions

--- lib/ukswrand/include/sys/endian.h
+++ lib/ukswrand/include/sys/endian.h
@@ -45,6 +45,47 @@
 #define	bswap32(x)	__bswap32(x)
 #define	bswap64(x)	__bswap64(x)
 
+/*
+ * General byte order swapping functions.
+ * TODO: Check if these are available in the unikraft environment.
+ */
+#define _BYTE_ORDER __BYTE_ORDER__
+#define _LITTLE_ENDIAN __ORDER_LITTLE_ENDIAN__
+#define _BIG_ENDIAN __ORDER_BIG_ENDIAN__
+/*
+ * Host to big endian, host to little endian, big endian to host, and little
+ * endian to host byte order functions as detailed in byteorder(9).
+ */
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+#define htobe16(x) bswap16((x))
+#define htobe32(x) bswap32((x))
+#define htobe64(x) bswap64((x))
+#define htole16(x) ((uint16_t)(x))
+#define htole32(x) ((uint32_t)(x))
+#define htole64(x) ((uint64_t)(x))
+
+#define be16toh(x) bswap16((x))
+#define be32toh(x) bswap32((x))
+#define be64toh(x) bswap64((x))
+#define le16toh(x) ((uint16_t)(x))
+#define le32toh(x) ((uint32_t)(x))
+#define le64toh(x) ((uint64_t)(x))
+#else /* _BYTE_ORDER != _LITTLE_ENDIAN */
+#define htobe16(x) ((uint16_t)(x))
+#define htobe32(x) ((uint32_t)(x))
+#define htobe64(x) ((uint64_t)(x))
+#define htole16(x) bswap16((x))
+#define htole32(x) bswap32((x))
+#define htole64(x) bswap64((x))
+
+#define be16toh(x) ((uint16_t)(x))
+#define be32toh(x) ((uint32_t)(x))
+#define be64toh(x) ((uint64_t)(x))
+#define le16toh(x) bswap16((x))
+#define le32toh(x) bswap32((x))
+#define le64toh(x) bswap64((x))
+#endif /* _BYTE_ORDER == _LITTLE_ENDIAN */
+
 /* Alignment-agnostic encode/decode bytestream to/from little/big endian. */
 static __inline uint16_t
 be16dec(const void *pp)
