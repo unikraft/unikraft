bfd5747b8a2d62f3410ea09a3a188f632033f98c
Alex Apostolescu <alexx.apostolescu@gmail.com>
2024-05-07 17:11:11 +0300

    stubs.h: Add SYSINIT as stub
    Use uk_early_initcall() to cover the init function. This approach lacks the initialisation of data however.

--- lib/ukswrand/include/stubs.h
+++ lib/ukswrand/include/stubs.h
@@ -1,9 +1,14 @@
 #ifndef _FORTUNA_UTILS_STUBS_
 #define _FORTUNA_UTILS_STUBS_
 
+#include <uk/init.h>
 #include <stdint.h>
 #define __NEED_size_t
 #include <nolibc-internal/shareddefs.h>
+
+/* TODO: This should be replaced */
+#define	SYSINIT(uniq, subs, order, _func, _data)	\
+uk_early_initcall(_func, 0x0)
 
 void explicit_bzero(void *p, size_t n);
 int timingsafe_bcmp(const void *b1, const void *b2, size_t n);
