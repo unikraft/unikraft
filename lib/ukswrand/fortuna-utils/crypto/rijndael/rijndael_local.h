c00d8b877fc8fe2a8d6d0e6a2464269ee5d4c8ef
Alex Apostolescu <alexx.apostolescu@gmail.com>
2024-05-07 14:48:16 +0300

    rijndael_local: Include stdint.h

--- lib/ukswrand/fortuna-utils/crypto/rijndael/rijndael_local.h
+++ lib/ukswrand/fortuna-utils/crypto/rijndael/rijndael_local.h
@@ -1,5 +1,7 @@
 /*	$KAME: rijndael_local.h,v 1.5 2003/08/28 08:37:24 itojun Exp $	*/
 
+#include <stdint.h>
+
 /* the file should not be used from outside */
 typedef uint8_t			u8;
 typedef uint16_t		u16;
