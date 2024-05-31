d46b2683967be6d5722b87860c30d546247ea633
Alex Apostolescu <alexx.apostolescu@gmail.com>
2024-05-31 07:43:02 +0300

    lib/ukrandom: Add interrupt context check for ChaCha
    Solve a previous TODO by checking that interruots are not disabled.
    
    Signed-off-by: Alex Apostolescu <alexx.apostolescu@gmail.com>

--- lib/ukrandom/include/uk/random.h
+++ lib/ukrandom/include/uk/random.h
@@ -50,12 +50,13 @@ extern struct uk_swrand uk_swrand_def;
 __u32 uk_swrand_randr_r(struct uk_swrand *r);
 
 /* Uses the pre-initialized default generator  */
-/* TODO: Add assertion when we can test if we are in interrupt context */
 /* TODO: Revisit with multi-CPU support */
 static inline __u32 uk_swrand_randr(void)
 {
 	unsigned long iflags;
 	__u32 ret;
+
+	UK_ASSERT(!ukplat_lcpu_irqs_disabled());
 
 	iflags = ukplat_lcpu_save_irqf();
 	ret = uk_swrand_randr_r(&uk_swrand_def);
