6c8e31161d0eb4bb7ca0cbaf678096bd0c2a7224
Thassilo Schulze <thassilo@unikraft.io>
2024-07-11 10:49:13 +0200

    drivers/uktty: Adapt NS16550 for use with `ukconsole`
    These changes are functionally identical to those in the
    PL011 driver.
    
    Signed-off-by: Thassilo Schulze <thassilo@unikraft.io>

--- drivers/uktty/ns16550/ns16550.c
+++ drivers/uktty/ns16550/ns16550.c
@@ -27,6 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#include "uk/arch/lcpu.h"
 #include <libfdt.h>
 #include <uk/assert.h>
 #include <uk/config.h>
@@ -34,6 +35,13 @@
 #include <uk/libparam.h>
 #include <uk/ofw/fdt.h>
 #include <uk/plat/common/bootinfo.h>
+#include <uk/console.h>
+#include <uk/compiler.h>
+#include <uk/errptr.h>
+
+#if CONFIG_LIBUKALLOC
+#include <uk/alloc.h>
+#endif /* CONFIG_LIBUKALLOC */
 
 #if CONFIG_PAGING
 #include <uk/bus/platform.h>
@@ -77,18 +85,16 @@ static const char * const fdt_compatible[] = {
 	"ns16550", "ns16550a", NULL
 };
 
-/*
- * NS16550 UART base address
- * As we are using the PA = VA mapping, some SoC would set PA 0
- * as a valid address, so we can't use ns16550_uart_base == 0 to
- * indicate NS16550 hasn't been initialized. In this case, we
- * use ns16550_uart_initialized as an extra variable to check
- * whether the UART has been initialized.
- */
-static __u8 ns16550_uart_initialized;
-static __u64 ns16550_uart_base;
+struct ns16550_device {
+	struct uk_console dev;
+	__u64 base, size;
+};
 
-UK_LIBPARAM_PARAM_ALIAS(base, &ns16550_uart_base, __u64, "ns16550 base");
+#if CONFIG_LIBUKTTY_NS16550_EARLY_CONSOLE
+static struct ns16550_device earlycon;
+
+UK_LIBPARAM_PARAM_ALIAS(base, &earlycon.base, __u64, "ns15550 base");
+#endif /* CONFIG_LIBUKTTY_NS16550_EARLY_CONSOLE */
 
 /* The register shift. Default is 0 (device-tree spec v0.4 Sect. 4.2.2) */
 static __u32 ns16550_reg_shift;
@@ -97,21 +103,21 @@ static __u32 ns16550_reg_shift;
 static __u32 ns16550_reg_width = 1;
 
 /* Macros to access ns16550 registers with base address and reg shift */
-#define NS16550_REG(r) (ns16550_uart_base + (r << ns16550_reg_shift))
+#define NS16550_REG(base, r) ((base) + ((r) << ns16550_reg_shift))
 
-static __u32 ns16550_reg_read(__u32 reg)
+static __u32 ns16550_reg_read(__u64 base, __u32 reg)
 {
 	__u32 ret;
 
 	switch (ns16550_reg_width) {
 	case 1:
-		ret = ioreg_read8((__u8 *)NS16550_REG(reg)) & 0xff;
+		ret = ioreg_read8((__u8 *)NS16550_REG(base, reg)) & 0xff;
 		break;
 	case 2:
-		ret = ioreg_read16((__u16 *)NS16550_REG(reg)) & 0xffff;
+		ret = ioreg_read16((__u16 *)NS16550_REG(base, reg)) & 0xffff;
 		break;
 	case 4:
-		ret = ioreg_read32((__u32 *)NS16550_REG(reg));
+		ret = ioreg_read32((__u32 *)NS16550_REG(base, reg));
 		break;
 	default:
 		UK_CRASH("Invalid register width: %d\n", ns16550_reg_width);
@@ -119,58 +125,134 @@ static __u32 ns16550_reg_read(__u32 reg)
 	return ret;
 }
 
-static void ns16550_reg_write(__u32 reg, __u32 value)
+static void ns16550_reg_write(__u64 base, __u32 reg, __u32 value)
 {
 	switch (ns16550_reg_width) {
 	case 1:
-		ioreg_write8((__u8 *)NS16550_REG(reg),
+		ioreg_write8((__u8 *)NS16550_REG(base, reg),
 			     (__u8)(value & 0xff));
 		break;
 	case 2:
-		ioreg_write16((__u16 *)NS16550_REG(reg),
+		ioreg_write16((__u16 *)NS16550_REG(base, reg),
 			      (__u16)(value & 0xffff));
 		break;
 	case 4:
-		ioreg_write32((__u32 *)NS16550_REG(reg), value);
+		ioreg_write32((__u32 *)NS16550_REG(base, reg), value);
 		break;
 	default:
 		UK_CRASH("Invalid register width: %d\n", ns16550_reg_width);
 	}
 }
 
-static int init_ns16550(void)
+static void _putc(__u64 base, char a)
+{
+	/* Wait until TX FIFO becomes empty */
+	while (!(ns16550_reg_read(base, NS16550_LSR_OFFSET) &
+		 NS16550_LSR_TX_EMPTY))
+		;
+
+	/* Reset DLAB and write to THR */
+	ns16550_reg_write(base, NS16550_LCR_OFFSET,
+			  ns16550_reg_read(base, NS16550_LCR_OFFSET) &
+			  ~(NS16550_LCR_DLAB));
+	ns16550_reg_write(base, NS16550_THR_OFFSET, a & 0xff);
+}
+
+static void ns16550_putc(__u64 base, char a)
+{
+	if (a == '\n')
+		_putc(base, '\r');
+	_putc(base, a);
+}
+
+/* Try to get data from ns16550 UART without blocking */
+static int ns16550_getc(__u64 base)
+{
+	/* If RX FIFO is empty, return -1 immediately */
+	if (!(ns16550_reg_read(base, NS16550_LSR_OFFSET) &
+	      NS16550_LSR_RX_EMPTY))
+		return -1;
+
+	/* Reset DLAB and read from RBR */
+	ns16550_reg_write(base, NS16550_LCR_OFFSET,
+			  ns16550_reg_read(base, NS16550_LCR_OFFSET) &
+			  ~(NS16550_LCR_DLAB));
+	return (int)(ns16550_reg_read(base, NS16550_RBR_OFFSET) & 0xff);
+}
+
+static __ssz ns16550_out(struct uk_console *dev, const char *buf, __sz len)
+{
+	struct ns16550_device *ns16550_dev;
+	__sz l = len;
+
+	UK_ASSERT(dev);
+	UK_ASSERT(buf);
+
+	ns16550_dev = __containerof(dev, struct ns16550_device, dev);
+
+	while (l--)
+		ns16550_putc(ns16550_dev->base, *buf++);
+
+	return len;
+}
+
+static __ssz ns16550_in(struct uk_console *dev, char *buf, __sz len)
+{
+	struct ns16550_device *ns16550_dev;
+	int rc;
+
+	UK_ASSERT(dev);
+	UK_ASSERT(buf);
+
+	ns16550_dev = __containerof(dev, struct ns16550_device, dev);
+
+	for (__sz i = 0; i < len; i++) {
+		if ((rc = ns16550_getc(ns16550_dev->base)) < 0)
+			return i;
+		buf[i] = (char)rc;
+	}
+
+	return len;
+}
+
+static struct uk_console_ops ns16550_ops = {
+	.out  = ns16550_out,
+	.in = ns16550_in
+};
+
+static int init_ns16550(__u64 base)
 {
 	__u32 lcr;
 
 	/* Clear DLAB to access IER, FCR, LCR */
-	ns16550_reg_write(NS16550_LCR_OFFSET,
-			  ns16550_reg_read(NS16550_LCR_OFFSET) &
+	ns16550_reg_write(base, NS16550_LCR_OFFSET,
+			  ns16550_reg_read(base, NS16550_LCR_OFFSET) &
 			  ~(NS16550_LCR_DLAB));
 
 	/* Disable all interrupts */
-	ns16550_reg_write(NS16550_IER_OFFSET,
-			  ns16550_reg_read(NS16550_FCR_OFFSET) &
-					 ~(NS16550_IIR_NO_INT));
+	ns16550_reg_write(base, NS16550_IER_OFFSET,
+			  ns16550_reg_read(base, NS16550_FCR_OFFSET) &
+			  ~(NS16550_IIR_NO_INT));
 
 	/* Disable FIFOs */
-	ns16550_reg_write(NS16550_FCR_OFFSET,
-			  ns16550_reg_read(NS16550_FCR_OFFSET) &
-					 ~(NS16550_FCR_FIFO_EN));
+	ns16550_reg_write(base, NS16550_FCR_OFFSET,
+			  ns16550_reg_read(base, NS16550_FCR_OFFSET) &
+			  ~(NS16550_FCR_FIFO_EN));
 
 	/* Set line control parameters (8n1) */
-	lcr = ns16550_reg_read(NS16550_LCR_OFFSET) |
+	lcr = ns16550_reg_read(base, NS16550_LCR_OFFSET) |
 	      ~(NS16550_LCR_WL | NS16550_LCR_STOP | NS16550_LCR_PARITY);
 	lcr |= NS16550_LCR_8N1;
-	ns16550_reg_write(NS16550_LCR_OFFSET, lcr);
+	ns16550_reg_write(base, NS16550_LCR_OFFSET, lcr);
 
 	/* Set DLAB to access DLL / DLM */
-	ns16550_reg_write(NS16550_LCR_OFFSET,
-			  ns16550_reg_read(NS16550_LCR_OFFSET) &
+	ns16550_reg_write(base, NS16550_LCR_OFFSET,
+			  ns16550_reg_read(base, NS16550_LCR_OFFSET) &
 			  ~(NS16550_LCR_DLAB));
 
 	/* Set baud (115200) */
-	ns16550_reg_write(NS16550_DLL_OFFSET, NS16550_DLL_115200);
-	ns16550_reg_write(NS16550_DLM_OFFSET, NS16550_DLM_115200);
+	ns16550_reg_write(base, NS16550_DLL_OFFSET, NS16550_DLL_115200);
+	ns16550_reg_write(base, NS16550_DLM_OFFSET, NS16550_DLM_115200);
 
 	return 0;
 }
@@ -198,7 +280,8 @@ static inline int config_fdt_chosen_stdout(const void *dtb)
 	if (unlikely(rc)) /* could not parse node */
 		return rc;
 
-	ns16550_uart_base = base;
+	earlycon.base = base;
+	earlycon.size = size;
 
 	return 0;
 }
@@ -213,7 +296,7 @@ static int early_init(struct ukplat_bootinfo *bi)
 	/* If the base is not set by the cmdline, try
 	 * the dtb chosen/stdout-path.
 	 */
-	if (!ns16550_uart_base) {
+	if (!earlycon.base) {
 		rc = config_fdt_chosen_stdout((void *)bi->dtb);
 		if (unlikely(rc < 0 && rc != -FDT_ERR_NOTFOUND)) {
 			uk_pr_err("Could not parse fdt (%d)", rc);
@@ -224,21 +307,23 @@ static int early_init(struct ukplat_bootinfo *bi)
 	/* Do not return an error if no config is detected, as
 	 * another console driver may be enabled in Kconfig.
 	 */
-	if (!ns16550_uart_base)
+	if (!earlycon.base)
 		return 0;
 
 	/* Configure the port */
-	rc = init_ns16550();
+	rc = init_ns16550(earlycon.base);
 	if (unlikely(rc)) {
 		uk_pr_err("Could not initialize ns16550 (%d)\n", rc);
 		return rc;
 	}
 
-	/* From this point we can print */
-	ns16550_uart_initialized = 1;
+	earlycon.dev = UK_CONSOLE("NS16550", &ns16550_ops,
+				  UK_CONSOLE_FLAG_STDOUT |
+				  UK_CONSOLE_FLAG_STDIN);
+	uk_console_register(&earlycon.dev);
 
-	mrd.pbase = ns16550_uart_base;
-	mrd.vbase = ns16550_uart_base;
+	mrd.pbase = earlycon.base;
+	mrd.vbase = earlycon.base;
 	mrd.pg_off = 0;
 	mrd.pg_count = 1;
 	mrd.len = 0x1000;
@@ -255,15 +340,71 @@ static int early_init(struct ukplat_bootinfo *bi)
 }
 #endif /* !CONFIG_LIBUKTTY_NS16550_EARLY_CONSOLE */
 
+#if CONFIG_LIBUKALLOC
+static int fdt_get_device(struct ns16550_device *dev, const void *dtb,
+			  int *offset)
+{
+	__u64 reg_base, reg_size;
+	int rc;
+
+	UK_ASSERT(dev);
+	UK_ASSERT(dtb);
+	UK_ASSERT(offset);
+
+	*offset = fdt_node_offset_by_compatible_list(dtb, *offset,
+						     fdt_compatible);
+	if (unlikely(*offset < 0))
+		return -ENOENT;
+
+	rc = fdt_get_address(dtb, *offset, 0, &reg_base, &reg_size);
+	if (unlikely(rc)) {
+		uk_pr_err("Could not parse fdt node\n");
+		return -EINVAL;
+	}
+
+	uk_pr_debug("ns16550 @ 0x%lx - 0x%lx\n", reg_base, reg_base + reg_size);
+
+	dev->dev = UK_CONSOLE("NS16550", &ns16550_ops, 0);
+	dev->base = reg_base;
+	dev->size = reg_size;
+
+	return 0;
+}
+
+static int register_device(struct ns16550_device *dev, struct uk_alloc *a)
+{
+	struct ns16550_device *console_dev;
+
+	UK_ASSERT(dev);
+	UK_ASSERT(a);
+
+	console_dev = uk_malloc(a, sizeof(*console_dev));
+	if (unlikely(!console_dev)) {
+		uk_pr_err("Could not allocate ns16550 device\n");
+		return -ENOMEM;
+	}
+
+	console_dev->dev = dev->dev;
+	console_dev->base = dev->base;
+	console_dev->size = dev->size;
+
+	uk_console_register(&console_dev->dev);
+	uk_pr_info("tty: ns16550 (%p)\n", &console_dev->base);
+
+	return 0;
+}
+#endif /* CONFIG_LIBUKALLOC */
+
 static int init(struct uk_init_ctx *ictx __unused)
 {
+#if CONFIG_LIBUKALLOC
 	struct ukplat_bootinfo *bi;
 	const __u64 *regs;
 	const void *dtb;
-	int offset, len;
-	__u64 reg_base;
-	__u64 reg_size;
+	int offset = -1, len;
 	int rc;
+	struct uk_alloc *a;
+	struct ns16550_device dev;
 
 	bi = ukplat_bootinfo_get();
 	UK_ASSERT(bi);
@@ -271,120 +412,61 @@ static int init(struct uk_init_ctx *ictx __unused)
 	dtb = (void *)bi->dtb;
 	UK_ASSERT(dtb);
 
+	a = uk_alloc_get_default();
+	UK_ASSERT(a);
+
 	uk_pr_debug("Probing ns16550\n");
 
-	/* Pick the 1st device in the dtb */
-	offset = fdt_node_offset_by_compatible_list((void *)bi->dtb, -1,
-						    fdt_compatible);
-	if (unlikely((offset < 0))) {
-		uk_pr_err("ns16550 not found in fdt\n");
-		return -ENODEV;
-	}
+	while (1) {
+		rc = fdt_get_device(&dev, dtb, &offset);
+		if (unlikely(offset == -FDT_ERR_NOTFOUND))
+			break; /* No more devices */
 
-	rc = fdt_get_address((void *)bi->dtb, offset, 0, &reg_base, &reg_size);
-	if (unlikely(rc)) {
-		uk_pr_err("Could not parse fdt node\n");
-		return rc;
-	}
+		if (unlikely(rc < 0)) {
+			uk_pr_err("Could not get ns16550 device\n");
+			return rc;
+		}
 
 #if CONFIG_PAGING
-	/* Map device region */
-	ns16550_uart_base = uk_bus_pf_devmap(reg_base, reg_size);
-	if (unlikely(PTRISERR(ns16550_uart_base))) {
-		uk_pr_err("Could not map ns16550\n");
-		return PTR2ERR(ns16550_uart_base);
-	}
-#else /* !CONFIG_PAGING */
-	ns16550_uart_base = reg_base;
+		/* Map device region */
+		dev.base = uk_bus_pf_devmap(dev.base, dev.size);
+		if (unlikely(PTRISERR(dev.base))) {
+			uk_pr_err("Could not map ns16550\n");
+			return PTR2ERR(dev.base);
+		}
 #endif /* !CONFIG_PAGING */
 
-	regs = fdt_getprop(dtb, offset, "reg-shift", &len);
-	if (regs)
-		ns16550_reg_shift = fdt32_to_cpu(regs[0]);
+#if CONFIG_LIBUKTTY_NS16550_EARLY_CONSOLE
+		/* `ukconsole` mandates that there is only a single
+		 * `struct uk_console` registered per device.
+		 */
+		if (dev.base == earlycon.base) {
+			uk_pr_info("Skipping ns16550 device\n");
+			continue;
+		}
+#endif /* CONFIG_LIBUKTTY_NS16550_EARLY_CONSOLE */
 
-	regs = fdt_getprop(dtb, offset, "reg-io-width", &len);
-	if (regs)
-		ns16550_reg_width = fdt32_to_cpu(regs[0]);
+		regs = fdt_getprop(dtb, offset, "reg-shift", &len);
+		if (regs)
+			ns16550_reg_shift = fdt32_to_cpu(regs[0]);
 
-	uk_pr_debug("ns16550 @ 0x%lx - 0x%lx\n", reg_base, reg_base + reg_size);
+		regs = fdt_getprop(dtb, offset, "reg-io-width", &len);
+		if (regs)
+			ns16550_reg_width = fdt32_to_cpu(regs[0]);
 
-	rc = init_ns16550();
-	if (unlikely(rc)) {
-		uk_pr_err("Could not initialize ns16550\n");
-		return rc;
+		rc = init_ns16550(dev.base);
+		if (unlikely(rc)) {
+			uk_pr_err("Could not initialize ns16550\n");
+			return rc;
+		}
+
+		rc = register_device(&dev, a);
+		if (unlikely(rc < 0))
+			return rc;
 	}
-
-	ns16550_uart_initialized = 1;
-	uk_pr_info("tty: ns16550\n");
+#endif /* CONFIG_LIBUKALLOC */
 
 	return 0;
-}
-
-static void _putc(char a)
-{
-	/* Wait until TX FIFO becomes empty */
-	while (!(ns16550_reg_read(NS16550_LSR_OFFSET) & NS16550_LSR_TX_EMPTY))
-		;
-
-	/* Reset DLAB and write to THR */
-	ns16550_reg_write(NS16550_LCR_OFFSET,
-			  ns16550_reg_read(NS16550_LCR_OFFSET) &
-			  ~(NS16550_LCR_DLAB));
-	ns16550_reg_write(NS16550_THR_OFFSET, a & 0xff);
-}
-
-static void ns16550_putc(char a)
-{
-	/* Avoid using the UART before base address initialized */
-	if (!ns16550_uart_initialized)
-		return;
-
-	if (a == '\n')
-		_putc('\r');
-	_putc(a);
-}
-
-/* Try to get data from ns16550 UART without blocking */
-static int ns16550_getc(void)
-{
-	/* Avoid using the UART before base address initialized */
-	if (!ns16550_uart_initialized)
-		return -1;
-
-	/* If RX FIFO is empty, return -1 immediately */
-	if (!(ns16550_reg_read(NS16550_LSR_OFFSET) & NS16550_LSR_RX_EMPTY))
-		return -1;
-
-	/* Reset DLAB and read from RBR */
-	ns16550_reg_write(NS16550_LCR_OFFSET,
-			  ns16550_reg_read(NS16550_LCR_OFFSET) &
-			  ~(NS16550_LCR_DLAB));
-	return (int)(ns16550_reg_read(NS16550_RBR_OFFSET) & 0xff);
-}
-
-int ukplat_coutk(const char *buf, unsigned int len)
-{
-	for (unsigned int i = 0; i < len; i++)
-		ns16550_putc(buf[i]);
-	return len;
-}
-
-int ukplat_coutd(const char *str, unsigned int len)
-{
-	return ukplat_coutk(str, len);
-}
-
-int ukplat_cink(char *buf, unsigned int maxlen)
-{
-	int ret;
-	unsigned int num = 0;
-
-	while (num < maxlen && (ret = ns16550_getc()) >= 0) {
-		*(buf++) = (char)ret;
-		num++;
-	}
-
-	return (int)num;
 }
 
 #if CONFIG_LIBUKTTY_NS16550_EARLY_CONSOLE
