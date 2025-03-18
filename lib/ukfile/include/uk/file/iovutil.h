eacce9de53d4f4121b741c92e4e6fededdf681cc
Andrei Tatar <andrei@unikraft.io>
2025-03-18 13:48:36 +0100

    lib/ukfile: Improve & expand iovec utils
    This change corrects the logic of the existing zero, scatter and gather
    iovec utils, eliminating a number of buggy cornercases; the function
    documentation has been updated to more explicitly state preconditions.
    In addition, 2 new util inlines are added: uk_iov_len and uk_iov_ff.
    
    Signed-off-by: Andrei Tatar <andrei@unikraft.io>

--- lib/ukfile/include/uk/file/iovutil.h
+++ lib/ukfile/include/uk/file/iovutil.h
@@ -15,8 +15,77 @@
 #include <uk/essentials.h>
 
 /**
+ * Count the number of bytes described by `iov[iovcnt]`.
+ *
+ * @return Total number of bytes described by `iov[iovcnt]`
+ */
+static inline
+size_t uk_iov_len(const struct iovec *iov, size_t iovcnt)
+{
+	size_t total = 0;
+
+	for (size_t i = 0; i < iovcnt; i++)
+		total += iov[i].iov_len;
+	return total;
+}
+
+/**
+ * Count the number of bytes remaining in `iov[iovcnt]` starting at
+ * `iov[iovi][cur]`.
+ *
+ * If `iovi` is in bounds (< `iovcnt`), `cur` must also be in bounds
+ * (<= `iov[iovi].iov_len`).
+ *
+ * @return Total number of bytes remaining
+ */
+static inline
+size_t uk_iov_remaining(const struct iovec *iov, size_t iovcnt,
+			size_t iovi, size_t cur)
+{
+	if (iovi < iovcnt)
+		return uk_iov_len(iov + iovi, iovcnt - iovi) - cur;
+	return 0;
+}
+
+/**
+ * Fast-forward the iovec cursors `iovip` and `curp` by at most `len` bytes.
+ *
+ * If `*iovip` is in bounds (< `iovcnt`), `*curp` must also be in bounds
+ * (<= `iov[*iovip].iov_len`).
+ *
+ * @return Number of bytes actually fast-forwarded by
+ */
+static inline
+size_t uk_iov_ff(const struct iovec *iov, size_t iovcnt, size_t len,
+		 size_t *iovip, size_t *curp)
+{
+	size_t ret = 0;
+	size_t i = *iovip;
+	size_t cur = *curp;
+
+	while (i < iovcnt && len) {
+		const size_t sz = MIN(len, iov[i].iov_len - cur);
+
+		UK_ASSERT(cur <= iov[i].iov_len);
+		len -= sz;
+		cur += sz;
+		ret += sz;
+		if (cur == iov[i].iov_len) {
+			i++;
+			cur = 0;
+		}
+	}
+	*iovip = i;
+	*curp = cur;
+	return ret;
+}
+
+/**
  * Zero out at most `len` bytes in memory regions described by `iov[iovcnt]`,
  * starting at `*curp` offset from the buffer at `iov[*iovip]`.
+ *
+ * If `*iovip` is in bounds (< `iovcnt`), `*curp` must also be in bounds
+ * (<= `iov[*iovip].iov_len`).
  *
  * If the total remaining space in `iov` is less than `len`, exit early.
  * After the call, `*iovip` and `*curp` are updated with the new positions.
@@ -31,34 +100,23 @@ size_t uk_iov_zero(const struct iovec *iov, size_t iovcnt, size_t len,
 	size_t i = *iovip;
 	size_t cur = *curp;
 
-	UK_ASSERT(i < iovcnt);
-	UK_ASSERT(cur < iov[i].iov_len);
-	if (cur) {
-		const size_t l = MIN(len, iov[i].iov_len - cur);
+	while (i < iovcnt && len) {
+		const size_t sz = MIN(len, iov[i].iov_len - cur);
 
-		memset((char *)iov[i].iov_base + cur, 0, l);
-		ret += l;
-		len -= l;
-		cur += l;
+		UK_ASSERT(cur <= iov[i].iov_len); /* cur must be in bounds */
+		UK_ASSERT(sz || cur == iov[i].iov_len); /* must make progress */
+		if (sz) {
+			memset((char *)iov[i].iov_base + cur, 0, sz);
+			ret += sz;
+			len -= sz;
+			cur += sz;
+		}
 		if (cur == iov[i].iov_len) {
 			i++;
 			cur = 0;
 		}
 	}
-	if (len) {
-		UK_ASSERT(!cur);
-		while (i < iovcnt && len) {
-			const size_t l = MIN(iov[i].iov_len, len);
 
-			memset(iov[i].iov_base, 0, l);
-			ret += l;
-			len -= l;
-			if (len)
-				i++;
-			else
-				cur = l;
-		}
-	}
 	*iovip = i;
 	*curp = cur;
 	return ret;
@@ -67,6 +125,9 @@ size_t uk_iov_zero(const struct iovec *iov, size_t iovcnt, size_t len,
 /**
  * Copy at most `len` bytes from `buf` into memory regions described by
  * `iov[iovcnt]`, starting at `*curp` offset from the buffer at `iov[*iovip]`.
+ *
+ * If `*iovip` is in bounds (< `iovcnt`), `*curp` must also be in bounds
+ * (<= `iov[*iovip].iov_len`).
  *
  * If the total remaining space in `iov` is less than `len`, exit early.
  * After the call, `*iovip` and `*curp` are updated with the new positions.
@@ -81,36 +142,24 @@ size_t uk_iov_scatter(const struct iovec *iov, size_t iovcnt, const char *buf,
 	size_t i = *iovip;
 	size_t cur = *curp;
 
-	UK_ASSERT(i < iovcnt);
-	UK_ASSERT(cur < iov[i].iov_len);
-	if (cur) {
-		const size_t l = MIN(len, iov[i].iov_len - cur);
+	while (i < iovcnt && len) {
+		const size_t sz = MIN(len, iov[i].iov_len - cur);
 
-		memcpy((char *)iov[i].iov_base + cur, buf, l);
-		ret += l;
-		buf += l;
-		len -= l;
-		cur += l;
+		UK_ASSERT(cur <= iov[i].iov_len); /* cur must be in bounds */
+		UK_ASSERT(sz || cur == iov[i].iov_len); /* must make progress */
+		if (sz) {
+			memcpy((char *)iov[i].iov_base + cur, buf, sz);
+			ret += sz;
+			buf += sz;
+			len -= sz;
+			cur += sz;
+		}
 		if (cur == iov[i].iov_len) {
 			i++;
 			cur = 0;
 		}
 	}
-	if (len) {
-		UK_ASSERT(!cur);
-		while (i < iovcnt && len) {
-			const size_t l = MIN(iov[i].iov_len, len);
 
-			memcpy(iov[i].iov_base, buf, l);
-			ret += l;
-			buf += l;
-			len -= l;
-			if (len)
-				i++;
-			else
-				cur = l;
-		}
-	}
 	*iovip = i;
 	*curp = cur;
 	return ret;
@@ -119,6 +168,9 @@ size_t uk_iov_scatter(const struct iovec *iov, size_t iovcnt, const char *buf,
 /**
  * Copy at most `len` bytes from the memory regions described by `iov[iovcnt]`,
  * starting at `*curp` offset from the buffer at `iov[*iovip]`, into `buf`.
+ *
+ * If `*iovip` is in bounds (< `iovcnt`), `*curp` must also be in bounds
+ * (<= `iov[*iovip].iov_len`).
  *
  * If the total remaining bytes in `iov` are less than `len`, exit early.
  * After the call, `*iovip` and `*curp` are updated with the new positions.
@@ -133,67 +185,27 @@ size_t uk_iov_gather(char *buf, const struct iovec *iov, size_t iovcnt,
 	size_t i = *iovip;
 	size_t cur = *curp;
 
-	UK_ASSERT(i < iovcnt);
-	UK_ASSERT(cur < iov[i].iov_len);
-	if (cur) {
-		const size_t l = MIN(len, iov[i].iov_len - cur);
+	while (i < iovcnt && len) {
+		const size_t sz = MIN(len, iov[i].iov_len - cur);
 
-		memcpy(buf, (char *)iov[i].iov_base + cur, l);
-		ret += l;
-		buf += l;
-		len -= l;
-		cur += l;
+		UK_ASSERT(cur <= iov[i].iov_len); /* cur must be in bounds */
+		UK_ASSERT(sz || cur == iov[i].iov_len); /* must make progress */
+		if (sz) {
+			memcpy(buf, (char *)iov[i].iov_base + cur, sz);
+			ret += sz;
+			buf += sz;
+			len -= sz;
+			cur += sz;
+		}
 		if (cur == iov[i].iov_len) {
 			i++;
 			cur = 0;
 		}
 	}
-	if (len) {
-		UK_ASSERT(!cur);
-		while (i < iovcnt && len) {
-			const size_t l = MIN(iov[i].iov_len, len);
 
-			memcpy(buf, iov[i].iov_base, l);
-			ret += l;
-			buf += l;
-			len -= l;
-			if (len)
-				i++;
-			else
-				cur = l;
-		}
-	}
 	*iovip = i;
 	*curp = cur;
 	return ret;
-}
-
-/**
- * Check how many bytes the memory regions described by `iov[iovcnt]`,
- * starting at `cur` offset from the buffer at `iov[iovi]` can fit.
- *
- * @return total bytes the iov can fit
- */
-static inline
-size_t uk_iov_remaining(const struct iovec *iov, size_t iovcnt,
-			size_t iovi, size_t cur)
-{
-	size_t len = 0, i = iovi;
-
-	UK_ASSERT(i < iovcnt);
-	UK_ASSERT(cur < iov[i].iov_len);
-
-	if (cur) {
-		len += iov[i].iov_len - cur;
-		i++;
-	}
-
-	while (i < iovcnt) {
-		len += iov[i].iov_len;
-		i++;
-	}
-
-	return len;
 }
 
 #endif /* __UKFILE_IOVUTIL_H__ */
