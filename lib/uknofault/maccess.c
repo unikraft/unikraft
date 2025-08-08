9a1617a57f03f8b339f721c89b8e7a46500a2bca
Michalis Pappas <michalis@unikraft.io>
2025-08-08 17:38:11 +0200

    lib/uknofault: Adapt to strongly typed uk_event_status API
    Update event handlers and event raising calls to use the new
    enum return type and error parameter.
    
    Checkpatch-Ignore: LONG_LINE
    Signed-off-by: Michalis Pappas <michalis@unikraft.io>

--- lib/uknofault/maccess.c
+++ lib/uknofault/maccess.c
@@ -5,12 +5,13 @@
  */
 
 #include <uk/arch/limits.h>
-#include <uk/nofault.h>
+#include <uk/arch/paging.h>
 #include <uk/arch/types.h>
 #include <uk/arch/traps.h>
-#include <uk/arch/paging.h>
-#include <uk/event.h>
+#include <uk/compiler.h>
 #include <uk/config.h>
+#include <uk/event.h>
+#include <uk/nofault.h>
 
 #include "arch/maccess.h"
 
@@ -44,8 +45,9 @@ typedef int nf_paging_state_t;
 #endif /* !CONFIG_LIBUKVMEM */
 
 /* Must be a library global symbol so that the linker can resolve it */
-int __used nf_mf_handler(const struct nf_excpttab_entry *e,
-			 struct ukarch_trap_ctx *ctx)
+enum uk_event_status __used nf_mf_handler(const struct nf_excpttab_entry *e,
+					  struct ukarch_trap_ctx *ctx,
+					  int *error __unused)
 {
 	/* Just continue execution at the fault label */
 	nf_regs_ip(ctx->regs) = nf_excpttab_get_cont_ip(e);
@@ -53,11 +55,11 @@ int __used nf_mf_handler(const struct nf_excpttab_entry *e,
 	return UK_EVENT_HANDLED;
 }
 
-static int nf_mem_fault_handler(void *data)
+static enum uk_enum_status nf_mem_fault_handler(void *data, int *error)
 {
 	struct ukarch_trap_ctx *ctx = (struct ukarch_trap_ctx *)data;
 
-	return nf_handle_trap(nf_regs_ip(ctx->regs), ctx);
+	return nf_handle_trap(nf_regs_ip(ctx->regs), ctx, error);
 }
 
 UK_EVENT_HANDLER_PRIO(UKARCH_TRAP_PAGE_FAULT, nf_mem_fault_handler,
