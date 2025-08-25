b0dbd93da28153a160275340bd2045f4c68d3b32
Michalis Pappas <michalis@unikraft.io>
2025-08-25 14:47:23 +0200

    lib/uklock: Adapt to the new posix-process event API
    Adapt to the new posix-process event API.
    
    Signed-off-by: Michalis Pappas <michalis@unikraft.io>

--- lib/uklock/semaphore.c
+++ lib/uklock/semaphore.c
@@ -17,13 +17,12 @@ void uk_semaphore_init(struct uk_semaphore *s, long count)
 
 #if CONFIG_LIBPOSIX_PROCESS_MULTITHREADING
 /* parent and child share System V semaphores */
-static int uk_posix_clone_sysvsem(const struct clone_args *cl_args __unused,
-				  size_t cl_args_len __unused,
-				  struct uk_thread *child __unused,
-				  struct uk_thread *parent __unused)
+static
+int uk_posix_clone_sysvsem(void *arg __unused)
 {
 	UK_WARN_STUBBED();
-	return 0;
+	return UK_EVENT_HANDLED_CONT;
 }
-UK_POSIX_CLONE_HANDLER(CLONE_SYSVSEM, true, uk_posix_clone_sysvsem, 0x0);
+
+POSIX_PROCESS_CLONE_HANDLER(CLONE_SYSVSEM, uk_posix_clone_sysvsem);
 #endif /* CONFIG_LIBPOSIX_PROCESS_MULTITHREADING */
