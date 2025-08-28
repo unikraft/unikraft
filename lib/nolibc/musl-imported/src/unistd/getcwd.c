cc8f8a2eb1acd034a7585b3037188afbe5edc346
Andrei Tatar <ttr@unikraft.io>
2025-08-28 17:26:38 +0200

    lib/nolibc: Fix return local variable warning
    This change fixes a warning in getcwd about possibly returning a local
    variable's address. The warning is technically true, as the caller
    could, by sheer luck, pass buf == tmp which would cause a strdup.
    This fix plugs that cornercase with a temporary local, preserving the
    documented logic of calling strdup only if !buf.
    
    Signed-off-by: Andrei Tatar <ttr@unikraft.io>

--- lib/nolibc/musl-imported/src/unistd/getcwd.c
+++ lib/nolibc/musl-imported/src/unistd/getcwd.c
@@ -8,19 +8,20 @@ long uk_syscall_r_getcwd(char *buf, size_t sz);
 char *getcwd(char *buf, size_t size)
 {
 	char tmp[buf ? 1 : PATH_MAX];
+	char *p = buf;
 	if (!buf) {
-		buf = tmp;
+		p = tmp;
 		size = sizeof tmp;
 	} else if (!size) {
 		errno = EINVAL;
 		return 0;
 	}
-	long ret = uk_syscall_r_getcwd(buf, size);
+	long ret = uk_syscall_r_getcwd(p, size);
 	if (ret < 0)
 		return 0;
-	if (ret == 0 || buf[0] != '/') {
+	if (ret == 0 || p[0] != '/') {
 		errno = ENOENT;
 		return 0;
 	}
-	return buf == tmp ? strdup(buf) : buf;
+	return buf ? buf : strdup(tmp);
 }
