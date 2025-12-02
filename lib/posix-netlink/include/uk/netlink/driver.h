89af301c7fe3ddcbf3ce906373c9c5485b232686
Andrei Tatar <ttr@unikraft.io>
2025-12-02 14:35:52 +0100

    lib/posix-netlink: Refactor & API completeness
    This change is a minor refactor of the netlink socket implementation,
    completing its implementation of the posix-socket API, with stubs where
    needed. In addition, the netlink driver API is better documented, and
    several previous issues and cornercases fixed.
    
    Checkpatch-Ignore: MACRO_ARG_REUSE
    Signed-off-by: Andrei Tatar <ttr@unikraft.io>

--- lib/posix-netlink/include/uk/netlink/driver.h
+++ lib/posix-netlink/include/uk/netlink/driver.h
@@ -17,30 +17,69 @@
 extern "C" {
 #endif /* __cplusplus */
 
-struct nl_ctx {
-	struct uk_alloc *a;
-	const struct posix_netlink_protocol *drv;
+/* Netlink context and driver helpers */
 
-	int bound;
+struct nl_ctx {
+	const struct posix_netlink_protocol *drv;
+	struct uk_mbox *nl_recvqueue;
+	struct uk_alloc *allocator;
+
+	__u32 flags; /* Internal use flags */
+	__u32 nl_groups;
 #if CONFIG_LIBPOSIX_PROCESS
 	pid_t nl_pid; /* port of client */
 #endif /* CONFIG_LIBPOSIX_PROCESS */
-	__u32 nl_groups;
-
-	struct uk_mbox *nl_recvqueue;
 };
 
-/*
- * Helper to get pid from ctx
- */
+/* Helper to get pid from ctx agnostic of CONFIG_LIBPOSIX_PROCESS */
 #if CONFIG_LIBPOSIX_PROCESS
 #define nl_ctx_pid(ctx) ({ (ctx)->nl_pid; })
 #else /* !CONFIG_LIBPOSIX_PROCESS */
 #define nl_ctx_pid(ctx) (0)
 #endif /* !CONFIG_LIBPOSIX_PROCESS */
 
+static inline struct uk_streambuf *nlbuf_alloc(struct nl_ctx *ctx, size_t len)
+{
+	return uk_streambuf_alloc2(ctx->allocator, len, sizeof(long),
+				   UK_STREAMBUF_C_WIPEZERO);
+}
+
+#define nlbuf_reserve(nlbuf, len)	uk_streambuf_reserve((nlbuf), (len))
+#define nlbuf_free(nlbuf)		uk_streambuf_free((nlbuf))
+#define nlbuf_len(nlbuf)		uk_streambuf_seek((nlbuf))
+#define nlbuf_data(nlbuf)		uk_streambuf_buf((nlbuf))
+
+static inline void *nlbuf_align(struct uk_streambuf *nlbuf, size_t align)
+{
+	const size_t seek = uk_streambuf_seek(nlbuf);
+
+	return uk_streambuf_reserve(nlbuf, ALIGN_UP(seek, align) - seek);
+}
+
+static inline void nlbuf_send(struct nl_ctx *ctx, struct uk_streambuf *nlbuf)
+{
+	uk_mbox_post(ctx->nl_recvqueue, nlbuf);
+}
+
+/* Netlink driver ops & driver registration */
+
 /**
- * Accept a connection on a socket.
+ * REQUIRED. Handle netlink requests on a socket.
+ *
+ * Drivers should attempt to signal any error conditions as netlink replies, and
+ * error out of the handler ONLY when replying is impossible.
+ * It is forbidden to return error after a reply has been sent.
+ *
+ * @param ctx Netlink context of the socket
+ * @param nlh Netlink request message
+ *
+ * @return 0 on success, negative errno otherwise
+ */
+typedef int (*posix_netlink_protocol_handle_func_t)(struct nl_ctx *ctx,
+						    const struct nlmsghdr *nlh);
+
+/**
+ * OPTIONAL. Callback on creating a netlink socket.
  *
  * @param sock Reference to the socket
  * @param addr The address of the peer socket
@@ -49,78 +88,47 @@ struct nl_ctx {
  * @param flags Additional flags to be set for the accepted connection. If
  *    flags is 0, accept4 is the same as POSIX accept.
  *
- * @return NULL or a valid pointer to driver-specific data on success,
- *    ERR2PTR(-errno) otherwise
+ * @return 0 on success, negative errno otherwise
  */
 typedef int (*posix_netlink_protocol_create_func_t)(struct nl_ctx *ctx);
 
-typedef int (*posix_netlink_protocol_handle_func_t)
-	     (struct nl_ctx *ctx, const struct nlmsghdr *nlh);
-
+/**
+ * OPTIONAL. Callback on closing a netlink socket.
+ *
+ * @param ctx Netlink context of the socket
+ */
 typedef void (*posix_netlink_protocol_close_func_t)(struct nl_ctx *ctx);
 
 struct posix_netlink_protocol_ops {
-	posix_netlink_protocol_create_func_t create;
 	posix_netlink_protocol_handle_func_t handle;
+	posix_netlink_protocol_create_func_t create;
 	posix_netlink_protocol_close_func_t close;
 };
 
-/**
- * The POSIX netlink protocol driver defines the operations to be used for the
- * specified (classic) netlink protocol as well as the memory allocator.
- */
-struct __align(8) posix_netlink_protocol {
-	/** The netlink protocol ID */
-	const int protocol;
-	/** Name of the driver library */
-	const char *libname;
+struct posix_netlink_protocol {
 	/** The interfaces for this socket */
 	const struct posix_netlink_protocol_ops *ops;
-	/** Private data for this socket driver */
-	void *private;
-};
+	/** Name of the driver library */
+	const char *libname;
+	/** The netlink protocol ID */
+	const int protocol;
+} __align(8);
 
-static inline struct uk_streambuf *nlbuf_alloc(struct nl_ctx *ctx, size_t len)
-{
-	return uk_streambuf_alloc2(ctx->a, len, sizeof(long),
-				   UK_STREAMBUF_C_WIPEZERO);
-}
-
-#define nlbuf_reserve(nlbuf, len)	uk_streambuf_reserve((nlbuf), (len))
-#define nlbuf_align(nlbuf, align)					       \
-	uk_streambuf_reserve((nlbuf), ALIGN_UP(uk_streambuf_seek((nlbuf)),     \
-					       (align)) -		       \
-					       uk_streambuf_seek((nlbuf)))
-#define nlbuf_free(nlbuf)		uk_streambuf_free((nlbuf))
-
-static inline void nlbuf_send(struct nl_ctx *ctx, struct uk_streambuf *nlbuf)
-{
-	return uk_mbox_post(ctx->nl_recvqueue, nlbuf);
-}
-
-#define nlbuf_len(nlbuf)		uk_streambuf_seek((nlbuf))
-#define nlbuf_data(nlbuf)		uk_streambuf_buf((nlbuf))
-
-/**
- * Registers a netlink protocol driver
- */
-#define _POSIX_NETLINK_PROTOCOL_DRVRNAME(lib, prot)			\
+#define _POSIX_NETLINK_PROTOCOL_DRVRNAME(lib, prot) \
 	posix_netlink_protocol_ ## lib ## _ ## prot
 
-#define _POSIX_NETLINK_PROTOCOL_SECNAME(lib, prot)			\
+#define _POSIX_NETLINK_PROTOCOL_SECNAME(lib, prot) \
 	STRINGIFY(_POSIX_NETLINK_PROTOCOL_DRVRNAME(lib, prot))
 
-/*
- * Creates a static struct posix_netlink_protocol for the AF protocol
- */
+/* Register a netlink driver part of `lib` with `vops` for AF `prot`. */
 #define _POSIX_NETLINK_PROTOCOL_REGISTER(lib, prot, vops)		\
 	__used __align(8)						\
 		__section("." _POSIX_NETLINK_PROTOCOL_SECNAME(lib, prot))\
-	static struct posix_netlink_protocol				\
+	static const struct posix_netlink_protocol			\
 	_POSIX_NETLINK_PROTOCOL_DRVRNAME(lib, prot) = {			\
-		.protocol = prot,					\
+		.protocol = (prot),					\
 		.libname = STRINGIFY(lib),				\
-		.ops = vops						\
+		.ops = (vops)						\
 	}
 
 #define POSIX_NETLINK_PROTOCOL_REGISTER(prot, vops) \
--- lib/posix-netlink/netlink.c
+++ lib/posix-netlink/netlink.c
@@ -5,11 +5,18 @@
  */
 
 #include <string.h>
-#include <unistd.h>
 
+#include <uk/file/iovutil.h>
 #include <uk/netlink/driver.h>
 #include <uk/socket_driver.h>
 #include <uk/print.h>
+
+#if CONFIG_LIBPOSIX_PROCESS
+#include <uk/process.h>
+#endif /* CONFIG_LIBPOSIX_PROCESS */
+
+#define NL_CONNECTED	1
+#define NL_BOUND	2
 
 #define sock2nlctx(s) ((struct nl_ctx *)posix_sock_get_data(s))
 
@@ -41,36 +48,32 @@ static void *nl_create(struct posix_socket_driver *drv,
 
 	/* AF_NETLINK sockets must be created with SOCK_RAW or SOCK_DGRAM */
 	if (unlikely(((type & ~(SOCK_NONBLOCK | SOCK_CLOEXEC)) != SOCK_RAW) &&
-		     ((type & ~(SOCK_NONBLOCK | SOCK_CLOEXEC)) != SOCK_DGRAM))) {
-		err = EINVAL;
-		goto err_out;
-	}
+		     ((type & ~(SOCK_NONBLOCK | SOCK_CLOEXEC)) != SOCK_DGRAM)))
+		return ERR2PTR(-EINVAL);
 
 	/* Lookup a driver for the requested protocol */
 	nl_drv = nl_driver_find(protocol);
 	if (unlikely(!nl_drv)) {
 		uk_pr_debug("did not find protocol handler for %d\n", protocol);
-		err = EAFNOSUPPORT;
-		goto err_out;
+		return ERR2PTR(-EAFNOSUPPORT);
 	}
 
 	nl_ctx = uk_zalloc(drv->allocator, sizeof(*nl_ctx));
-	if (unlikely(!nl_ctx)) {
-		err = ENOMEM;
-		goto err_out;
-	}
-	nl_ctx->a = drv->allocator;
+	if (unlikely(!nl_ctx))
+		return ERR2PTR(-ENOMEM);
+
+	nl_ctx->allocator = drv->allocator;
 	nl_ctx->drv = nl_drv;
 #if CONFIG_LIBPOSIX_PROCESS
 	/* FIXME: PID is here like port number and must be unique. It just
 	 *        starts with PID if free.
 	 */
-	nl_ctx->nl_pid = getpid();
+	nl_ctx->nl_pid = uk_sys_getpid();
 #endif /* CONFIG_LIBPOSIX_PROCESS */
 
-	nl_ctx->nl_recvqueue = uk_mbox_create(nl_ctx->a, 512);
+	nl_ctx->nl_recvqueue = uk_mbox_create(nl_ctx->allocator, 512);
 	if (unlikely(!nl_ctx->nl_recvqueue)) {
-		err = ENOMEM;
+		err = -ENOMEM;
 		goto err_free_ctx;
 	}
 
@@ -86,11 +89,10 @@ static void *nl_create(struct posix_socket_driver *drv,
 	return nl_ctx;
 
 err_free_recvqueue:
-	uk_mbox_free(nl_ctx->a, nl_ctx->nl_recvqueue);
+	uk_mbox_free(nl_ctx->allocator, nl_ctx->nl_recvqueue);
 err_free_ctx:
-	uk_free(nl_ctx->a, nl_ctx);
-err_out:
-	return ERR2PTR(-err);
+	uk_free(nl_ctx->allocator, nl_ctx);
+	return ERR2PTR(err);
 }
 
 static int nl_close(posix_sock *s)
@@ -107,8 +109,8 @@ static int nl_close(posix_sock *s)
 		nlbuf_free(nlbuf);
 	}
 
-	uk_mbox_free(nl_ctx->a, nl_ctx->nl_recvqueue);
-	uk_free(nl_ctx->a, nl_ctx);
+	uk_mbox_free(nl_ctx->allocator, nl_ctx->nl_recvqueue);
+	uk_free(nl_ctx->allocator, nl_ctx);
 	return 0;
 }
 
@@ -119,22 +121,29 @@ static int nl_bind(posix_sock *s,
 	const struct sockaddr_nl *nl_addr;
 	struct nl_ctx *nl_ctx = sock2nlctx(s);
 
-	if (unlikely(nl_ctx->bound))
-		return -EADDRINUSE;
+	/* Bound netlink sockets exist for subscribing to multicast messages.
+	 * We only support unicast user -> kernel comms + repies, thus bind is
+	 * merely a stub that stores the bound address and returns success.
+	 *
+	 * TODO: Add true multicast subscription when needed.
+	 */
+
+	if (unlikely(nl_ctx->flags & NL_BOUND))
+		return -EINVAL;
 	if (unlikely(!addr || addr_len < sizeof(*nl_addr)))
 		return -EINVAL;
 	nl_addr = (const struct sockaddr_nl *)addr;
 
 	if (unlikely((nl_addr->nl_family != AF_NETLINK) ||
-		     (nl_addr->nl_pad != 0))
+		     (nl_addr->nl_pad != 0)))
 		return -EINVAL;
 
-	/* We only support the client to connect to us: the kernel */
+	/* We only support automatic PID/port allocation */
 	if (nl_addr->nl_pid != 0)
-		return -EHOSTUNREACH;
+		return -EADDRNOTAVAIL;
 
 	nl_ctx->nl_groups = nl_addr->nl_groups;
-	nl_ctx->bound = 1;
+	nl_ctx->flags |= NL_BOUND;
 	return 0;
 }
 
@@ -142,22 +151,63 @@ static int nl_sockname(posix_sock *s,
 		       struct sockaddr *restrict addr,
 		       socklen_t *restrict addr_len)
 {
-	struct sockaddr_nl *nl_addr;
 	struct nl_ctx *nl_ctx = sock2nlctx(s);
+	const struct sockaddr_nl nl_addr = {
+		.nl_family = AF_NETLINK,
+		.nl_pad = 0,
+		.nl_pid = nl_ctx_pid(nl_ctx),
+		.nl_groups = nl_ctx->nl_groups
+	};
 
-	if (*addr_len < sizeof(*nl_addr)) {
-		/* addr_len is too small, only inform about needed size */
-		goto out;
-	}
+	if (unlikely(!addr || !addr_len))
+		return -EFAULT;
 
-	nl_addr = (struct sockaddr_nl *)addr;
-	nl_addr->nl_family = AF_NETLINK;
-	nl_addr->nl_pid = nl_ctx_pid(nl_ctx);
-	nl_addr->nl_pad = 0;
-	nl_addr->nl_groups = nl_ctx->nl_groups;
+	memcpy(addr, &nl_addr, MIN(*addr_len, sizeof(nl_addr)));
+	*addr_len = sizeof(nl_addr);
+	return 0;
+}
 
-out:
-	*addr_len = sizeof(*nl_addr);
+static int nl_connect(posix_sock *s,
+		      const struct sockaddr *addr, socklen_t addr_len)
+{
+	struct nl_ctx *nl_ctx = sock2nlctx(s);
+	const struct sockaddr_nl *nl_addr;
+
+	/* We can only ever send messages to the kernel, thus connect only
+	 * validates its args and returns success.
+	 */
+
+	if (unlikely(!addr || addr_len < sizeof(*nl_addr)))
+		return -EINVAL;
+
+	nl_addr = (const struct sockaddr_nl *)addr;
+	if (unlikely(nl_addr->nl_family != AF_NETLINK))
+		return -EAFNOSUPPORT;
+	if (unlikely(nl_addr->nl_pid != 0))
+		return -EHOSTUNREACH;
+	nl_ctx->flags |= NL_CONNECTED;
+	return 0;
+}
+
+static int nl_getpeername(posix_sock *s,
+			  struct sockaddr *restrict addr,
+			  socklen_t *restrict addr_len)
+{
+	struct nl_ctx *nl_ctx = sock2nlctx(s);
+	const struct sockaddr_nl nl_addr = {
+		.nl_family = AF_NETLINK,
+		.nl_pad = 0,
+		.nl_pid = 0, /* Only kernel can be peer */
+		.nl_groups = 0 /* All messages are unicast */
+	};
+
+	if (unlikely(!addr || !addr_len))
+		return -EFAULT;
+	if (unlikely(!(nl_ctx->flags & NL_CONNECTED)))
+		return -ENOTCONN;
+	/* Write out peer (truncating if needed) */
+	memcpy(addr, &nl_addr, MIN(*addr_len, sizeof(nl_addr)));
+	*addr_len = sizeof(nl_addr);
 	return 0;
 }
 
@@ -165,7 +215,7 @@ static inline int nl_handle(struct nl_ctx *nl_ctx, const void *buf, size_t len)
 {
 	const struct nlmsghdr *nlh;
 	size_t tmp_len;
-	int ret = 0;
+	int ret;
 
 	UK_ASSERT(nl_ctx);
 	UK_ASSERT(nl_ctx->drv);
@@ -194,9 +244,11 @@ static inline int nl_handle(struct nl_ctx *nl_ctx, const void *buf, size_t len)
 		uk_pr_debug("Call handler for msg %p (driver %s)\n",
 			    nlh, nl_ctx->drv->libname);
 		ret = nl_ctx->drv->ops->handle(nl_ctx, nlh);
+		UK_ASSERT(ret <= 0);
+		if (unlikely(ret < 0))
+			return ret;
 	}
-
-	return ret;
+	return 0;
 }
 
 static ssize_t nl_sendto(posix_sock *s,
@@ -204,67 +256,65 @@ static ssize_t nl_sendto(posix_sock *s,
 			 const struct sockaddr *dest_addr, socklen_t addrlen)
 {
 	struct nl_ctx *nl_ctx = sock2nlctx(s);
-	struct sockaddr_nl *nl_addr;
+	const struct sockaddr_nl *nl_addr;
 	int ret;
 
-	if (addrlen < sizeof(*nl_addr))
-		return -EINVAL;
+	if (dest_addr) {
+		if (unlikely(addrlen < sizeof(*nl_addr)))
+			return -EINVAL;
 
-	nl_addr = (struct sockaddr_nl *)dest_addr;
-	if (unlikely(nl_addr->nl_family != AF_NETLINK || nl_addr->nl_pid != 0))
-		return -EHOSTUNREACH;
-
-	ret = nl_handle(nl_ctx, buf, len);
-	if (unlikely(ret < 0))
-		return -EINVAL;
-	return len;
-}
-
-static size_t copy2iovec(struct iovec iovec[], size_t iovec_len,
-			 const void *src, size_t len)
-{
-	size_t left = len;
-	size_t cpylen;
-	size_t i = 0;
-
-	for (i = 0; i < iovec_len && left; ++i) {
-		cpylen = MIN(iovec[i].iov_len, left);
-		memcpy(iovec[i].iov_base, src, cpylen);
-
-		left -= cpylen;
-		src  += cpylen;
+		nl_addr = (const struct sockaddr_nl *)dest_addr;
+		if (unlikely(nl_addr->nl_family != AF_NETLINK))
+			return -EAFNOSUPPORT;
+		if (unlikely(nl_addr->nl_pid != 0))
+			return -EHOSTUNREACH;
+	} else if (unlikely(!(nl_ctx->flags & NL_CONNECTED))) {
+		return -EDESTADDRREQ;
 	}
 
-	return (len - left);
+	ret = nl_handle(nl_ctx, buf, len);
+	if (unlikely(ret))
+		return ret;
+	return len;
 }
 
 static ssize_t nl_recvmsg(posix_sock *s, struct msghdr *msg,
 			  int flags __unused)
 {
 	struct uk_streambuf *nlbuf;
-	struct sockaddr_nl *nl_addr;
 	struct nl_ctx *nl_ctx = sock2nlctx(s);
+	const struct sockaddr_nl nl_addr = {
+		.nl_family = AF_NETLINK,
+		.nl_pad = 0,
+		.nl_pid = 0, /* All messages originate from the kernel */
+		.nl_groups = 0 /* All messages are unicast */
+	};
+	size_t iovi = 0;
+	size_t cur = 0;
+	size_t msglen;
 	size_t cpylen;
 
 	uk_pr_debug("Picking up packet from netlink mbox\n");
-	uk_mbox_recv(nl_ctx->nl_recvqueue, (void **)&nlbuf);
+	if (unlikely(uk_mbox_recv_try(nl_ctx->nl_recvqueue, (void **)&nlbuf)))
+		/* No pending messages */
+		return -EAGAIN;
 	if (unlikely(!nlbuf))
 		return -EIO;
 
-	cpylen = copy2iovec(msg->msg_iov, msg->msg_iovlen,
-			    nlbuf_data(nlbuf), nlbuf_len(nlbuf));
+	msglen = nlbuf_len(nlbuf);
+	cpylen = uk_iov_scatter(msg->msg_iov, msg->msg_iovlen,
+				nlbuf_data(nlbuf), msglen,
+				&iovi, &cur);
 	nlbuf_free(nlbuf);
 	uk_pr_debug("Message received\n");
 
-	/* Fill name structure */
-	if (msg->msg_name && msg->msg_namelen == sizeof(struct sockaddr_nl)) {
-		nl_addr = msg->msg_name;
-		nl_addr->nl_family = AF_NETLINK;
-		nl_addr->nl_pid = 0;
-		nl_addr->nl_pad = 0;
-		nl_addr->nl_groups = nl_ctx->nl_groups;
+	/* Fill in msg struct & return */
+	msg->msg_flags = (cpylen < msglen) ? MSG_TRUNC : 0;
+	if (msg->msg_name) {
+		memcpy(msg->msg_name, &nl_addr,
+		       MIN(msg->msg_namelen, sizeof(nl_addr)));
+		msg->msg_namelen = sizeof(nl_addr);
 	}
-
 	return (ssize_t)cpylen;
 }
 
@@ -272,16 +322,25 @@ static ssize_t nl_recvfrom(posix_sock *s,
 			   void *buf, size_t len, int flags __unused,
 			   struct sockaddr *from, socklen_t *fromlen)
 {
-	struct sockaddr_nl *nl_addr;
 	struct uk_streambuf *nlbuf;
 	struct nl_ctx *nl_ctx = sock2nlctx(s);
+	const struct sockaddr_nl nl_addr = {
+		.nl_family = AF_NETLINK,
+		.nl_pad = 0,
+		.nl_pid = 0, /* All messages originate from the kernel */
+		.nl_groups = 0 /* All messages are unicast */
+	};
 	size_t cpylen;
 
-	if (from && !fromlen)
+	if (unlikely(!buf))
+		return -EFAULT;
+	if (unlikely(from && !fromlen))
 		return -EINVAL;
 
 	uk_pr_debug("Picking up packet from netlink mbox\n");
-	uk_mbox_recv(nl_ctx->nl_recvqueue, (void **)&nlbuf);
+	if (unlikely(uk_mbox_recv_try(nl_ctx->nl_recvqueue, (void **)&nlbuf)))
+		/* No pending messages */
+		return -EAGAIN;
 	if (unlikely(!nlbuf))
 		return -EIO;
 
@@ -291,47 +350,96 @@ static ssize_t nl_recvfrom(posix_sock *s,
 	uk_pr_debug("Message received\n");
 
 	if (from) {
-		nl_addr = (struct sockaddr_nl *)from;
-		nl_addr->nl_family = AF_NETLINK;
-		nl_addr->nl_pid = 0;
-		nl_addr->nl_pad = 0;
-		nl_addr->nl_groups = nl_ctx->nl_groups;
-		*fromlen = sizeof(*nl_addr);
+		memcpy(from, &nl_addr, MIN(*fromlen, sizeof(nl_addr)));
+		*fromlen = sizeof(nl_addr);
 	}
 
 	return (ssize_t)cpylen;
 }
 
-static int nl_sockopt(const posix_sock *s __unused,
-		      int lvl __unused, int opt __unused,
-		      const void *val __unused,
-		      socklen_t optlen __unused)
+static void *nl_accept4(posix_sock *s __unused,
+			struct sockaddr *addr __unused,
+			socklen_t *addr_len __unused, int flags __unused)
+{
+	return ERR2PTR(-EOPNOTSUPP);
+}
+
+static int nl_shutdown(posix_sock *s __unused, int how __unused)
+{
+	return -ENOTCONN;
+}
+
+static int nl_getsockopt(posix_sock *s __unused,
+			 int lvl __unused, int opt __unused,
+			 void *optval __unused, socklen_t *optlen __unused)
+{
+	UK_WARN_STUBBED();
+	return -ENOPROTOOPT;
+}
+
+static int nl_setsockopt(posix_sock *s __unused,
+			 int lvl __unused, int opt __unused,
+			 const void *val __unused, socklen_t optlen __unused)
 {
 	UK_WARN_STUBBED();
 	return 0;
 }
 
-static int nl_ioctl(const posix_sock *s __unused,
-		    int request __unused,
-		    void *argp __unused)
+static int nl_listen(posix_sock *s __unused, int backlog __unused)
 {
-	return -ENOTSUP;
+	UK_WARN_STUBBED();
+	return 0;
 }
 
-static void nl_poll_setup(const posix_sock *s __unused)
+static ssize_t nl_sendmsg(posix_sock *s,
+			  const struct msghdr *msg, int flags)
+{
+	if (msg->msg_iovlen > 1) {
+		/* Fragmented netlink messages not currently supported */
+		UK_WARN_STUBBED();
+		return -EINVAL;
+	}
+	return nl_sendto(s,
+			 msg->msg_iov[0].iov_base, msg->msg_iov[0].iov_len,
+			 flags,
+			 msg->msg_name, msg->msg_namelen);
+}
+
+static int nl_sockpair(struct posix_socket_driver *d __unused,
+		       int family __unused, int type __unused,
+		       int prot __unused, void *usockvec[2] __unused)
+{
+	return -EOPNOTSUPP;
+}
+
+static int nl_ioctl(posix_sock *s __unused,
+		    int request __unused, void *argp __unused)
+{
+	return -EINVAL;
+}
+
+static void nl_poll_setup(posix_sock *s __unused)
 {
 	UK_WARN_STUBBED();
 }
 
 static const struct posix_socket_ops netlink_vops = {
 	.create		= nl_create,
-	.close		= nl_close,
+	.accept4	= nl_accept4,
 	.bind		= nl_bind,
-	.getsockname    = nl_sockname,
-	.sendto		= nl_sendto,
-	.recvmsg	= nl_recvmsg,
+	.shutdown	= nl_shutdown,
+	.getpeername	= nl_getpeername,
+	.getsockname	= nl_sockname,
+	.getsockopt	= nl_getsockopt,
+	.setsockopt	= nl_setsockopt,
+	.connect	= nl_connect,
+	.listen		= nl_listen,
 	.recvfrom	= nl_recvfrom,
-	.setsockopt	= nl_sockopt,
+	.recvmsg	= nl_recvmsg,
+	.sendmsg	= nl_sendmsg,
+	.sendto		= nl_sendto,
+	.socketpair	= nl_sockpair,
+	.close		= nl_close,
 	.ioctl		= nl_ioctl,
 	.poll_setup	= nl_poll_setup,
 };
