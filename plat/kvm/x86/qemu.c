ae0c2fd2dcfca4c7c7c214e2aaf7fc398ab98f5e
Sergiu Moga <sergiu@unikraft.io>
2026-02-04 20:25:56 +0200

    plat/kvm/x86: Register QEMU `isa-debug-exit` driver into `libukpm`
    Implement the minimum necessary subset of functionalities to shutdown
    the system using QEMU's isa-debug-exit device. This also helps with
    checking the return code of the QEMU process upon guest termination.
    
    Signed-off-by: Sergiu Moga <sergiu@unikraft.io>

---
+++ plat/kvm/x86/qemu.c
@@ -0,0 +1,71 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2026, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+
+#include <uk/arch.h>
+#include <uk/arch/util.h>
+#include <uk/boot/earlytab.h>
+#include <uk/pm.h>
+#include <uk/prio.h>
+
+/* The port used by QEMU by default for the isa-debug-exit device */
+#define QEMU_ISA_DEBUG_EXIT_PORT	0x501
+/* This corresponds to an 83 (41 << 1 | 1) return value from QEMU */
+#define QEMU_ISA_DEBUG_EXIT_NO_CRASH	41
+/* This corresponds to an 85 (42 << 1 | 1) return value from QEMU */
+#define QEMU_ISA_DEBUG_EXIT_CRASH	42
+
+/* The port used by QEMU by default for the pvpanic device */
+#define QEMU_PVPANIC_EXIT_PORT		0x505
+/* This corresponds to a GUEST_PANICKED event for QEMU */
+#define QEMU_PVPANIC_GUEST_PANICKED	(1 << 0)
+/* This corresponds to a GUEST_CRASHLOADED event for QEMU */
+#define QEMU_PVPANIC_GUEST_CRASHLOADED	(1 << 1)
+
+/**
+ * Trigger an exit() in QEMU with the code `value << 1 | 1`.
+ * @param value the value used in the calculation of the exit code
+ */
+__isr static inline void qemu_debug_exit(int value)
+{
+	uk_arch_outw(QEMU_ISA_DEBUG_EXIT_PORT, value);
+}
+
+__isr static int qemu_exit(void)
+{
+	qemu_debug_exit(QEMU_ISA_DEBUG_EXIT_NO_CRASH);
+
+	/* Return error if writing to port failed as that should not return. */
+	return -EIO;
+}
+
+__isr static int qemu_crash(void)
+{
+	/* If we are crashing, then try to exit QEMU with the isa-debug-exit
+	 * device.
+	 * Should be harmless if it is not present. This is used to enable
+	 * automated tests on virtio.
+	 * Also send a panic request to the pvpanic device.
+	 * Should be also harmless and helps with automated tests.
+	 */
+	qemu_debug_exit(QEMU_ISA_DEBUG_EXIT_CRASH);
+	uk_arch_outb(QEMU_PVPANIC_EXIT_PORT, QEMU_PVPANIC_GUEST_PANICKED);
+
+	/* Return error if writing to port failed as that should not return. */
+	return -EIO;
+}
+
+static const struct uk_pm_ops qemu_pm_ops = {
+	.syshalt = qemu_exit,
+	.sysrestart = qemu_exit,
+	.syscrash = qemu_crash,
+};
+
+static int qemu_register_pm_ops(struct ukplat_bootinfo __unused *bi)
+{
+	return uk_pm_ops_register(&qemu_pm_ops);
+}
+
+UK_BOOT_EARLYTAB_ENTRY(qemu_register_pm_ops, UK_PRIO_EARLIEST);
