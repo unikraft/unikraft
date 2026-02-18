0cac7acb3cfb24a002fee1dc084745036bd1d14c
Tudorica Radu Andrei <raduandreitudorica3@gmail.com>
2026-02-18 22:41:18 +0200

    lib/ukallocbuddy: Switch memory region size error to debug
    Switching memory region size error to debug level to reduce console noise during normal operation.
    
    Signed-off-by: Radu Andrei Tudorica <raduandreitudorica3@gmail.com>

--- lib/ukallocbbuddy/bbuddy.c
+++ lib/ukallocbbuddy/bbuddy.c
@@ -565,9 +565,10 @@ static int bbuddy_addmem(struct uk_alloc *a, void *base, size_t len)
 	 */
 	if (range < round_pgup(sizeof(*memr) + BYTES_PER_MAPWORD) +
 			__PAGE_SIZE) {
-		uk_pr_err("%"__PRIuptr": Failed to add memory region %"__PRIuptr"-%"__PRIuptr": Not enough space after applying page alignments\n",
-			  (uintptr_t) a, (uintptr_t) base,
-			  (uintptr_t) base + (uintptr_t) len);
+		uk_pr_debug("%"__PRIuptr": Failed to add memory region %"__PRIuptr \
+            "-%"__PRIuptr": Not enough space after applying page alignments\n",
+            (uintptr_t) a, (uintptr_t) base,
+            (uintptr_t) base + (uintptr_t) len);
 		return -EINVAL;
 	}
 
