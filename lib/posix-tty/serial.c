84cdfd0c15e1deb1dcadeeef2a7c4699a835a296
Sergiu Moga <sergiu@unikraft.io>
2026-03-10 18:17:07 +0200

    lib/posix-tty: Select `stdio` console via `posix_tty.console` arg
    Register `libposix_tty` with `uklibparam` to expose a `posix_tty.console`
    kernel argument. This allows the user to select which registered console
    device to use as the backing stdio console for the TTY, by specifying a
    devfs-style device node name (e.g. `ttyS0`, `hvc0`, `tty0`).
    
    Add an `init_tty_cons()` lib init call that parses the argument, resolves
    the requested console by class and index among all registered console
    devices, and stores it in `tty_cons`. If no argument is provided, fall
    back to the first registered console device.
    
    Replace the generic `uk_console_out`/`uk_console_in` calls with their
    `_direct` variants targeting the resolved `tty_cons`.
    
    Checkpatch-Ignore: COMMIT_SUBJECT_LONG_LINE
    Signed-off-by: Sergiu Moga <sergiu@unikraft.io>

--- lib/posix-tty/serial.c
+++ lib/posix-tty/serial.c
@@ -5,6 +5,7 @@
  */
 
 #include <termios.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
@@ -13,8 +14,22 @@
 #include <uk/file.h>
 #include <uk/file/nops.h>
 #include <uk/console.h>
+#include <uk/console/driver.h>
+#include <uk/init.h>
 #include <uk/posix-fd.h>
 #include <uk/posix-serialfile.h>
+
+#if CONFIG_LIBUKLIBPARAM
+#include <uk/libparam.h>
+#endif /* CONFIG_LIBUKLIBPARAM */
+
+static struct uk_console *tty_cons;
+static char *tty_cons_arg;
+
+#if CONFIG_LIBUKLIBPARAM
+UK_LIBPARAM_PARAM_ALIAS(console, &tty_cons_arg, charp,
+			"Default application stdio console device based on devfs node");
+#endif /* CONFIG_LIBUKLIBPARAM */
 
 static const char SERIAL_VOLID[] = "serial_vol";
 
@@ -71,14 +86,17 @@ static inline __ssz _console_out(const char *buf, __sz len)
 		next_nl = memchr(buf, '\n', l);
 		if (next_nl) {
 			off = next_nl - buf;
-			if ((rc = uk_console_out(buf, off)) < 0)
+			if ((rc = uk_console_out_direct(tty_cons,
+							buf, off)) < 0)
 				return rc;
-			if ((rc = uk_console_out("\r\n", 2)) < 0)
+			if ((rc = uk_console_out_direct(tty_cons,
+							"\r\n", 2)) < 0)
 				return rc;
 			buf = next_nl + 1;
 			l -= off + 1;
 		} else {
-			if ((rc = uk_console_out(buf, l)) < 0)
+			if ((rc = uk_console_out_direct(tty_cons,
+							buf, l)) < 0)
 				return rc;
 			break;
 		}
@@ -109,7 +127,7 @@ static ssize_t serial_read(const struct uk_file *f,
 		if (unlikely(!buf && len))
 			return -EFAULT;
 
-		bytes_read = uk_console_in(buf, len);
+		bytes_read = uk_console_in_direct(tty_cons, buf, len);
 		if (!bytes_read)
 			break;
 		if (unlikely(bytes_read < 0))
@@ -278,3 +296,74 @@ const struct uk_file uk_file_serial = {
 	.state = &serial_state,
 	._release = uk_file_static_release
 };
+
+static int init_tty_cons(struct uk_init_ctx *ictx __unused)
+{
+	__sz i, ccount, idx, cclass_str_len = 0;
+	enum uk_console_devclass tty_dclass;
+	struct uk_console *cons;
+	const char *str;
+
+	/* If no argument provided, assume the first registered one */
+	if (!tty_cons_arg) {
+		tty_cons = uk_console_get(0);
+		return 0;
+	}
+
+	if (!strncmp(tty_cons_arg, "hvc", sizeof("hvc") - 1)) {
+		tty_dclass = UK_CONSOLE_CLASS_HVC;
+		cclass_str_len = sizeof("hvc") - 1;
+	} else if (!strncmp(tty_cons_arg, "ttyS", sizeof("ttyS") - 1)) {
+		tty_dclass = UK_CONSOLE_CLASS_UART;
+		cclass_str_len = sizeof("ttyS") - 1;
+	} else if (!strncmp(tty_cons_arg, "tty", sizeof("tty") - 1)) {
+		tty_dclass = UK_CONSOLE_CLASS_FB;
+		cclass_str_len = sizeof("tty") - 1;
+	} else {
+		uk_pr_err("Invalid console argument\n");
+		return -EINVAL;
+	}
+
+	rc = sscanf(tty_cons_arg + cclass_str_len, "%lu", &idx);
+	if (unlikely(rc < 0)) {
+		uk_pr_err("Failed to parse tty_console arg\n");
+		return rc;
+	}
+
+	/**
+	 * Now go through each registered console and find the idx'th console
+	 * of class tty_cclass.
+	 */
+	ccount = uk_console_count();
+	for (i = 0; i < ccount; i++) {
+		cons = uk_console_get(i);
+		UK_ASSERT(cons);
+
+		 /* If not the class we care about, ignore and move on */
+		if (cons->dclass != tty_dclass)
+			continue;
+
+		/**
+		 * If idx is 0 and this is the first console of class
+		 * tty_cclass then we are done - assign it.
+		 * Otherwise we keep decrementing idx for each tty_cclass
+		 * class registration we find and when it hits 0 then we know
+		 * we found the user-intended idx'th console registration
+		 * of this class.
+		 */
+		if (!idx) {
+			tty_cons = cons;
+			break;
+		}
+		idx--;
+	}
+
+	if (unlikely(!tty_cons)) {
+		uk_pr_err("Requested console not found\n");
+		return -ENOENT;
+	}
+
+	return 0;
+}
+
+uk_lib_initcall_prio(init_tty_cons, 0x0, UK_PRIO_LATEST);
