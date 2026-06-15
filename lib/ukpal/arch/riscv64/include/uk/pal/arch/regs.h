42e399e26576d42d23b73fb93fb9d19ef7f99531
Zheng Zhang <zhangzheng@iscas.ac.cn>
2026-06-15 09:51:53 +0800

    lib/ukpal: Provide riscv64 PAL definitions
    Add the riscv64 PAL register, exception and sysctx definitions.
    
    Co-authored-by: Eduard Vintila <eduard.vintila47@gmail.com>

---
+++ lib/ukpal/arch/riscv64/include/uk/pal/arch/regs.h
@@ -0,0 +1,19 @@
+#ifndef __UK_PAL_REGS_H__
+#error "Do not include this header directly"
+#endif
+
+#ifndef UK_PAL_RISCV64_REGS_OFFSETOF_T0
+#error "UK_PAL_RISCV64_REGS_OFFSETOF_T0 undefined"
+#endif
+#ifndef UK_PAL_RISCV64_REGS_OFFSETOF_A0
+#error "UK_PAL_RISCV64_REGS_OFFSETOF_A0 undefined"
+#endif
+#ifndef UK_PAL_RISCV64_REGS_OFFSETOF_S0
+#error "UK_PAL_RISCV64_REGS_OFFSETOF_S0 undefined"
+#endif
+#ifndef UK_PAL_RISCV64_REGS_OFFSETOF_RA
+#error "UK_PAL_RISCV64_REGS_OFFSETOF_RA undefined"
+#endif
+#ifndef UK_PAL_RISCV64_REGS_OFFSETOF_TP
+#error "UK_PAL_RISCV64_REGS_OFFSETOF_TP undefined"
+#endif
