e5545c4c70669f9618041b393c4ce482e3bf6eb7
Michalis Pappas <michalis@unikraft.io>
2026-09-21 18:05:56 +0200

    plat/xen: Manage the page tables with libukpt
    Update xen/arm64 to manage pategables with libukpt. That avoids introducing
    duplicate page table management code as the implementation grows. Introduce
    uk_plat_xen_fixmap API to abstract libukpt from Xen drivers.
    
    Notable changes:
    * build_pagetable() replaces in-place rather than unmapping first to avoid
      faulting, given that it operations on the currently executing image.
    * PTEs are now encoded by the PAL, so memory attribute indices move to
      MAIR_INIT_ATTR, i.e. the layout the rest of Unikraft uses.
    
    Checkpatch-Ignore: CAMELCASE
    Signed-off-by: Michalis Pappas <michalis@unikraft.io>

--- plat/xen/arm/mm.c
+++ plat/xen/arm/mm.c
@@ -24,6 +24,7 @@
  */
 /* Ported from Mini-OS */
 
+#include <common/pt.h>
 #include <xen-arm/os.h>
 #include <xen-arm/mm.h>
 #include <libfdt.h>
@@ -33,71 +34,28 @@
 
 #if defined(__aarch64__)
 extern char stack[];
-extern lpae_t boot_l1_pgtable[512];
-extern lpae_t boot_l2_pgtable[512];
-extern lpae_t fixmap_pgtable[512];
-extern lpae_t idmap_pgtable[512];
 #endif
 
 paddr_t physical_address_offset;
 
-inline void set_pgt_entry(lpae_t *ptr, lpae_t val)
-{
-	*ptr = val;
-	uk_arch_arm64_dsb(ishst);
-	uk_arch_arm64_isb();
-}
-
-static void alloc_init_pud(lpae_t *pgd, unsigned long vaddr,
-					unsigned long vend, paddr_t phys)
-{
-	lpae_t *pud;
-	int count = 0;
-
-	/*
-	 * FIXME: to support >1GiB physical memory, need to allocate a new
-	 * level-2 page table from boot memory.
-	 */
-	if (!(*pgd))
-		BUG();
-
-	pud = (lpae_t *)to_virt((*pgd) & ~ATTR_MASK_L) + l2_pgt_idx(vaddr);
-	do {
-		set_pgt_entry(pud,
-			(phys & L2_MASK) | BLOCK_DEF_ATTR | L2_BLOCK);
-		vaddr += L2_SIZE;
-		phys += L2_SIZE;
-		pud++;
-		count++;
-	} while (vaddr < vend);
-}
-
 static void build_pagetable(unsigned long start_pfn, unsigned long max_pfn)
 {
-	paddr_t start_paddr, end_paddr;
-	unsigned long start_vaddr, end_vaddr;
-	unsigned long vaddr;
-	lpae_t *pgd;
+	unsigned int level = UK_PAL_PAGE_LARGE_LEVEL;
+	__sz pgsize = UK_PAL_PAGE_Lx_SIZE(level);
+	paddr_t start_paddr;
+	__sz len;
+	int rc;
 
 	start_paddr = PFN_PHYS(start_pfn);
-	end_paddr = PFN_PHYS(start_pfn + max_pfn);
+	len = PFN_PHYS(max_pfn);
 
-	start_vaddr = (unsigned long)to_virt(start_paddr);
-	end_vaddr = (unsigned long)to_virt(end_paddr);
-	pgd = &boot_l1_pgtable[l1_pgt_idx(start_vaddr)];
+	UK_ASSERT(UK_PAL_PAGE_Lx_ALIGNED(start_paddr, level));
 
-	vaddr = start_vaddr;
-	do {
-		unsigned long next;
-
-		next = (vaddr + L1_SIZE);
-		if (next > end_vaddr)
-			next = end_vaddr;
-		alloc_init_pud(pgd, vaddr, next, start_paddr);
-		start_paddr += next - vaddr;
-		vaddr = next;
-		pgd++;
-	} while (vaddr != end_vaddr);
+	rc = xenplat_pt_map((__vaddr_t)to_virt(start_paddr), start_paddr,
+			    (len + pgsize - 1) / pgsize, level,
+			    XENPLAT_PT_ATTR_MEM);
+	if (unlikely(rc))
+		UK_CRASH("Could not map RAM: %d\n", rc);
 }
 
 void arch_mm_prepare(unsigned long *start_pfn_p, unsigned long *max_pfn_p)
